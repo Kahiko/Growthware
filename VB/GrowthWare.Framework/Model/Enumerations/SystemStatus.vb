8f7d88035c9041dc26fa4cb10e381599105eeb65
kahiko <Kahiko@users.noreply.github.com>
2014-08-25 18:28:52 +0000

    good check in point

---
+++ VB/GrowthWare.Framework/Model/Enumerations/SystemStatus.vb
@@ -0,0 +1,24 @@
+﻿Namespace Model.Enumerations
+    Public Enum SystemStatus
+        ''' <summary>
+        ''' The active
+        ''' </summary>
+        Active = 1
+        ''' <summary>
+        ''' The change password
+        ''' </summary>
+        ChangePassword = 4
+        ''' <summary>
+        ''' The disabled
+        ''' </summary>
+        Disabled = 3
+        ''' <summary>
+        ''' The inactive
+        ''' </summary>
+        Inactive = 2
+        ''' <summary>
+        ''' Not used
+        ''' </summary>
+        None = 0
+    End Enum
+End Namespace
