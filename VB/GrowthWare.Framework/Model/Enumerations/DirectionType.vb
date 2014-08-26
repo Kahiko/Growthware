cbf1ec60d215331ce73170da00612f95a74b581a
kahiko <Kahiko@users.noreply.github.com>
2014-08-26 17:40:10 +0000

    first check in

---
+++ VB/GrowthWare.Framework/Model/Enumerations/DirectionType.vb
@@ -0,0 +1,12 @@
+﻿Namespace Model.Enumerations
+    ''' <summary>
+    ''' Enumeration of Direction Types
+    ''' </summary>
+    ''' <remarks>
+    ''' Used for indicating to move a sort order (up/down).
+    ''' </remarks>
+    Public Enum DirectionType
+        Up = 1
+        Down = 0
+    End Enum
+End Namespace
