606129a15654cc98b489ca6952b2cff4bfcc975c
kahiko <Kahiko@users.noreply.github.com>
2014-08-26 17:43:07 +0000

    first check in

---
+++ VB/GrowthWare.WebSupport/Utilities/FunctionUtility.vb
@@ -0,0 +1,169 @@
+﻿Imports GrowthWare.Framework.Common
+Imports GrowthWare.Framework.Model.Profiles
+Imports System.Web
+Imports System.Collections.ObjectModel
+Imports System.Globalization
+Imports GrowthWare.Framework.Model.Enumerations
+Imports GrowthWare.Framework.BusinessData.BusinessLogicLayer
+
+Namespace Utilities
+    Public Module FunctionUtility
+        Private m_FunctionProfileInfoName As String = "FunctionProfileInfo"
+
+        ''' <summary>
+        ''' Retrieves all functions from the either the database or cache
+        ''' </summary>
+        ''' <returns>A Collection of MFunctinProfiles</returns>
+        Public Function GetFunctions() As Collection(Of MFunctionProfile)
+            Dim mSecurityEntityProfile As MSecurityEntityProfile = SecurityEntityUtility.GetCurrentProfile()
+            Dim mBFunctions As BFunctions = New BFunctions(mSecurityEntityProfile, ConfigSettings.CentralManagement)
+            Dim mCacheName As String = mSecurityEntityProfile.Id.ToString() + "_Functions"
+            Dim mFunctionCollection As Collection(Of MFunctionProfile) = Nothing
+            mFunctionCollection = CType(HttpContext.Current.Cache(mCacheName), Collection(Of MFunctionProfile))
+            If mFunctionCollection Is Nothing Then
+                mFunctionCollection = mBFunctions.GetFunctions(mSecurityEntityProfile.Id)
+                CacheController.AddToCacheDependency(mCacheName, mFunctionCollection)
+            End If
+            Return mFunctionCollection
+        End Function
+
+        ''' <summary>
+        ''' Moves the specified profile.
+        ''' </summary>
+        ''' <param name="profile">The profile.</param>
+        ''' <param name="direction">The direction.</param>
+        ''' <param name="updatedBy">The updated by.</param>
+        ''' <param name="upDatedDate">Up dated date.</param>
+        Public Sub Move(ByRef profile As MFunctionProfile, ByVal direction As DirectionType, ByVal updatedBy As Integer, ByVal upDatedDate As DateTime)
+
+        End Sub
+
+        ''' <summary>
+        ''' Get a single function given it's action.
+        ''' </summary>
+        ''' <param name="action">String</param>
+        ''' <returns>MFunctionProfile</returns>
+        ''' <remarks>Returns null object if not found</remarks>
+        Public Function GetProfile(ByVal action As String) As MFunctionProfile
+            Dim mRetVal As MFunctionProfile = Nothing
+            If Not String.IsNullOrEmpty(action) Then
+                Dim mResult = From mProfile In GetFunctions() Where mProfile.Action.ToLower(CultureInfo.CurrentCulture) = action.ToLower(CultureInfo.CurrentCulture) Select mProfile
+                mRetVal = New MFunctionProfile()
+                Try
+                    mRetVal = mResult.First
+                Catch ex As NullReferenceException
+                    mRetVal = Nothing
+                End Try
+            End If
+            Return mRetVal
+        End Function
+
+        ''' <summary>
+        ''' Gets the current profile.
+        ''' </summary>
+        ''' <returns>MFunctionProfile.</returns>
+        Public Function GetCurrentProfile() As MFunctionProfile
+            Dim mRetVal As MFunctionProfile
+            mRetVal = CType(HttpContext.Current.Items(m_FunctionProfileInfoName), MFunctionProfile)
+            Return mRetVal
+        End Function
+
+        ''' <summary>
+        ''' Get a single function given it's id.
+        ''' </summary>
+        ''' <param name="id">int or Integer</param>
+        ''' <returns>MFunctionProfile</returns>
+        ''' <remarks>Returns null object if not found</remarks>
+        Public Function GetProfile(ByVal id As Integer) As MFunctionProfile
+            Dim mResult = From mProfile In GetFunctions() Where mProfile.Id = id Select mProfile
+            Dim mRetVal As MFunctionProfile = New MFunctionProfile()
+            Try
+                mRetVal = mResult.First
+            Catch ex As NullReferenceException
+                mRetVal = Nothing
+            End Try
+            Return mRetVal
+        End Function
+
+        ''' <summary>
+        ''' Gets the function menu order.
+        ''' </summary>
+        ''' <param name="profile">The profile.</param>
+        ''' <returns>DataTable.</returns>
+        Public Function GetFunctionMenuOrder(ByRef profile As MFunctionProfile) As DataTable
+            Dim mBFunctions As BFunctions = New BFunctions(SecurityEntityUtility.GetCurrentProfile(), ConfigSettings.CentralManagement)
+            Return mBFunctions.GetMenuOrder(profile)
+        End Function
+
+        ''' <summary>
+        ''' Removes the cached functions.
+        ''' </summary>
+        Public Sub RemoveCachedFunctions()
+            Dim mCacheName As String = String.Empty
+            Dim mSecurityProfiles As Collection(Of MSecurityEntityProfile) = SecurityEntityUtility.GetProfiles()
+            For Each mProfile In mSecurityProfiles
+                mCacheName = mProfile.Id.ToString() + "_Functions"
+                CacheController.RemoveFromCache(mCacheName)
+            Next
+
+        End Sub
+
+        ''' <summary>
+        ''' Returns a datatable of the search data
+        ''' </summary>
+        ''' <param name="searchCriteria">MSearchCriteria</param>
+        ''' <returns>NULL/Nothing if no records are returned.</returns>
+        ''' <remarks></remarks>
+        Public Function Search(ByVal searchCriteria As MSearchCriteria) As DataTable
+            Try
+                Dim mBFunctions As BFunctions = New BFunctions(SecurityEntityUtility.GetCurrentProfile(), ConfigSettings.CentralManagement)
+                Return mBFunctions.Search(searchCriteria)
+            Catch ex As IndexOutOfRangeException
+                'no data is not a problem
+                Return Nothing
+            End Try
+        End Function
+
+        ''' <summary>
+        ''' Saves the specified profile.
+        ''' </summary>
+        ''' <param name="profile">The profile.</param>
+        ''' <param name="saveGroups">if set to <c>true</c> [save groups].</param>
+        ''' <param name="saveRoles">if set to <c>true</c> [save roles].</param>
+        Public Sub Save(ByVal profile As MFunctionProfile, ByVal saveGroups As Boolean, ByVal saveRoles As Boolean)
+            Try
+                Dim mBFunctions As BFunctions = New BFunctions(SecurityEntityUtility.GetCurrentProfile(), ConfigSettings.CentralManagement)
+                mBFunctions.Save(profile, saveGroups, saveRoles)
+            Catch ex As Exception
+                Dim mLog As Logger = Logger.Instance()
+                mLog.Error(ex)
+            Finally
+                RemoveCachedFunctions()
+            End Try
+        End Sub
+
+        ''' <summary>
+        ''' Deletes the specified function seq id.
+        ''' </summary>
+        ''' <param name="functionSeqId">The function seq id.</param>
+        Public Sub Delete(ByVal functionSeqId As Integer)
+            Try
+                Dim mBFunctions As BFunctions = New BFunctions(SecurityEntityUtility.GetCurrentProfile(), ConfigSettings.CentralManagement)
+                mBFunctions.Delete(functionSeqId)
+            Catch ex As Exception
+                Dim mLog As Logger = Logger.Instance()
+                mLog.Error(ex)
+            Finally
+                RemoveCachedFunctions()
+            End Try
+        End Sub
+
+        ''' <summary>
+        ''' Sets the current profile.
+        ''' </summary>
+        ''' <param name="profile">The profile.</param>
+        Public Sub SetCurrentProfile(ByVal profile As MFunctionProfile)
+            HttpContext.Current.Items(m_FunctionProfileInfoName) = profile
+        End Sub
+    End Module
+End Namespace
