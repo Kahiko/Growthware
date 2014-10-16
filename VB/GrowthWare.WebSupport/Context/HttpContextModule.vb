a771a880ee353f3c15f5a1e3b509d357dbbaa9a2
kahiko <Kahiko@users.noreply.github.com>
2014-10-16 10:46:34 +0000

    Morning checkin

--- VB/GrowthWare.WebSupport/Context/HttpContextModule.vb
+++ VB/GrowthWare.WebSupport/Context/HttpContextModule.vb
@@ -106,7 +106,7 @@ Namespace Context
                         mLog.Debug("hashCode: " + mHashCode)
                         mLog.Debug("Processing action: " + mAction)
 
-                        If Not mFunctionProfile.Source().ToUpper(CultureInfo.InvariantCulture).Contains("MENUS") And Not (mAction.ToUpper(CultureInfo.InvariantCulture) = "LOGOFF" Or mAction.ToUpper(CultureInfo.InvariantCulture) = "LOGON") Then
+                        If Not mFunctionProfile.Source.ToUpper(CultureInfo.InvariantCulture).Contains("MENUS") And Not (mAction.ToUpper(CultureInfo.InvariantCulture) = "LOGOFF" Or mAction.ToUpper(CultureInfo.InvariantCulture) = "LOGON") Then
                             Dim mAccountProfile As MAccountProfile = AccountUtility.CurrentProfile()
                             If Not mAccountProfile.Status = DirectCast(SystemStatus.ChangePassword, Integer) Then
                                 mLog.Debug("Processing for account " + mAccountProfile.Account)
