174e42147312204b781c1f50f364e503b06a8d1d
kahiko <Kahiko@users.noreply.github.com>
2014-12-31 19:00:04 +0000

    added modernizr

--- CS/GrowthWare.WebApplication/App_Start/BundleConfig.cs
+++ CS/GrowthWare.WebApplication/App_Start/BundleConfig.cs
@@ -20,6 +20,13 @@ namespace GrowthWare.WebApplication
             )
         );
 
+        // Use the development version of Modernizr to develop with and learn from. Then, when you're
+        // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
+        bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
+                    "~/Scripts/modernizr-*"
+            )
+        );
+
         bundles.Add(
             new ScriptBundle("~/bundles/jqueryUI").Include(
             "~/" + ConfigSettings.AppName + "Public/Scripts/jquery-ui.js"
