20569f46c7b71b874101d006ae4d09091faf58cc
kahiko <Kahiko@users.noreply.github.com>
2015-01-17 00:04:02 +0000

    fixed skinning issue

--- VB/GrowthWare.WebApplication/Index.aspx.vb
+++ VB/GrowthWare.WebApplication/Index.aspx.vb
@@ -8,11 +8,11 @@ Public Class Index
 
     Protected Shadows Sub Page_PreInit(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.PreInit
         Dim mSecProfile As MSecurityEntityProfile = SecurityEntityUtility.CurrentProfile()
-        Dim mMasterPage As String = "~/Public/Skins/" + mSecProfile.Skin + "/Default.Master"
+        Dim mMasterPage As String = "Public/Skins/" + mSecProfile.Skin + "/Default.master"
         Dim fileName As String = String.Empty
-        fileName = HttpContext.Current.Server.MapPath("~\\") + mMasterPage
+        fileName = HttpContext.Current.Server.MapPath("~\") + mMasterPage
         If Not File.Exists(fileName) Then
-            mMasterPage = "~/Public/Skins/Default/Default.Master"
+            mMasterPage = "Public/Skins/Default/Default.Master"
         End If
         Me.MasterPageFile = mMasterPage
         MyBase.Page_PreInit(sender, e)
