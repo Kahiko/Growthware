25fda086d7640dc9afd197fa9bdbf4c0d42ac650
kahiko <Kahiko@users.noreply.github.com>
2015-02-13 22:01:14 +0000

    Added Set Log Level

---
+++ CS/GrowthWare.WebApplication/Functions/System/Administration/Logs/SetLogLevel.aspx.cs
@@ -0,0 +1,44 @@
+﻿using GrowthWare.Framework.Common;
+using GrowthWare.Framework.Model.Enumerations;
+using GrowthWare.WebSupport.Base;
+using System;
+using System.Web.Services;
+
+namespace GrowthWare.WebApplication.Functions.System.Administration.Logs
+{
+    public partial class SetLogLevel : BaseWebpage
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            Logger mLog = Logger.Instance();
+            dropLogLevel.SelectedIndex = mLog.CurrentLogLevel;
+        }
+
+        [WebMethod(CacheDuration = 0, EnableSession = false)]
+        public static void InvokeSetLogLevel(int logLevel)
+        {
+            Logger mLog = Logger.Instance();
+            switch (logLevel)
+            {
+                case 0:
+                    mLog.SetThreshold(LogPriority.Debug);
+                    break;
+                case 1:
+                    mLog.SetThreshold(LogPriority.Info);
+                    break;
+                case 2:
+                    mLog.SetThreshold(LogPriority.Warn);
+                    break;
+                case 3:
+                    mLog.SetThreshold(LogPriority.Error);
+                    break;
+                case 4:
+                    mLog.SetThreshold(LogPriority.Fatal);
+                    break;
+                default:
+                    mLog.SetThreshold(LogPriority.Error);
+                    break;
+            }
+        }
+    }
+}
