ad004ee6c7868d5e56a8048bc62a1274710542a9
kahiko <Kahiko@users.noreply.github.com>
2015-03-23 17:18:50 +0000

    tightening up security

--- VB/GrowthWare.WebApplication/Controllers/SecurityEntitiesController.vb
+++ VB/GrowthWare.WebApplication/Controllers/SecurityEntitiesController.vb
@@ -18,7 +18,7 @@ Public Class SecurityEntitiesController
             If Not HttpContext.Current.Items("EditId") Is Nothing Then
                 Dim mEditId = Integer.Parse(HttpContext.Current.Items("EditId").ToString())
                 If mEditId = uiProfile.Id Then
-                    Dim mSecurityInfo As MSecurityInfo = DirectCast(HttpContext.Current.Items("SecurityInfo"), MSecurityInfo)
+                    Dim mSecurityInfo As MSecurityInfo = New MSecurityInfo(FunctionUtility.GetProfile(ConfigSettings.GetAppSettingValue("Actions_EditSecurityEntity", True)), AccountUtility.CurrentProfile())
                     If Not mSecurityInfo Is Nothing Then
                         If mEditId <> -1 Then
                             If mSecurityInfo.MayEdit Then
