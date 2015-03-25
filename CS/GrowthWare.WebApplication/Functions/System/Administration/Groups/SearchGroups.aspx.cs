b204a7f63fda8412dcc4caa6a618d5409f0375d7
kahiko <Kahiko@users.noreply.github.com>
2015-03-25 13:04:48 +0000

    Started adding group management

---
+++ CS/GrowthWare.WebApplication/Functions/System/Administration/Groups/SearchGroups.aspx.cs
@@ -0,0 +1,28 @@
+﻿using GrowthWare.Framework.Model.Profiles;
+using GrowthWare.WebSupport;
+using GrowthWare.WebSupport.Base;
+using GrowthWare.WebSupport.Utilities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace GrowthWare.WebApplication.Functions.System.Administration.Groups
+{
+    public partial class SearchGroups : BaseWebpage
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            string mAction = GWWebHelper.GetQueryValue(Request, "action");
+            if (!String.IsNullOrEmpty(mAction))
+            {
+                MAccountProfile mAccountProfile = AccountUtility.CurrentProfile();
+                MFunctionProfile mFunctionProfile = FunctionUtility.GetProfile(mAction);
+                MSecurityInfo mSecurityInfo = new MSecurityInfo(mFunctionProfile, mAccountProfile);
+                SearchControl.ShowAddLink = mSecurityInfo.MayAdd;
+            }
+        }
+    }
+}
