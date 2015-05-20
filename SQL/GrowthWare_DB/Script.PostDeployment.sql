2276ff04224acd7751831def6b554246a96842c9
kahiko <Kahiko@users.noreply.github.com>
2015-05-20 21:14:48 +0000

    Removed Web configuration

--- SQL/GrowthWare_DB/Script.PostDeployment.sql
+++ SQL/GrowthWare_DB/Script.PostDeployment.sql
@@ -752,14 +752,14 @@ SET @V_ParentID = (SELECT Function_SeqID FROM ZGWSecurity.Functions WHERE [Actio
 exec ZGWSecurity.Set_Function -1,'Select A Security Entity','Select A Security Entity',@V_Function_Type_SeqID,'Functions/System/SecurityEntities/SelectSecurityEntity.aspx','',@V_EnableViewStateFalse,@V_EnableNotificationsFalse,@V_Redirect_On_Timeout,@V_IsNavTrue,@V_LinkBehaviorInternal,@V_NO_UIFalse,@V_NAV_TYPE_Vertical,@V_MyAction,@V_META_KEY_WORDS,@V_ParentID,'Used to select a Security Entity.', @V_SystemID, @V_Debug
 set @V_FunctionID = (select Function_SeqID from ZGWSecurity.Functions where action=@V_MyAction)
 exec ZGWSecurity.Set_Function_Roles @V_FunctionID,1,'Authenticated',@V_ViewPermission,@V_SystemID, @V_Debug
--- Web configuration
-print 'Adding Web configuration'
-SET @V_Function_Type_SeqID = (SELECT Function_Type_SeqID FROM ZGWSecurity.Function_Types WHERE [Name] = 'Module')
-set @V_MyAction = 'WebConfig'
-SET @V_ParentID = (SELECT Function_SeqID FROM ZGWSecurity.Functions WHERE [Action] = 'SystemAdministration')
-exec ZGWSecurity.Set_Function -1,'Web Config','Web Config',@V_Function_Type_SeqID,'Functions/System/Administration/Configuration/AddEditWebConfig.aspx','',@V_EnableViewStateFalse,@V_EnableNotificationsFalse,@V_Redirect_On_Timeout,@V_IsNavTrue,@V_LinkBehaviorInternal,@V_NO_UIFalse,@V_NAV_TYPE_Hierarchical,@V_MyAction,@V_META_KEY_WORDS,@V_ParentID,'Adds or edits web.config file settings.', @V_SystemID, @V_Debug
-set @V_FunctionID = (select Function_SeqID from ZGWSecurity.Functions where action=@V_MyAction)
-exec ZGWSecurity.Set_Function_Roles @V_FunctionID,1,'Developer',@V_ViewPermission,@V_SystemID, @V_Debug
+---- Web configuration
+--print 'Adding Web configuration'
+--SET @V_Function_Type_SeqID = (SELECT Function_Type_SeqID FROM ZGWSecurity.Function_Types WHERE [Name] = 'Module')
+--set @V_MyAction = 'WebConfig'
+--SET @V_ParentID = (SELECT Function_SeqID FROM ZGWSecurity.Functions WHERE [Action] = 'SystemAdministration')
+--exec ZGWSecurity.Set_Function -1,'Web Config','Web Config',@V_Function_Type_SeqID,'Functions/System/Administration/Configuration/AddEditWebConfig.aspx','',@V_EnableViewStateFalse,@V_EnableNotificationsFalse,@V_Redirect_On_Timeout,@V_IsNavTrue,@V_LinkBehaviorInternal,@V_NO_UIFalse,@V_NAV_TYPE_Hierarchical,@V_MyAction,@V_META_KEY_WORDS,@V_ParentID,'Adds or edits web.config file settings.', @V_SystemID, @V_Debug
+--set @V_FunctionID = (select Function_SeqID from ZGWSecurity.Functions where action=@V_MyAction)
+--exec ZGWSecurity.Set_Function_Roles @V_FunctionID,1,'Developer',@V_ViewPermission,@V_SystemID, @V_Debug
 -- Line Count
 print 'Adding Line Count'
 SET @V_Function_Type_SeqID = (SELECT Function_Type_SeqID FROM ZGWSecurity.Function_Types WHERE [Name] = 'Module')
