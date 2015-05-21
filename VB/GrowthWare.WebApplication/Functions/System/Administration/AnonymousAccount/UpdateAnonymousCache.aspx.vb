11cce0dcc8ab0c9c7baf33313b0b220f74b67812
kahiko <Kahiko@users.noreply.github.com>
2015-05-21 14:51:31 +0000

    Implemented UpdateAnonymousCache

---
+++ VB/GrowthWare.WebApplication/Functions/System/Administration/AnonymousAccount/UpdateAnonymousCache.aspx.vb
@@ -0,0 +1,17 @@
+﻿Imports GrowthWare.Framework.Model.Enumerations
+Imports GrowthWare.WebSupport
+Imports GrowthWare.WebSupport.Utilities
+
+Public Class UpdateAnonymousCache
+    Inherits System.Web.UI.Page
+
+    Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
+        CacheController.RemoveFromCache(AccountUtility.AnonymousAccountProfile)
+        CacheController.RemoveFromCache("AnonymousClientChoicesState")
+        Dim enumValues As Array = System.[Enum].GetValues(GetType(MenuType))
+        For Each resource As MenuType In enumValues
+            CacheController.RemoveFromCache(resource.ToString() + "Anonymous")
+        Next
+    End Sub
+
+End Class
