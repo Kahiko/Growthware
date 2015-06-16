bdc3481ef486ea6d87fdde9bbe8628e4b4d7983d
kahiko <Kahiko@users.noreply.github.com>
2015-06-16 17:18:26 +0000

    added setting formStyles

--- VB/GrowthWare.WebApplication/Content/Skins/Default/Default.Master.vb
+++ VB/GrowthWare.WebApplication/Content/Skins/Default/Default.Master.vb
@@ -1,8 +1,14 @@
-﻿Public Class _Default2
+﻿Imports GrowthWare.Framework.Model.Profiles
+Imports GrowthWare.WebSupport.Utilities
+Imports GrowthWare.WebSupport
+
+Public Class _Default2
     Inherits System.Web.UI.MasterPage
 
     Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
-
+        Dim mSecurityEntityID As Integer = Integer.Parse(ClientChoicesUtility.GetClientChoicesState(AccountUtility.CurrentProfile.Account).Item(MClientChoices.SecurityEntityId).ToString())
+        Dim mSecurityEntityProfile As MSecurityEntityProfile = SecurityEntityUtility.GetProfile(mSecurityEntityID)
+        formStyles.Attributes("href") = GWWebHelper.RootSite + "/Content/SiteStyles/" + mSecurityEntityProfile.Style + ".css"
     End Sub
 
 End Class
