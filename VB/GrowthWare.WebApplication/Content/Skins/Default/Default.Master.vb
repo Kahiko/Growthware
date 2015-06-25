deb46ad94ebd742646d7c64c6b291dc09a80d736
kahiko <Kahiko@users.noreply.github.com>
2015-06-25 19:36:53 +0000

    Fix null exception when Account is known to iis but not in database.

--- VB/GrowthWare.WebApplication/Content/Skins/Default/Default.Master.vb
+++ VB/GrowthWare.WebApplication/Content/Skins/Default/Default.Master.vb
@@ -6,7 +6,11 @@ Public Class _Default2
     Inherits System.Web.UI.MasterPage
 
     Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
-        Dim mSecurityEntityID As Integer = Integer.Parse(ClientChoicesUtility.GetClientChoicesState(AccountUtility.CurrentProfile.Account).Item(MClientChoices.SecurityEntityId).ToString())
+        Dim mSecurityEntityID As Integer = SecurityEntityUtility.DefaultProfile.Id
+        Dim mAccountProfile As MAccountProfile = AccountUtility.CurrentProfile()
+        If Not mAccountProfile Is Nothing Then
+            mSecurityEntityID = Integer.Parse(ClientChoicesUtility.GetClientChoicesState(AccountUtility.CurrentProfile().Account)(MClientChoices.SecurityEntityId).ToString())
+        End If
         Dim mSecurityEntityProfile As MSecurityEntityProfile = SecurityEntityUtility.GetProfile(mSecurityEntityID)
         formStyles.Attributes("href") = GWWebHelper.RootSite + "/Content/FormStyles/" + mSecurityEntityProfile.Style + ".css"
     End Sub
