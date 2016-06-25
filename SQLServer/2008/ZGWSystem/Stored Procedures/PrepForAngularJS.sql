de8f13e79744c37b715a7ba50b277d477d1eac15
kahiko <maui.regan@gmail.com>
2016-06-25 03:27:36 -0400

    Needed in order to support both .ASPX and AngularJS frontend implementations

---
+++ SQLServer/2008/ZGWSystem/Stored Procedures/PrepForAngularJS.sql
@@ -0,0 +1,42 @@
+﻿/*
+Usage:
+	DECLARE 
+		@P_UseAngular BIT = 0
+
+	exec ZGWSystem.Set_DataBase_Information
+		@P_Database_Information_SeqID,
+		@P_Version,
+		@P_Enable_Inheritance,
+		@P_Added_Updated_By,
+		@P_Primary_Key,
+		@P_Debug
+*/
+-- =============================================
+-- Author:		Michael Regan
+-- Create date: 06/25/2016
+-- Description:	Setups up data for the use of AngularJS for the frontend
+--	or for .APSX
+-- =============================================
+CREATE PROCEDURE [ZGWSystem].[PrepForAngularJS]
+	@P_UseAngular BIT = 1
+AS
+	if @P_UseAngular = 1
+		BEGIN
+			UPDATE [ZGWSecurity].[Functions] SET [Controller] = 'SearchController', [Source] = 'Functions/System/Search/SearchPage.aspx'
+			WHERE [Action] like 'search%'
+			UPDATE [ZGWSecurity].[Functions] SET [Controller] = 'SearchController', [Source] = 'Functions/System/Search/SearchPage.aspx'
+			WHERE [Action] = 'Manage_Groups'
+		END
+	ELSE
+		BEGIN
+			UPDATE [ZGWSecurity].[Functions] SET [Source] = 'Functions/System/Administration/Accounts/SearchAccounts.aspx' WHERE [Action] = 'Search_Accounts'
+			UPDATE [ZGWSecurity].[Functions] SET [Source] = 'Functions/System/Administration/Functions/SearchFunctions.aspx' WHERE [Action] = 'Search_Functions'
+			UPDATE [ZGWSecurity].[Functions] SET [Source] = 'Functions/System/Administration/Messages/SearchMessages.aspx' WHERE [Action] = 'Search_Messages'
+			UPDATE [ZGWSecurity].[Functions] SET [Source] = 'Functions/System/Administration/NVP/SearchNVP.aspx' WHERE [Action] = 'Search_Name_Value_Pairs'
+			UPDATE [ZGWSecurity].[Functions] SET [Source] = 'Functions/System/Administration/Roles/SearchRoles.aspx' WHERE [Action] = 'Search_Roles'
+			UPDATE [ZGWSecurity].[Functions] SET [Source] = 'Functions/System/Administration/SecurityEntities/SearchSecurityEntities.aspx' WHERE [Action] = 'Search_Security_Entities'
+			UPDATE [ZGWSecurity].[Functions] SET [Source] = 'Functions/System/Administration/States/SearchStates.aspx' WHERE [Action] = 'Search_States'
+			UPDATE [ZGWSecurity].[Functions] SET [Source] = 'Functions/System/Administration/Groups/SearchGroups.aspx' WHERE [Action] = 'Manage_Groups'
+		END
+	--END IF
+RETURN 0
