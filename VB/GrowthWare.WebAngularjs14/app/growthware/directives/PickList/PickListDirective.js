1a07a8967d8eb861388eddb7b2d07b1b03bbbbc0
kahiko <maui.regan@gmail.com>
2016-07-25 06:05:02 -0400

    natural sort should now work with multi dim array

--- VB/GrowthWare.WebAngularjs14/app/growthware/directives/PickList/PickListDirective.js
+++ VB/GrowthWare.WebAngularjs14/app/growthware/directives/PickList/PickListDirective.js
@@ -123,8 +123,8 @@
                             }
                         }
                         if ($scope.sortOnChange.toLowerCase() == "true") {
-                            arrFbox.sort(naturalSort);
-                            arrTbox.sort(naturalSort);
+                            arrFbox.sort(GW.Common.naturalSort());
+                            arrTbox.sort(GW.Common.naturalSort());
                         }
                         objFromBox.length = 0;
                         objToBox.length = 0;
@@ -172,50 +172,7 @@
                     }
                 };
 
-                /********** Natural Sorting *****************/
-                /*
-                * Natural Sort algorithm for Javascript - Version 0.6 - Released under MIT license
-                * Author: Jim Palmer (based on chunking idea from Dave Koelle)
-                * Contributors: Mike Grier (mgrier.com), Clint Priest, Kyle Adams, guillermo
-                */
-                function naturalSort(a, b) {
-                    var re = /(^-?[0-9]+(\.?[0-9]*)[df]?e?[0-9]?$|^0x[0-9a-f]+$|[0-9]+)/gi,
-                        sre = /(^[ ]*|[ ]*$)/g,
-                        dre = /(^([\w ]+,?[\w ]+)?[\w ]+,?[\w ]+\d+:\d+(:\d+)?[\w ]?|^\d{1,4}[\/\-]\d{1,4}[\/\-]\d{1,4}|^\w+, \w+ \d+, \d{4})/,
-                        hre = /^0x[0-9a-f]+$/i,
-                        ore = /^0/,
-                    // convert all to strings and trim()
-                        x = a.toString().replace(sre, '') || '',
-                        y = b.toString().replace(sre, '') || '',
-                    // chunk/tokenize
-                        xN = x.replace(re, '\0$1\0').replace(/\0$/, '').replace(/^\0/, '').split('\0'),
-                        yN = y.replace(re, '\0$1\0').replace(/\0$/, '').replace(/^\0/, '').split('\0'),
-                    // numeric, hex or date detection
-                        xD = parseInt(x.match(hre)) || (xN.length != 1 && x.match(dre) && Date.parse(x)),
-                        yD = parseInt(y.match(hre)) || xD && y.match(dre) && Date.parse(y) || null;
-                    // first try and sort Hex codes or Dates
-                    if (yD)
-                        if (xD < yD) return -1;
-                        else if (xD > yD) return 1;
-                    // natural sorting through split numeric strings and default strings
-                    for (var cLoc = 0, numS = Math.max(xN.length, yN.length) ; cLoc < numS; cLoc++) {
-                        // find floats not starting with '0', string or 0 if not defined (Clint Priest)
-                        var oFxNcL = !(xN[cLoc] || '').match(ore) && parseFloat(xN[cLoc]) || xN[cLoc] || 0;
-                        var oFyNcL = !(yN[cLoc] || '').match(ore) && parseFloat(yN[cLoc]) || yN[cLoc] || 0;
-                        // handle numeric vs string comparison - number < string - (Kyle Adams)
-                        if (isNaN(oFxNcL) !== isNaN(oFyNcL)) return (isNaN(oFxNcL)) ? 1 : -1;
-                            // rely on string comparison if different types - i.e. '02' < 2 != '02' < '2'
-                        else if (typeof oFxNcL !== typeof oFyNcL) {
-                            oFxNcL += '';
-                            oFyNcL += '';
-                        }
-                        if (oFxNcL < oFyNcL) return -1;
-                        if (oFxNcL > oFyNcL) return 1;
-                    }
-                    return 0;
-                }
-                /********** End Natural Sorting *****************/
-
+ 
                 angular.element(document).ready(function () {
                     // All avalible items will include the selected ones as well
                     // so wee need to remove the selected items from the all itmes.
