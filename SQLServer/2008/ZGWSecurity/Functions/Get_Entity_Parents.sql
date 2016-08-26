54b078014df7d6b91101d128a1940a280e61f858
kahiko <maui.regan@gmail.com>
2016-08-26 18:16:01 -0400

    Removed cursor now uses CTE with MAXRECURSION 32767

--- SQLServer/2008/ZGWSecurity/Functions/Get_Entity_Parents.sql
+++ SQLServer/2008/ZGWSecurity/Functions/Get_Entity_Parents.sql
@@ -10,11 +10,7 @@ Usage:
 -- Description: Returns all "Parent" Secutriy_Entity_SeqID's
 --	Given the Security_Entity_SeqID 
 -- Note:
---	Works through recursion and should be changed
---	to use CTE, however due to constratins i'm leaving
---	this close to the orginial and will attempt
---	to change this later after the bulk of development
---	has been completed.  (if i remember)
+--	Works through recursion of MAXRECURSION 32767
 -- =============================================
 CREATE FUNCTION [ZGWSecurity].[Get_Entity_Parents]
 (
@@ -30,36 +26,33 @@ AS
 BEGIN
 	IF (ZGWSystem.Inheritance_Enabled()=1)
 		BEGIN
-			IF (@P_IncludeParent=1)
-				BEGIN
-					INSERT INTO @retParents
-						SELECT Security_Entity_SeqID, Parent_Security_Entity_SeqID
-						FROM ZGWSecurity.Security_Entities WHERE Security_Entity_SeqID=@P_Security_Entity_SeqID
-				END
-			-- END IF
-			IF (@P_Security_Entity_SeqID=0 or @P_Security_Entity_SeqID=ZGWSecurity.Get_Default_Entity_ID()) RETURN
-			DECLARE @Report_ID int, @Report_ParentID int
-			DECLARE RetrieveReports CURSOR STATIC LOCAL FOR
-				SELECT  Security_Entity_SeqID,  Parent_Security_Entity_SeqID
-				FROM ZGWSecurity.Security_Entities WHERE Security_Entity_SeqID=@P_Security_Entity_SeqID
-			OPEN RetrieveReports
-				FETCH NEXT FROM RetrieveReports
-				INTO @Report_ID,  @Report_ParentID
-				WHILE (@@FETCH_STATUS = 0)
-					BEGIN
-						INSERT INTO @retParents SELECT * FROM ZGWSecurity.Get_Entity_Parents(1, @Report_ParentID)
-						FETCH NEXT FROM RetrieveReports INTO @Report_ID, @Report_ParentID
-					END
-			CLOSE RetrieveReports
-			DEALLOCATE RetrieveReports
-			RETURN
+			;WITH tblParent(Security_Entity_SeqID, Parent_Security_Entity_SeqID) AS
+			(
+				SELECT Security_Entity_SeqID, Parent_Security_Entity_SeqID
+					FROM ZGWSecurity.Security_Entities WITH(NOLOCK) WHERE Security_Entity_SeqID = @P_Security_Entity_SeqID
+				UNION ALL
+				SELECT 
+					SE.Security_Entity_SeqID, SE.Parent_Security_Entity_SeqID
+				FROM 
+					ZGWSecurity.Security_Entities SE WITH(NOLOCK) 
+					INNER JOIN tblParent
+						ON SE.Security_Entity_SeqID = tblParent.Parent_Security_Entity_SeqID
+			)
+			INSERT INTO @retParents(Security_Entity_SeqID)
+			SELECT 
+				tblParent.Security_Entity_SeqID
+			FROM  
+				tblParent
+			WHERE Security_Entity_SeqID <> @P_Security_Entity_SeqID
+			OPTION(MAXRECURSION 32767)
+			IF (@P_IncludeParent=1) INSERT INTO @retParents(Security_Entity_SeqID)VALUES(@P_Security_Entity_SeqID);
 		END
 	ELSE
 		BEGIN
 			INSERT INTO @retParents VALUES(ZGWSecurity.Get_Default_Entity_ID(),1)
-			INSERT INTO @retParents VALUES(@P_Security_Entity_SeqID,1)
+			IF (@P_IncludeParent=1) INSERT INTO @retParents VALUES(@P_Security_Entity_SeqID,1)
 		END
 	-- END IF
-RETURN
+	RETURN
 
 END
