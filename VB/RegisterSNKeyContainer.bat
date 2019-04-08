98cb8af751caaa54121aee355f97e1dc3cfd110d
Michael <Michael@DESKTOP-3MH1TRV>
2019-04-08 06:27:57 -0400

    Changes for being on a new machine ... need a better way.

--- VB/RegisterSNKeyContainer.bat
+++ VB/RegisterSNKeyContainer.bat
@@ -1,4 +1,4 @@
 rem password is haaheo
-sn -i D:\Development\GrowthWare\2013\VB\GrowthWare.Framework\GrowthWare.Framework.pfx VS_KEY_59637F8F348CFB06
-sn -i D:\Development\GrowthWare\2013\VB\GrowthWare.Framework.BusinessData\GrowthWare.Framework.BusinessData.pfx VS_KEY_E969778E144FCB84
-sn -i D:\Development\GrowthWare\2013\VB\GrowthWare.WebSupport\GrowthWare.WebSupport.pfx VS_KEY_81D92A06C864C701
+sn -i D:\Development\GrowthWare\VB\GrowthWare.Framework\GrowthWare.Framework.pfx VS_KEY_9D4335265B64D3F9
+sn -i D:\Development\GrowthWare\VB\GrowthWare.Framework.BusinessData\GrowthWare.Framework.BusinessData.pfx VS_KEY_2D493D277BA7E37B
+sn -i D:\Development\GrowthWare\VB\GrowthWare.WebSupport\GrowthWare.WebSupport.pfx VS_KEY_45F960AFA78CEFFE
