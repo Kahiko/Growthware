c422787292363332a4f1fc0c7fbc83a94683c0d7
Michael <Michael@DESKTOP-3MH1TRV>
2019-04-20 14:14:56 -0400

    Moved IsWebApiRequest to  GWWebHelper

--- VB/GrowthWare.WebAngularjs14/Global.asax.vb
+++ VB/GrowthWare.WebAngularjs14/Global.asax.vb
@@ -1,5 +1,6 @@
 ﻿Imports System.Web.Http
 Imports System.Web.Optimization
+Imports GrowthWare.WebSupport
 
 Public Class WebApiApplication
     Inherits System.Web.HttpApplication
@@ -10,14 +11,6 @@ Public Class WebApiApplication
     End Sub
 
     Protected Sub Application_PostAuthorizeRequest()
-        If IsWebApiRequest() Then HttpContext.Current.SetSessionStateBehavior(SessionStateBehavior.Required)
+        If GWWebHelper.IsWebApiRequest Then HttpContext.Current.SetSessionStateBehavior(SessionStateBehavior.Required)
     End Sub
-
-    Private Shared Function IsWebApiRequest() As Boolean
-        Dim mRetVal As Boolean = False
-        If HttpContext.Current.Request.AppRelativeCurrentExecutionFilePath.Contains("/gw/") Or HttpContext.Current.Request.AppRelativeCurrentExecutionFilePath.Contains("/api/") Then
-            mRetVal = True
-        End If
-        Return mRetVal
-    End Function
 End Class
