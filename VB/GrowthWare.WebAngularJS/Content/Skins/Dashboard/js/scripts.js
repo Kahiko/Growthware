77d2732b5d554b2d7c6b7324996d01dd428b0d91
Kahiko <Michael.Regan@verizon.net>
2019-06-07 06:59:20 -0400

    can now access menu sub items but don't like the way it's working ... needs more work

--- VB/GrowthWare.WebAngularJS/Content/Skins/Dashboard/js/scripts.js
+++ VB/GrowthWare.WebAngularJS/Content/Skins/Dashboard/js/scripts.js
@@ -1,4 +1,23 @@
 function initializeJS() {
+    $(document).on("click", ".sub-menu", function () {
+        var last = jQuery('.sub-menu.open', jQuery('#sidebar'));
+        jQuery('.menu-arrow').removeClass('arrow_carrot-right');
+        jQuery('.sub', last).slideUp(200);
+        var sub = jQuery(this.children).next();
+        if (sub.is(":visible")) {
+            jQuery('.menu-arrow').addClass('arrow_carrot-right');
+            sub.slideUp(200);
+        } else {
+            jQuery('.menu-arrow').addClass('arrow_carrot-down');
+            sub.slideDown(200);
+        }
+        var o = (jQuery(this).offset());
+        diff = 200 - o.top;
+        if (diff > 0)
+            jQuery("#sidebar").scrollTo("-=" + Math.abs(diff), 500);
+        else
+            jQuery("#sidebar").scrollTo("+=" + Math.abs(diff), 500);
+    });
 
     //tool tips
     jQuery('.tooltips').tooltip();
@@ -16,23 +35,7 @@ function initializeJS() {
     
     //sidebar dropdown menu
     jQuery('#sidebar .sub-menu > a').click(function () {
-        var last = jQuery('.sub-menu.open', jQuery('#sidebar'));        
-        jQuery('.menu-arrow').removeClass('arrow_carrot-right');
-        jQuery('.sub', last).slideUp(200);
-        var sub = jQuery(this).next();
-        if (sub.is(":visible")) {
-            jQuery('.menu-arrow').addClass('arrow_carrot-right');            
-            sub.slideUp(200);
-        } else {
-            jQuery('.menu-arrow').addClass('arrow_carrot-down');            
-            sub.slideDown(200);
-        }
-        var o = (jQuery(this).offset());
-        diff = 200 - o.top;
-        if(diff>0)
-            jQuery("#sidebar").scrollTo("-="+Math.abs(diff),500);
-        else
-            jQuery("#sidebar").scrollTo("+="+Math.abs(diff),500);
+
     });
 
     // sidebar menu toggle
