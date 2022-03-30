4b0967eabb064724b6f1d05950d6523a8d6a33fb
michael_regan <michael.regan@azimuth.com>
2022-03-30 06:25:39 -0400

    Moved Routing into separate file

--- Core/GrowthWare.Web.Angular/ClientApp/src/app/app-routing.module.ts
+++ Core/GrowthWare.Web.Angular/ClientApp/src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+// Application Components
+import { AppComponent } from 'src/app/layout/app.component';
+import { HomeComponent } from './home/home.component';
+import { CounterComponent } from './counter/counter.component';
+import { FetchDataComponent } from './fetch-data/fetch-data.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent, pathMatch: 'full' },
+  { path: 'counter', component: CounterComponent },
+  { path: 'fetch-data', component: FetchDataComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- Core/GrowthWare.Web.Angular/ClientApp/src/app/app.module.ts
+++ Core/GrowthWare.Web.Angular/ClientApp/src/app/app.module.ts
@@ -7,7 +7,9 @@ import { RouterModule } from '@angular/router';
 // Thrid party UI
 import { AngularMaterialModules } from 'src/app/app.angular.material.module';   // single line to import all Angular Material modules
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-// Application Services
+// Application Services/Modules
+import { AccountService } from './services/account.service';
+import { AppRoutingModule } from './app-routing.module';
 import { ConfigurationService } from 'src/app/services/configuration.service';
 import { LogService } from 'src/app/services/logservice.service';
 // Application UI
@@ -17,7 +19,6 @@ import { AppComponent } from 'src/app/layout/app.component';
 import { HomeComponent } from './home/home.component';
 import { CounterComponent } from './counter/counter.component';
 import { FetchDataComponent } from './fetch-data/fetch-data.component';
-import { AccountService } from './services/account.service';
 
 @NgModule({
   declarations: [
@@ -28,16 +29,12 @@ import { AccountService } from './services/account.service';
     FetchDataComponent
   ],
   imports: [
+    AppRoutingModule,
     BrowserModule.withServerTransition({ appId: 'ng-cli-universal' }),
     BrowserAnimationsModule,
     HttpClientModule,
     FormsModule,
     AngularMaterialModules,
-    RouterModule.forRoot([
-      { path: '', component: HomeComponent, pathMatch: 'full' },
-      { path: 'counter', component: CounterComponent },
-      { path: 'fetch-data', component: FetchDataComponent },
-    ]),
   ],
   providers: [
     AccountService,
