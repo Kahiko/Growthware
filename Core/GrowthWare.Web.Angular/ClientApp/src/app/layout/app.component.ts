4f21ce96433ef9f75a7f84be22b01b8e3a01d170
michael_regan <michael.regan@azimuth.com>
2022-04-08 17:27:17 -0400

    Fixed build errors

--- Core/GrowthWare.Web.Angular/ClientApp/src/app/layout/app.component.ts
+++ Core/GrowthWare.Web.Angular/ClientApp/src/app/layout/app.component.ts
@@ -1,11 +1,9 @@
 import { Component } from '@angular/core';
-import { onMainContentChange } from 'src/animations/GWAnimations';
 
 @Component({
   selector: 'app-root',
   styleUrls: ['./app.component.scss'],
-  templateUrl: './app.component.html',
-  animations: [ onMainContentChange ]
+  templateUrl: './app.component.html'
 })
 export class AppComponent {
   public onSideNavChange: boolean = false;
