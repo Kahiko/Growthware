76d4f592b760cbf24dd3e1ac49e8692fd2c74a52
michael_regan <michael.regan@azimuth.com>
2022-05-02 00:17:55 -0400

    deleted the GWLibComponent

--- Core/GrowthWare.Web.Angular/Angular/projects/gw-lib/src/lib/lib.module.ts
+++ Core/GrowthWare.Web.Angular/Angular/projects/gw-lib/src/lib/lib.module.ts
@@ -1,16 +1,14 @@
 import { NgModule } from '@angular/core';
-import { GWLibComponent } from './features/gw-lib.component';
 import { DynamicTableComponent } from './features/dynamic-table/dynamic-table.component';
 
 @NgModule({
   declarations: [
-    GWLibComponent,
     DynamicTableComponent,
   ],
   imports: [
   ],
   exports: [
-    GWLibComponent
+    DynamicTableComponent
   ]
 })
 export class GWLibModule { }
--- Core/GrowthWare.Web.Angular/Angular/projects/gw-lib/src/public-api.ts
+++ Core/GrowthWare.Web.Angular/Angular/projects/gw-lib/src/public-api.ts
@@ -2,7 +2,8 @@
  * Public API Surface of gw-lib
  */
 
-export * from './lib/gw-lib.service';
-export * from './lib/features/gw-lib.component';
+// export * from './lib/gw-lib.service';
+// export * from './lib/features/gw-lib.component';
+export * from './lib/features/dynamic-table/dynamic-table.component';
 export * from './lib/lib.module';
 export * from './lib/common'
