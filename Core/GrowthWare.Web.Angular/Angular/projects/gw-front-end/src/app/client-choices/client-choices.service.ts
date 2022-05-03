629b0a06a8580d1687961533c663e30e416c05e5
michael_regan <michael.regan@azimuth.com>
2022-05-03 05:26:22 -0400

    working on ClientChoices

--- Core/GrowthWare.Web.Angular/Angular/projects/gw-front-end/src/app/client-choices/client-choices.service.ts
+++ Core/GrowthWare.Web.Angular/Angular/projects/gw-front-end/src/app/client-choices/client-choices.service.ts
@@ -1,9 +1,23 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpParams } from '@angular/common/http';
+
+import { ClientChoices } from './client-choices.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ClientChoicesService {
+  private _HttpClient: HttpClient;
 
-  constructor() { }
+  constructor(httpClient: HttpClient) {
+    this._HttpClient = httpClient;
+  }
+
+  getClientChoices(accountId: number) {
+
+  }
+
+  updateClientChoices(clientChoices: ClientChoices) {
+
+  }
 }
