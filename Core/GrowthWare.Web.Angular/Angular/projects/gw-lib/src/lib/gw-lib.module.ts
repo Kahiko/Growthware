1216d6daab12ab878bfee06746ca6e6f31f8972c
michael_regan <michael.regan@azimuth.com>
2022-05-04 20:37:01 -0400

    removed the default service, component and the tests

--- Core/GrowthWare.Web.Angular/Angular/projects/gw-lib/src/lib/gw-lib.module.ts
+++ Core/GrowthWare.Web.Angular/Angular/projects/gw-lib/src/lib/gw-lib.module.ts
@@ -1,16 +1,13 @@
 import { NgModule } from '@angular/core';
-import { GwLibComponent } from './gw-lib.component';
 import { DynamicTableComponent } from './features/dynamic-table/dynamic-table.component';
 
 @NgModule({
   declarations: [
-    GwLibComponent,
     DynamicTableComponent,
   ],
   imports: [
   ],
   exports: [
-    GwLibComponent,
     DynamicTableComponent,
   ]
 })
