1ecf42bb4d43fe86f6f1440893feda4e93c6d0f0
michael_regan <michael.regan@azimuth.com>
2022-05-08 07:53:13 -0400

    finished moving the services

--- Core/GrowthWare.Web.Angular/Angular/projects/gw-lib/src/public-api.ts
+++ Core/GrowthWare.Web.Angular/Angular/projects/gw-lib/src/public-api.ts
@@ -4,7 +4,7 @@
 
 export * from './lib/features/dynamic-table/dynamic-table.component';
 export * from './lib/features/dynamic-table/dynamic-table.service';
-export * from './lib/services/search.service';
+export * from './lib/services/gw-lib.search.service';
 export * from './lib/gw-lib.module';
-export * from './lib/gw-lib.search.service';
+export * from './lib/services/gw-lib.search.service';
 export * from './lib/common'
