cf8c8cb8eefeb1d7a838b1ec9b77fcaf1b9dd86c
michael_regan <michael.regan@azimuth.com>
2022-05-15 10:15:00 -0400

    Adding a pager component

--- Core/GrowthWare.Web.Angular/Angular/projects/gw-lib/src/lib/gw-lib.module.ts
+++ Core/GrowthWare.Web.Angular/Angular/projects/gw-lib/src/lib/gw-lib.module.ts
@@ -1,16 +1,19 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { GWLibDynamicTableComponent } from './features/dynamic-table/dynamic-table.component';
+import { GWLibPagerComponent } from './features/pager/pager.component';
 
 @NgModule({
   declarations: [
     GWLibDynamicTableComponent,
+    GWLibPagerComponent,
   ],
   imports: [
     BrowserModule
   ],
   exports: [
     GWLibDynamicTableComponent,
+    GWLibPagerComponent,
   ]
 })
 export class GwLibModule { }
--- Core/GrowthWare.Web.Angular/Angular/projects/gw-lib/src/public-api.ts
+++ Core/GrowthWare.Web.Angular/Angular/projects/gw-lib/src/public-api.ts
@@ -4,6 +4,7 @@
 
 export * from './lib/features/dynamic-table/dynamic-table.component';
 export * from './lib/features/dynamic-table/dynamic-table.service';
+export * from './lib/features/pager/pager.component';
 export * from './lib/services/search.service';
 export * from './lib/gw-lib.module';
 export * from './lib/common'
