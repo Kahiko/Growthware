1e72dbbafc5fe90ca06f156318f2c91cf1284268
michael_regan <michael.regan@azimuth.com>
2022-06-06 06:30:01 -0400

    forgot to add securityInfo

---
+++ Core/GrowthWare.Web.Angular/Angular/projects/gw-lib/src/lib/models/security-info.model.ts
@@ -0,0 +1,16 @@
+export interface ISecurityInfo {
+  mayAdd: Boolean
+  mayEdit: Boolean
+  mayDelete: Boolean
+  mayView: Boolean
+}
+
+export class SecurityInfo implements ISecurityInfo {
+
+  constructor(
+    public mayAdd: boolean = false,
+    public mayEdit: boolean = false,
+    public mayDelete: boolean = false,
+    public mayView: boolean = false
+  ) {}
+}
