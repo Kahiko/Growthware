72c82cd0a96eaa737b5d240a71d96b535c2c91d5
michael_regan <michael.regan@azimuth.com>
2022-06-07 08:58:05 -0400

    now exports the modal feature

--- Core/GrowthWare.Web.Angular/Angular/projects/gw-lib/src/public-api.ts
+++ Core/GrowthWare.Web.Angular/Angular/projects/gw-lib/src/public-api.ts
@@ -4,6 +4,7 @@
 
 export * from './lib/common-code';
 export * from './lib/features/dynamic-table';
+export * from './lib/features/modal';
 export * from './lib/features/pager';
 export * from './lib/models';
 export * from './lib/services';
