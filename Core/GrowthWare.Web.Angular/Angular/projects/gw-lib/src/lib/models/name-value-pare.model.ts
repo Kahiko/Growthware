1dae5e6420377ca058e25263682f6d83ee1d6892
michael_regan <michael.regan@azimuth.com>
2022-06-15 10:12:35 -0400

    Adding new model

---
+++ Core/GrowthWare.Web.Angular/Angular/projects/gw-lib/src/lib/models/name-value-pare.model.ts
@@ -0,0 +1,22 @@
+import { ISearchCriteria } from "./search-criteria.model";
+
+export interface INameValuePare {
+  name: string,
+  payLoad: any
+}
+
+export class DataNVP implements INameValuePare {
+
+  constructor(public name: string, public payLoad: Array<any>) {}
+}
+
+export class SearchCriteriaNVP implements INameValuePare {
+
+  constructor(public name: string, public payLoad: ISearchCriteria) {}
+}
+
+export class SearchTotalRecordsNVP implements INameValuePare {
+
+  constructor(public name: string, public payLoad: number) {}
+}
+
