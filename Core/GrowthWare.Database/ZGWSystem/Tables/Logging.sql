dbe73c6287d3467042bbc8a51bd966f95d4f9561
michael_regan <michael.regan@azimuth.com>
2022-07-03 07:32:34 -0400

    formatting

--- Core/GrowthWare.Database/ZGWSystem/Tables/Logging.sql
+++ Core/GrowthWare.Database/ZGWSystem/Tables/Logging.sql
@@ -1,11 +1,11 @@
 CREATE TABLE [ZGWSystem].[Logging] (
-    [LogDate]   DATETIME        CONSTRAINT [DF_ZGWSystem.Logging_LogDate] DEFAULT (GETDATE()),
-    [Level]     VARCHAR (5)     NOT NULL,
-    [Account]   VARCHAR (128)   NOT NULL,
-    [Component] VARCHAR (50)    NOT NULL,
-    [ClassName]    VARCHAR (50)    NOT NULL,
+    [LogDate]       DATETIME        CONSTRAINT [DF_ZGWSystem.Logging_LogDate] DEFAULT (GETDATE()),
+    [Level]         VARCHAR (5)     NOT NULL,
+    [Account]       VARCHAR (128)   NOT NULL,
+    [Component]     VARCHAR (50)    NOT NULL,
+    [ClassName]     VARCHAR (50)    NOT NULL,
     [MethodName]    VARCHAR (50)    NOT NULL,
-    [Msg]       VARCHAR (50)    NOT NULL,
+    [Msg]           VARCHAR (50)    NOT NULL,
     CONSTRAINT [CI_ZGWSystem.Logging] UNIQUE CLUSTERED ([LogDate] ASC)
 );
 
