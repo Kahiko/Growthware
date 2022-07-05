87437ec2a54a0442d86694842faf185b3d796840
kahiko <michael.regan@azimuth.com>
2022-07-05 06:30:22 -0400

    add console.log message

--- Core/GrowthWare.Web.Angular/Angular/proxy.conf.js
+++ Core/GrowthWare.Web.Angular/Angular/proxy.conf.js
@@ -1,8 +1,7 @@
 const { env } = require('process');
 
-const target = env.ASPNETCORE_HTTPS_PORT ? `https://localhost:${env.ASPNETCORE_HTTPS_PORT}` :
-  env.ASPNETCORE_URLS ? env.ASPNETCORE_URLS.split(';')[0] : 'http://localhost:35360';
-
+const target = env.ASPNETCORE_HTTPS_PORT ? `https://localhost:${env.ASPNETCORE_HTTPS_PORT}` : env.ASPNETCORE_URLS ? env.ASPNETCORE_URLS.split(';')[0] : 'http://localhost:35360';
+console.log(`proxy.conf.js - Proxy target: '${target}'`);
 const PROXY_CONFIG = [
   {
     context: [
@@ -12,6 +11,7 @@ const PROXY_CONFIG = [
    ],
     target: target,
     secure: false,
+    http2: true,
     headers: {
       Connection: 'Keep-Alive'
     }
