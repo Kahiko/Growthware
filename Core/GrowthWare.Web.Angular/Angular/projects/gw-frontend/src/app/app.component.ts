c250722296a4426b96733baa9ddffcd9a9c7e6ec
kahiko <michael.regan@azimuth.com>
2022-07-28 03:41:00 -0400

    Updated const animateText

--- Core/GrowthWare.Web.Angular/Angular/projects/gw-frontend/src/app/app.component.ts
+++ Core/GrowthWare.Web.Angular/Angular/projects/gw-frontend/src/app/app.component.ts
@@ -20,45 +20,26 @@ export const animateText = trigger('animateText', [
   state(
     'hide',
     style({
-      display: 'none',
+      width: 0,
       opacity: 0,
     })
   ),
   state(
     'show',
     style({
-      display: 'block',
+      width: 150,
       opacity: 1,
     })
   ),
-  transition('close => open', animate('350ms ease-in')),
-  transition('open => close', animate('200ms ease-out')),
+  transition('hide => show', animate('3500ms ease-in')), // not working
+  transition('show => hide', animate('200ms ease-out')), // not working
 ]);
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
-  animations: [
-    trigger('animateText', [
-      state(
-        'hide',
-        style({
-          width: 0,
-          opacity: 0,
-        })
-      ),
-      state(
-        'show',
-        style({
-          width: 150,
-          opacity: 1,
-        })
-      ),
-      transition('hide => show', animate('3500ms ease-in')), // not working
-      transition('show => hide', animate('200ms ease-out')), // not working
-    ]),
-  ],
+  animations: [animateText],
 })
 export class AppComponent {
   title = 'FrontEnd';
