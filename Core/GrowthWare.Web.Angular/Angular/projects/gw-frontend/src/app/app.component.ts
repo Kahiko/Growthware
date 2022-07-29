783d9f75ede714f98a1060bc6f2ba65c62531586
kahiko <michael.regan@azimuth.com>
2022-07-29 05:18:29 -0400

    fixed the links api and home were broken

--- Core/GrowthWare.Web.Angular/Angular/projects/gw-frontend/src/app/app.component.ts
+++ Core/GrowthWare.Web.Angular/Angular/projects/gw-frontend/src/app/app.component.ts
@@ -33,13 +33,13 @@ export class AppComponent {
   sideNavLinks: Array<ISideNavLink> = [];
 
   constructor() {
-    let mSideNavLink = new SideNavLink('home', '', 'Home');
+    let mSideNavLink = new SideNavLink('home', 'home', 'Home');
     this.sideNavLinks.push(mSideNavLink);
     mSideNavLink = new SideNavLink('dialpad', 'counter', 'Counter');
     this.sideNavLinks.push(mSideNavLink);
     mSideNavLink = new SideNavLink('thermostat', 'fetch-data', 'Fetch Data');
     this.sideNavLinks.push(mSideNavLink);
-    mSideNavLink = new SideNavLink('api', 'swagger', 'API');
+    mSideNavLink = new SideNavLink('api', 'swagger', 'API', false);
     this.sideNavLinks.push(mSideNavLink);
     mSideNavLink = new SideNavLink('manage_accounts', 'search-accounts', 'Search');
     this.sideNavLinks.push(mSideNavLink);
