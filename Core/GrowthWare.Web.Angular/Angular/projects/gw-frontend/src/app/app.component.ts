53e7164688243937d18901e6cfae22f2f723b72e
kahiko <michael.regan@azimuth.com>
2022-08-11 05:19:40 -0400

    Formatting

--- Core/GrowthWare.Web.Angular/Angular/projects/gw-frontend/src/app/app.component.ts
+++ Core/GrowthWare.Web.Angular/Angular/projects/gw-frontend/src/app/app.component.ts
@@ -35,12 +35,16 @@ export class AppComponent {
   constructor() {
     let mSideNavLink = new SideNavLink('home', 'home', 'Home');
     this.sideNavLinks.push(mSideNavLink);
+
     mSideNavLink = new SideNavLink('dialpad', 'counter', 'Counter');
     this.sideNavLinks.push(mSideNavLink);
+
     mSideNavLink = new SideNavLink('thermostat', 'fetch-data', 'Fetch Data');
     this.sideNavLinks.push(mSideNavLink);
+
     mSideNavLink = new SideNavLink('api', 'swagger', 'API', false);
     this.sideNavLinks.push(mSideNavLink);
+
     mSideNavLink = new SideNavLink('manage_accounts', 'search-accounts', 'Search');
     this.sideNavLinks.push(mSideNavLink);
   }
