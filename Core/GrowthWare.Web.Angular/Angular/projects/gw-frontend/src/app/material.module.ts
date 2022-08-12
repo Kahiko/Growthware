e3fb917c5c40724eb308027ba98ec1f300d09dc8
kahiko <michael.regan@azimuth.com>
2022-08-12 06:10:23 -0400

    Didn't need MatFormFieldModule

--- Core/GrowthWare.Web.Angular/Angular/projects/gw-frontend/src/app/material.module.ts
+++ Core/GrowthWare.Web.Angular/Angular/projects/gw-frontend/src/app/material.module.ts
@@ -21,7 +21,7 @@ import { MatButtonModule } from '@angular/material/button';
 // import { MatDialogModule } from '@angular/material/dialog';
 import { MatDividerModule } from '@angular/material/divider';
 // import { MatExpansionModule } from '@angular/material/expansion';
-import { MatFormFieldModule } from '@angular/material/form-field';
+// import { MatFormFieldModule } from '@angular/material/form-field';
 // import { MatGridListModule } from '@angular/material/grid-list';
 import { MatIconModule } from '@angular/material/icon';
 // import { MatInputModule } from '@angular/material/input';
@@ -65,7 +65,7 @@ import { MatToolbarModule } from '@angular/material/toolbar';
   // MatDialogModule,
   MatDividerModule,
   // MatExpansionModule,
-  MatFormFieldModule,
+  // MatFormFieldModule,
   // MatGridListModule,
   MatIconModule,
   // MatInputModule,
