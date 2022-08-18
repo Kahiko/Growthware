54aac2a2ebcf03138149a4361c46bc5ba440717e
kahiko <michael.regan@azimuth.com>
2022-08-18 05:27:44 -0400

    Prep for login

--- Core/GrowthWare.Web.Angular/Angular/projects/gw-frontend/src/app/skins/default/default.module.ts
+++ Core/GrowthWare.Web.Angular/Angular/projects/gw-frontend/src/app/skins/default/default.module.ts
@@ -11,15 +11,15 @@ import { MatToolbarModule } from '@angular/material/toolbar';
 // Modules/Components
 import { DefaultRoutingModule } from './default-routing.module';
 import { DefaultComponent } from './default/default.component';
-import { DefaultFooterComponent } from './layout/default-footer/default-footer.component';
-import { DefaultHeaderComponent } from './layout/default-header/default-header.component';
+// import { DefaultFooterComponent } from './layout/default-footer/default-footer.component';
+// import { DefaultHeaderComponent } from './layout/default-header/default-header.component';
 import { DefaultSidebarComponent } from './layout/default-sidebar/default-sidebar.component';
 
 @NgModule({
   declarations: [
     DefaultComponent,
-    DefaultFooterComponent,
-    DefaultHeaderComponent,
+    // DefaultFooterComponent,
+    // DefaultHeaderComponent,
     DefaultSidebarComponent
   ],
   imports: [
--- Core/GrowthWare.Web.Angular/Angular/projects/gw-frontend/src/app/skins/default/layout/default-header/default-header.component.ts
+++ Core/GrowthWare.Web.Angular/Angular/projects/gw-frontend/src/app/skins/default/layout/default-header/default-header.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { ModalService, ModalOptions, ModalSize } from '@Growthware/Lib';
 
 @Component({
   selector: 'gw-frontend-default-header',
@@ -6,12 +7,25 @@ import { Component, Input, OnInit } from '@angular/core';
   styleUrls: ['./default-header.component.scss']
 })
 export class DefaultHeaderComponent implements OnInit {
+  private mLoginId: string = 'loginModal';
+
+  isAuthenticated: boolean = false;
 
   @Input() sidenav: any;
 
-  constructor() { }
+  constructor(private _ModalSvc: ModalService) { }
 
   ngOnInit(): void {
+  }
+
+  onLogin(): void {
+    const mModalOptions: ModalOptions = new ModalOptions(this.mLoginId, 'Login', 'account/password and buttons', ModalSize.Normal);
+    this._ModalSvc.open(mModalOptions);
+    this.isAuthenticated = true;
+  }
+
+  onLogout(): void {
+    this.isAuthenticated = false;
   }
 
 }
