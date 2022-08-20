f7e2ec501801a6e4befc563bb7a6b70358eda0e5
kahiko <michael.regan@azimuth.com>
2022-08-20 04:14:15 -0400

    with previous

--- Core/GrowthWare.Web.Angular/Angular/projects/gw-frontend/src/app/features/accounts/accounts-routing.module.ts
+++ Core/GrowthWare.Web.Angular/Angular/projects/gw-frontend/src/app/features/accounts/accounts-routing.module.ts
@@ -1,12 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-import { AccountDetailsComponent } from '@Growthware/Lib';
-import { ListAccountsComponent } from './list-accounts/list-accounts.component';
-import { LoginComponent } from '@Growthware/Lib';
+import { AccountDetailsComponent, LoginComponent, SearchAccountsComponent } from '@Growthware/Lib';
 
 const routes: Routes = [
-  { path: '', component: ListAccountsComponent},
+  { path: '', component: SearchAccountsComponent},
   { path: 'edit-account', component: AccountDetailsComponent },
   { path: 'edit-my-account', component: AccountDetailsComponent },
   { path: 'login', component: LoginComponent}
--- Core/GrowthWare.Web.Angular/Angular/projects/gw-frontend/src/app/features/accounts/accounts.module.ts
+++ Core/GrowthWare.Web.Angular/Angular/projects/gw-frontend/src/app/features/accounts/accounts.module.ts
@@ -1,19 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-// Library Modules
-import { AccountModule } from '@Growthware/Lib';
 
 // Feature
 import { AccountsRoutingModule } from './accounts-routing.module';
-import { ListAccountsComponent } from './list-accounts/list-accounts.component';
 
 @NgModule({
   declarations: [
-    ListAccountsComponent,
+    // SearchAccountsComponent,
   ],
   imports: [
     CommonModule,
-    AccountModule,
     AccountsRoutingModule,
   ]
 })
