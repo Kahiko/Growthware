2d2aedc0f7977f452d9d0b097eaea4d17d1053bd
kahiko <michael.regan@azimuth.com>
2022-08-28 01:27:17 -0400

    added routerLink

--- Core/GrowthWare.Web.Angular/Angular/projects/gw-lib/src/lib/features/navigation/navigation.module.ts
+++ Core/GrowthWare.Web.Angular/Angular/projects/gw-lib/src/lib/features/navigation/navigation.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { VerticalListItemComponent } from './c/vertical-list-item/vertical-list-item.component';
-
+import { RouterModule } from '@angular/router';
+// Angular Material
 import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
+// Feature
+import { VerticalListItemComponent } from './c/vertical-list-item/vertical-list-item.component';
 
 @NgModule({
   declarations: [
@@ -12,7 +14,8 @@ import { MatListModule } from '@angular/material/list';
   imports: [
     CommonModule,
     MatIconModule,
-    MatListModule
+    MatListModule,
+    RouterModule
   ],
   exports: [
     VerticalListItemComponent
