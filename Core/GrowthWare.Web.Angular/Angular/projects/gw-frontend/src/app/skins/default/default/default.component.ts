2081b99a7f59f2b6223e4c93aecee28cee2de922
kahiko <michael.regan@azimuth.com>
2022-08-27 21:22:34 -0400

    Removed reference to ISideNavLink, SideNavLink Removed property sideNavLinks

--- Core/GrowthWare.Web.Angular/Angular/projects/gw-frontend/src/app/skins/default/default/default.component.ts
+++ Core/GrowthWare.Web.Angular/Angular/projects/gw-frontend/src/app/skins/default/default/default.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 // Library
-import { ISideNavLink, SideNavLink } from '@Growthware/Lib/src/lib/models';
 import { INavLink, NavLink } from '@Growthware/Lib';
 import { sideNavTextAnimation } from './animations/side-nav';
 
@@ -13,25 +12,11 @@ import { sideNavTextAnimation } from './animations/side-nav';
 export class DefaultComponent implements OnInit {
 
   showSideNavLinkText = false;
-  sideNavLinks: Array<ISideNavLink> = [];
   verticalNavLinks: Array<INavLink> = [];
 
   constructor() { }
 
   ngOnInit(): void {
-    let mSideNavLink = new SideNavLink('home', 'home', 'Home');
-    this.sideNavLinks.push(mSideNavLink);
-    mSideNavLink = new SideNavLink('dialpad', 'counter', 'Counter');
-    this.sideNavLinks.push(mSideNavLink);
-    mSideNavLink = new SideNavLink('thermostat', 'fetch-data', 'Fetch Data');
-    this.sideNavLinks.push(mSideNavLink);
-    mSideNavLink = new SideNavLink('api', 'swagger', 'API', false);
-    this.sideNavLinks.push(mSideNavLink);
-    mSideNavLink = new SideNavLink('manage_accounts', 'search-accounts', 'Manage Accounts');
-    this.sideNavLinks.push(mSideNavLink);
-    mSideNavLink = new SideNavLink('functions', 'search-functions', 'Manage Functions');
-    this.sideNavLinks.push(mSideNavLink);
-
     let mNavLink = new NavLink('home', 'home', 'Home');
     this.verticalNavLinks.push(mNavLink);
     mNavLink = new NavLink('dialpad', 'counter', 'Counter');
