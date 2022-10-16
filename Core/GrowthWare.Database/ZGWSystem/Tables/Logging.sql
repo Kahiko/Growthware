6d451d4b37fd7f1080d40e647038da3f20ca93e7
kahiko <michael.regan@azimuth.com>
2022-10-16 08:37:23 -0400

    Changed Msg to NVARCHAR(MAX)

--- Core/GrowthWare.Database/ZGWSystem/Tables/Logging.sql
+++ Core/GrowthWare.Database/ZGWSystem/Tables/Logging.sql
@@ -1,12 +1,12 @@
 CREATE TABLE [ZGWSystem].[Logging] (
-    [Account]    VARCHAR (128) NOT NULL,
-    [Component]  VARCHAR (50)  NOT NULL,
-    [ClassName]  VARCHAR (50)  NOT NULL,
-    [Level]      VARCHAR (5)   NOT NULL,
-    [LogDate]    DATETIME      CONSTRAINT [DF_ZGWSystem.Logging_LogDate] DEFAULT (getdate()) NULL,
-    [LogSeqId]   INT           IDENTITY (1, 1) NOT NULL,
-    [MethodName] VARCHAR (50)  NOT NULL,
-    [Msg]        VARCHAR (MAX) NOT NULL,
+    [Account]    VARCHAR (128)  NOT NULL,
+    [Component]  VARCHAR (50)   NOT NULL,
+    [ClassName]  VARCHAR (50)   NOT NULL,
+    [Level]      VARCHAR (5)    NOT NULL,
+    [LogDate]    DATETIME       CONSTRAINT [DF_ZGWSystem.Logging_LogDate] DEFAULT (getdate()) NULL,
+    [LogSeqId]   INT            IDENTITY (1, 1) NOT NULL,
+    [MethodName] VARCHAR (50)   NOT NULL,
+    [Msg]        NVARCHAR (MAX) NOT NULL,
     CONSTRAINT [CI_ZGWSystem.Logging] UNIQUE CLUSTERED ([LogDate] ASC)
 );
 
