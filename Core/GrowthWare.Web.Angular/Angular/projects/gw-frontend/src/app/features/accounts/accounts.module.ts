8b6f4b98859993e693eda8022c97cd1754038a2f
kahiko <michael.regan@azimuth.com>
2022-10-25 06:16:47 -0400

    changed imports and declarations

--- Core/GrowthWare.Web.Angular/Angular/projects/gw-frontend/src/app/features/accounts/accounts.module.ts
+++ Core/GrowthWare.Web.Angular/Angular/projects/gw-frontend/src/app/features/accounts/accounts.module.ts
@@ -1,9 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+// This Feature
 import { AccountsRoutingModule } from './accounts-routing.module';
-
-
 @NgModule({
   declarations: [],
   imports: [
--- Core/GrowthWare.Web.Angular/Angular/projects/gw-frontend/src/app/features/home/home.module.ts
+++ Core/GrowthWare.Web.Angular/Angular/projects/gw-frontend/src/app/features/home/home.module.ts
@@ -1,11 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+// This Feature
 import { HomeRoutingModule } from './home-routing.module';
-
+import { HomeComponent } from './c/home/home.component';
+import { GenericHomeComponent } from './c/generic-home/generic-home.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    GenericHomeComponent,
+    HomeComponent
+  ],
   imports: [
     CommonModule,
     HomeRoutingModule
