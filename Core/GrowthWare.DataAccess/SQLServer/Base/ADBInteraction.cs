c8947abd91ab8ad0db9d108f7b5c07df556b551c
kahiko <michael.regan@azimuth.com>
2022-11-24 08:56:06 -0500

    Removed unused using

--- Core/GrowthWare.DataAccess/SQLServer/Base/ADBInteraction.cs
+++ Core/GrowthWare.DataAccess/SQLServer/Base/ADBInteraction.cs
@@ -1,7 +1,6 @@
 using GrowthWare.DataAccess.Interfaces.Base;
 using GrowthWare.Framework.Interfaces;
 using System;
-using System.Collections.Generic;
 using System.Data;
 using System.Data.SqlClient;
 using System.Globalization;
