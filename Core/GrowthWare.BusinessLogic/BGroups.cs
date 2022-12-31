55302ab1b0f1a1755a0b3c3efb036c324aaba4d8
kahiko <michael.regan@verizon.net>
2022-12-31 08:29:48 -0500

    changed namespace syntax

--- Core/GrowthWare.BusinessLogic/BGroups.cs
+++ Core/GrowthWare.BusinessLogic/BGroups.cs
@@ -5,202 +5,200 @@ using System;
 using System.Collections;
 using System.Data;
 
-namespace GrowthWare.BusinessLogic
+namespace GrowthWare.BusinessLogic;
+/// <summary>
+/// Process business logic for accounts
+/// </summary>
+/// <remarks>
+/// <![CDATA[
+/// MSecurityEntity can be found in the GrowthWare.Framework.Model.Profiles namespace.  
+/// 
+/// The following properties are necessary for correct business logic operation.
+/// .ConnectionString
+/// .DALName
+/// .DALNameSpace
+/// ]]>
+/// </remarks>
+/// <example> This sample shows how to create an instance of the class.
+/// <code language="VB.NET">
+/// <![CDATA[
+/// Dim myBll as new BGroups(mySecurityEntityProfile)
+/// ]]>
+/// </code>
+/// </example>
+public class BGroups : AbstractBusinessLogic
 {
+    private IGroups m_DGroups;
+
     /// <summary>
-    /// Process business logic for accounts
+    /// Private BGroups() to ensure only new instances with passed parameters is used.
     /// </summary>
-    /// <remarks>
-    /// <![CDATA[
-    /// MSecurityEntity can be found in the GrowthWare.Framework.Model.Profiles namespace.  
-    /// 
-    /// The following properties are necessary for correct business logic operation.
-    /// .ConnectionString
-    /// .DALName
-    /// .DALNameSpace
-    /// ]]>
-    /// </remarks>
+    /// <remarks></remarks>
+    private BGroups()
+    {
+    }
+
+    /// <summary>
+    /// Parameters are need to pass along to the factory for correct connection to the desired datastore.
+    /// </summary>
+    /// <param name="securityEntityProfile">The Security Entity profile used to obtain the DAL name, DAL name space, and the Connection String</param>
+    /// <param name="centralManagement">Boolean value indicating if the system is being used to manage multiple database instances.</param>
+    /// <remarks></remarks>
     /// <example> This sample shows how to create an instance of the class.
     /// <code language="VB.NET">
     /// <![CDATA[
-    /// Dim myBll as new BGroups(mySecurityEntityProfile)
+    /// MSecurityEntity MSecurityEntity = MSecurityEntity = New MSecurityEntity();
+    /// MSecurityEntity.Id = ConfigSettings.DefaultSecurityEntityID;
+    /// MSecurityEntity.DAL = ConfigSettings.DAL;
+    /// MSecurityEntity.DAL_Namespace = ConfigSettings.DAL_NameSpace(MSecurityEntity.DAL);
+    /// MSecurityEntity.DAL_Name = ConfigSettings.DAL_AssemblyName(MSecurityEntity.DAL);
+    /// MSecurityEntity.ConnectionString = ConfigSettings.ConnectionString;
+    /// 
+    /// BGroupss mBGroups = BGroupss = New BGroupss(MSecurityEntity, ConfigSettings.CentralManagement);
+    /// ]]>
+    /// </code>
+    /// <code language="C#">
+    /// <![CDATA[
+    /// Dim MSecurityEntity As MSecurityEntity = New MSecurityEntity()
+    /// MSecurityEntity.Id = ConfigSettings.DefaultSecurityEntityID
+    /// MSecurityEntity.DAL = ConfigSettings.DAL
+    /// MSecurityEntity.DAL_Namespace = ConfigSettings.DAL_NameSpace(MSecurityEntity.DAL)
+    /// MSecurityEntity.DAL_Name = ConfigSettings.DAL_AssemblyName(MSecurityEntity.DAL)
+    /// MSecurityEntity.ConnectionString = ConfigSettings.ConnectionString
+    /// 
+    /// Dim mBGroups As BGroupss = New BGroupss(MSecurityEntity, ConfigSettings.CentralManagement)
     /// ]]>
     /// </code>
     /// </example>
-    public class BGroups : AbstractBusinessLogic
+    public BGroups(MSecurityEntity securityEntityProfile, bool centralManagement)
     {
-        private IGroups m_DGroups;
-
-        /// <summary>
-        /// Private BGroups() to ensure only new instances with passed parameters is used.
-        /// </summary>
-        /// <remarks></remarks>
-        private BGroups()
+        if (securityEntityProfile == null)
         {
+            throw new ArgumentNullException("securityEntityProfile", "The securityEntityProfile cannot be a null reference (Nothing in Visual Basic)!!");
         }
-
-        /// <summary>
-        /// Parameters are need to pass along to the factory for correct connection to the desired datastore.
-        /// </summary>
-        /// <param name="securityEntityProfile">The Security Entity profile used to obtain the DAL name, DAL name space, and the Connection String</param>
-        /// <param name="centralManagement">Boolean value indicating if the system is being used to manage multiple database instances.</param>
-        /// <remarks></remarks>
-        /// <example> This sample shows how to create an instance of the class.
-        /// <code language="VB.NET">
-        /// <![CDATA[
-        /// MSecurityEntity MSecurityEntity = MSecurityEntity = New MSecurityEntity();
-        /// MSecurityEntity.Id = ConfigSettings.DefaultSecurityEntityID;
-        /// MSecurityEntity.DAL = ConfigSettings.DAL;
-        /// MSecurityEntity.DAL_Namespace = ConfigSettings.DAL_NameSpace(MSecurityEntity.DAL);
-        /// MSecurityEntity.DAL_Name = ConfigSettings.DAL_AssemblyName(MSecurityEntity.DAL);
-        /// MSecurityEntity.ConnectionString = ConfigSettings.ConnectionString;
-        /// 
-        /// BGroupss mBGroups = BGroupss = New BGroupss(MSecurityEntity, ConfigSettings.CentralManagement);
-        /// ]]>
-        /// </code>
-        /// <code language="C#">
-        /// <![CDATA[
-        /// Dim MSecurityEntity As MSecurityEntity = New MSecurityEntity()
-        /// MSecurityEntity.Id = ConfigSettings.DefaultSecurityEntityID
-        /// MSecurityEntity.DAL = ConfigSettings.DAL
-        /// MSecurityEntity.DAL_Namespace = ConfigSettings.DAL_NameSpace(MSecurityEntity.DAL)
-        /// MSecurityEntity.DAL_Name = ConfigSettings.DAL_AssemblyName(MSecurityEntity.DAL)
-        /// MSecurityEntity.ConnectionString = ConfigSettings.ConnectionString
-        /// 
-        /// Dim mBGroups As BGroupss = New BGroupss(MSecurityEntity, ConfigSettings.CentralManagement)
-        /// ]]>
-        /// </code>
-        /// </example>
-        public BGroups(MSecurityEntity securityEntityProfile, bool centralManagement)
+        if (centralManagement)
         {
-            if (securityEntityProfile == null)
-            {
-                throw new ArgumentNullException("securityEntityProfile", "The securityEntityProfile cannot be a null reference (Nothing in Visual Basic)!!");
-            }
-            if (centralManagement)
-            {
-                if (m_DGroups == null)
-                {
-                    m_DGroups = (IGroups)ObjectFactory.Create(securityEntityProfile.DataAccessLayerAssemblyName, securityEntityProfile.DataAccessLayerNamespace, "DGroups");
-                }
-            }
-            else
+            if (m_DGroups == null)
             {
                 m_DGroups = (IGroups)ObjectFactory.Create(securityEntityProfile.DataAccessLayerAssemblyName, securityEntityProfile.DataAccessLayerNamespace, "DGroups");
             }
-
-            m_DGroups.ConnectionString = securityEntityProfile.ConnectionString;
-            m_DGroups.SecurityEntitySeqID = securityEntityProfile.Id;
+        }
+        else
+        {
+            m_DGroups = (IGroups)ObjectFactory.Create(securityEntityProfile.DataAccessLayerAssemblyName, securityEntityProfile.DataAccessLayerNamespace, "DGroups");
         }
 
-        /// <summary>
-        /// Gets the groups by security entity.
-        /// </summary>
-        /// <param name="SecurityEntityID">The security entity ID.</param>
-        /// <returns>DataTable.</returns>
-        public DataTable GetGroupsBySecurityEntity(int SecurityEntityID)
-        {
-            MGroupProfile myProfile = new MGroupProfile();
-            myProfile.SecurityEntityID = SecurityEntityID;
-            m_DGroups.Profile = myProfile;
-            return m_DGroups.GroupsBySecurityEntity();
-        }
+        m_DGroups.ConnectionString = securityEntityProfile.ConnectionString;
+        m_DGroups.SecurityEntitySeqID = securityEntityProfile.Id;
+    }
 
-        /// <summary>
-        /// Adds the group.
-        /// </summary>
-        /// <param name="profile">The profile.</param>
-        /// <returns>System.Int32.</returns>
-        public void AddGroup(MGroupProfile profile)
-        {
-            if (profile == null) throw new ArgumentNullException("profile", "profile cannot be a null reference (Nothing in Visual Basic)!!");
-            m_DGroups.Profile = profile;
-            if(DatabaseIsOnline()) m_DGroups.AddGroup();
-        }
+    /// <summary>
+    /// Gets the groups by security entity.
+    /// </summary>
+    /// <param name="SecurityEntityID">The security entity ID.</param>
+    /// <returns>DataTable.</returns>
+    public DataTable GetGroupsBySecurityEntity(int SecurityEntityID)
+    {
+        MGroupProfile myProfile = new MGroupProfile();
+        myProfile.SecurityEntityID = SecurityEntityID;
+        m_DGroups.Profile = myProfile;
+        return m_DGroups.GroupsBySecurityEntity();
+    }
 
-        /// <summary>
-        /// Gets the profile.
-        /// </summary>
-        /// <param name="groupId">The group ID.</param>
-        /// <returns>MGroupProfile.</returns>
-        public MGroupProfile GetProfile(int groupId)
-        {
-            MGroupProfile retProfile = new MGroupProfile();
-            retProfile.Id = groupId;
-            m_DGroups.Profile = retProfile;
-            retProfile = new MGroupProfile(m_DGroups.ProfileData());
-            return retProfile;
-        }
+    /// <summary>
+    /// Adds the group.
+    /// </summary>
+    /// <param name="profile">The profile.</param>
+    /// <returns>System.Int32.</returns>
+    public void AddGroup(MGroupProfile profile)
+    {
+        if (profile == null) throw new ArgumentNullException("profile", "profile cannot be a null reference (Nothing in Visual Basic)!!");
+        m_DGroups.Profile = profile;
+        if (DatabaseIsOnline()) m_DGroups.AddGroup();
+    }
 
-        /// <summary>
-        /// Deletes the group.
-        /// </summary>
-        /// <param name="profile">The profile.</param>
-        /// <returns><c>true</c> if XXXX, <c>false</c> otherwise</returns>
-        public bool DeleteGroup(MGroupProfile profile)
-        {
-            if (profile == null) throw new ArgumentNullException("profile", "profile cannot be a null reference (Nothing in Visual Basic)!!");
-            bool retVal = false;
-            m_DGroups.Profile = profile;
-            if (DatabaseIsOnline()) retVal = m_DGroups.DeleteGroup();
-            return retVal;
-        }
+    /// <summary>
+    /// Gets the profile.
+    /// </summary>
+    /// <param name="groupId">The group ID.</param>
+    /// <returns>MGroupProfile.</returns>
+    public MGroupProfile GetProfile(int groupId)
+    {
+        MGroupProfile retProfile = new MGroupProfile();
+        retProfile.Id = groupId;
+        m_DGroups.Profile = retProfile;
+        retProfile = new MGroupProfile(m_DGroups.ProfileData());
+        return retProfile;
+    }
 
-        /// <summary>
-        /// Updates the group.
-        /// </summary>
-        /// <param name="profile">The profile.</param>
-        /// <returns><c>true</c> if XXXX, <c>false</c> otherwise</returns>
-        public void Save(MGroupProfile profile)
-        {
-            if (profile == null) throw new ArgumentNullException("profile", "profile cannot be a null reference (Nothing in Visual Basic)!!");
-            m_DGroups.Profile = profile;
-            if (DatabaseIsOnline()) m_DGroups.Save();
-        }
+    /// <summary>
+    /// Deletes the group.
+    /// </summary>
+    /// <param name="profile">The profile.</param>
+    /// <returns><c>true</c> if XXXX, <c>false</c> otherwise</returns>
+    public bool DeleteGroup(MGroupProfile profile)
+    {
+        if (profile == null) throw new ArgumentNullException("profile", "profile cannot be a null reference (Nothing in Visual Basic)!!");
+        bool retVal = false;
+        m_DGroups.Profile = profile;
+        if (DatabaseIsOnline()) retVal = m_DGroups.DeleteGroup();
+        return retVal;
+    }
 
-        /// <summary>
-        /// Gets the selected roles.
-        /// </summary>
-        /// <param name="profile">The profile.</param>
-        /// <returns>System.String[][].</returns>
-        public string[] GetSelectedRoles(MGroupRoles profile)
+    /// <summary>
+    /// Updates the group.
+    /// </summary>
+    /// <param name="profile">The profile.</param>
+    /// <returns><c>true</c> if XXXX, <c>false</c> otherwise</returns>
+    public void Save(MGroupProfile profile)
+    {
+        if (profile == null) throw new ArgumentNullException("profile", "profile cannot be a null reference (Nothing in Visual Basic)!!");
+        m_DGroups.Profile = profile;
+        if (DatabaseIsOnline()) m_DGroups.Save();
+    }
+
+    /// <summary>
+    /// Gets the selected roles.
+    /// </summary>
+    /// <param name="profile">The profile.</param>
+    /// <returns>System.String[][].</returns>
+    public string[] GetSelectedRoles(MGroupRoles profile)
+    {
+        if (profile == null) throw new ArgumentNullException("profile", "profile cannot be a null reference (Nothing in Visual Basic)!!");
+        ArrayList ClientRoles = new ArrayList();
+        m_DGroups.GroupRolesProfile = profile;
+        DataTable myDataTable = null;
+        if (DatabaseIsOnline())
         {
-            if (profile == null) throw new ArgumentNullException("profile", "profile cannot be a null reference (Nothing in Visual Basic)!!");
-            ArrayList ClientRoles = new ArrayList();
-            m_DGroups.GroupRolesProfile = profile;
-            DataTable myDataTable = null;
-            if (DatabaseIsOnline()) 
+            try
             {
-                try
+                myDataTable = m_DGroups.GroupRoles();
+                DataRow myDR = null;
+                foreach (DataRow myDR_loopVariable in myDataTable.Rows)
                 {
-                    myDataTable = m_DGroups.GroupRoles();
-                    DataRow myDR = null;
-                    foreach (DataRow myDR_loopVariable in myDataTable.Rows)
-                    {
-                        myDR = myDR_loopVariable;
-                        ClientRoles.Add(myDR["Role"].ToString());
-                    }
-                }
-                catch (BusinessLogicLayerException)
-                {
-                    throw;
+                    myDR = myDR_loopVariable;
+                    ClientRoles.Add(myDR["Role"].ToString());
                 }
             }
-            return (string[])ClientRoles.ToArray(typeof(string));
+            catch (BusinessLogicLayerException)
+            {
+                throw;
+            }
         }
+        return (string[])ClientRoles.ToArray(typeof(string));
+    }
 
-        /// <summary>
-        /// Updates the group roles.
-        /// </summary>
-        /// <param name="profile">The profile.</param>
-        /// <returns><c>true</c> if XXXX, <c>false</c> otherwise</returns>
-        public bool UpdateGroupRoles(MGroupRoles profile)
-        {
-            if (profile == null) throw new ArgumentNullException("profile", "profile cannot be a null reference (Nothing in Visual Basic)!!");
-            bool mRetVal = false;
-            m_DGroups.GroupRolesProfile = profile;
-            if (DatabaseIsOnline()) mRetVal = m_DGroups.UpdateGroupRoles();
-            return mRetVal;
-        }
+    /// <summary>
+    /// Updates the group roles.
+    /// </summary>
+    /// <param name="profile">The profile.</param>
+    /// <returns><c>true</c> if XXXX, <c>false</c> otherwise</returns>
+    public bool UpdateGroupRoles(MGroupRoles profile)
+    {
+        if (profile == null) throw new ArgumentNullException("profile", "profile cannot be a null reference (Nothing in Visual Basic)!!");
+        bool mRetVal = false;
+        m_DGroups.GroupRolesProfile = profile;
+        if (DatabaseIsOnline()) mRetVal = m_DGroups.UpdateGroupRoles();
+        return mRetVal;
     }
 }
