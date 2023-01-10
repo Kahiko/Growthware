ca93b2eff69b2c60f73a7e5aa67f9d8aac17f3f4
kahiko <michael.regan@azimuth.com>
2023-01-10 06:59:12 -0500

    adding some basic logic

--- Core/GrowthWare.Web.Angular/Angular/projects/gw-lib/src/lib/features/account/c/change-password/change-password.component.ts
+++ Core/GrowthWare.Web.Angular/Angular/projects/gw-lib/src/lib/features/account/c/change-password/change-password.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-
+import { ActivatedRoute, Router } from '@angular/router';
+// Library
+import { AccountService } from '@Growthware/Lib/src/lib/features/account';
 @Component({
   selector: 'gw-lib-change-password',
   templateUrl: './change-password.component.html',
@@ -7,9 +9,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ChangePasswordComponent implements OnInit {
 
-  constructor() { }
+  private _Return: string = '';
+
+  constructor(
+    private _AccountSvc: AccountService,
+    private _Router: Router,
+    private _ActivatedRoute: ActivatedRoute,
+  ) { }
 
   ngOnInit(): void {
+    this._ActivatedRoute.queryParams
+    .subscribe(params => this._Return = params['return'] || '/home');
+  }
+
+  changePassword() {
+    // logic to change password
+    // on success change routes
+    this._Router.navigateByUrl(this._Return);
   }
 
 }
