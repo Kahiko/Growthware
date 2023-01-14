06244975e55dcf2641f4bab70408ce2b76e0e44a
kahiko <michael.regan@azimuth.com>
2023-01-14 08:57:48 -0500

    Added check for state.url before navigating

--- Core/GrowthWare.Web.Angular/Angular/projects/gw-frontend/src/app/guards/auth.guard.ts
+++ Core/GrowthWare.Web.Angular/Angular/projects/gw-frontend/src/app/guards/auth.guard.ts
@@ -17,26 +17,32 @@ export class AuthGuard implements CanActivate  {
 
   canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
     const token = localStorage.getItem("jwt");
-
+    let mRedirectUrl: string = '';
     if (token && !this._JwtHelper.isTokenExpired(token)) {
       // console.log(this._JwtHelper.decodeToken(token))
       switch (this._AccountSvc.authenticationResponse.status) {
         case 1:
-          this._Router.navigate(['/home']);
+          mRedirectUrl = '/home';
           break;
         case 4:
-          this._Router.navigate(['/change-password'], {
+          console.log(state);
+          mRedirectUrl = '/accounts/change-password';
+          break;
+        case 5:
+          mRedirectUrl = '/accounts/edit-my-account';
+          break;
+        default:
+          break;
+      }
+      if(mRedirectUrl !== '') {
+        if(!state.url.startsWith(mRedirectUrl)) {
+          this._Router.navigate([mRedirectUrl], {
             queryParams: {
               return: state.url
             }
           });
           return false;
-          break;
-        case 5:
-          this._Router.navigate(['/edit-my-account']);
-          break;
-        default:
-          break;
+        }
       }
       return true;
     }
