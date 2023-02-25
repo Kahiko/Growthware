91b122e020e0ab0b1277848e2c3f66b94d5893ac
Kahiko <michael.regan@verizon.net>
2023-02-25 06:01:40 -0500

    Added UploadResponse to ficillate UploadFile

---
+++ Core/GrowthWare.Framework/Models/UploadResponse.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace GrowthWare.Framework.Models;
+
+[Serializable(), CLSCompliant(true)]
+public class UploadResponse
+{
+    public dynamic Data { get; set; }
+    public string ErrorMessage { get; set; }
+    public string FileName { get; set; }
+    public bool IsSuccess { get; set; } = true;
+}
