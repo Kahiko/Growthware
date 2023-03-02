32615e6ab4aa6bd576e9a63951b0e57047c40573
Kahiko <michael.regan@verizon.net>
2023-03-02 06:48:00 -0500

    Added right click context menu

--- Core/GrowthWare.Web.Angular/Angular/projects/gw-lib/src/lib/features/file-manager/c/directory-tree/directory-tree.component.ts
+++ Core/GrowthWare.Web.Angular/Angular/projects/gw-lib/src/lib/features/file-manager/c/directory-tree/directory-tree.component.ts
@@ -1,5 +1,9 @@
 import { Component, Input, AfterViewInit, OnInit } from '@angular/core';
+import { ViewChild } from '@angular/core';
 import { NestedTreeControl } from '@angular/cdk/tree';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { TemplateRef } from '@angular/core';
+import { MatMenuTrigger } from '@angular/material/menu';
 // import { MatTreeNestedDataSource } from '@angular/material/tree';
 import { Router } from '@angular/router';
 import { BehaviorSubject, Subscription } from 'rxjs';
@@ -9,7 +13,7 @@ import { DataService } from '@Growthware/Lib/src/lib/services';
 import { GWCommon } from '@Growthware/Lib/src/lib/common-code';
 import { LogDestination, ILogOptions, LogOptions } from '@Growthware/Lib/src/lib/features/logging';
 import { LoggingService, LogLevel } from '@Growthware/Lib/src/lib/features/logging';
-
+import { ModalOptions, ModalService, WindowSize } from '@Growthware/Lib/src/lib/features/modal';
 // Feature
 import { FileManagerService } from '../../file-manager.service';
 import { IDirectoryTree } from '../../directory-tree.model';
@@ -28,14 +32,29 @@ export class DirectoryTreeComponent implements AfterViewInit, OnInit {
 
   activeNode?: IDirectoryTree;
   configurationName: string = ''
+  frmRenameDirectory!: FormGroup;
+  menuTopLeftPosition =  {x: '0', y: '0'}; // we create an object that contains coordinates 
+
+  get getControls() {
+    return this.frmRenameDirectory.controls;
+  }
+
   treeControl = new NestedTreeControl<IDirectoryTree>(node => node.children);
   readonly dataSource = this._DataSubject.asObservable();
+  // reference to the MatMenuTrigger in the DOM 
+  @ViewChild( MatMenuTrigger, {static: true}) private _MatMenuTrigger!: MatMenuTrigger;
+  @ViewChild('deleteDirectory', { read: TemplateRef }) private _DeleteDirectory!:TemplateRef<any>;
+  @ViewChild('directoryProperties', { read: TemplateRef }) private __DirectoryProperties!:TemplateRef<any>;
+  @ViewChild('renameDirectory', { read: TemplateRef }) private _RenameDirectory!:TemplateRef<any>;
+
 
   constructor(
     private _DataSvc: DataService,
     private _FileManagerSvc: FileManagerService,
     private _GWCommon: GWCommon,
+    private _FormBuilder: FormBuilder,
     private _LoggingSvc: LoggingService,
+    private _ModalSvc: ModalService,
     private _Router: Router
   ) { 
     // do nothing
@@ -48,6 +67,7 @@ export class DirectoryTreeComponent implements AfterViewInit, OnInit {
   ngOnInit(): void {
     this.configurationName = this._Router.url.split('?')[0] .replace('/', '').replace('\\','') + '_Directories';
     if(!this._GWCommon.isNullOrEmpty(this.configurationName)) {
+      this.populateRenameDirectoryForm()
       // logic to start getting data
       this._Subscriptions.add(this._DataSvc.dataChanged.subscribe((data: DataNVP) => {
         if(data.name.toLowerCase() === this.configurationName.toLowerCase()) {
@@ -79,7 +99,79 @@ export class DirectoryTreeComponent implements AfterViewInit, OnInit {
     }
   }
 
+  getErrorMessage(fieldName: string): string | undefined {
+    switch (fieldName) {
+      case 'newName':
+        if (this.getControls['newPassword'].hasError('required')) {
+          return 'Required';
+        }
+        break;
+      default:
+        break;
+    }
+    return undefined;
+  }
+
   hasChild = (_: number, node: IDirectoryTree) => !!node.children && node.children.length > 0;
+
+
+  onMenuDeleteClick(item: IDirectoryTree){
+    console.log('item', item);
+    const mModalOptions: ModalOptions = new ModalOptions('DirectoryTreeComponent.onMenuDeleteClick', 'Delete Directory', this._DeleteDirectory, new WindowSize(84, 300));
+    mModalOptions.buttons.okButton.visible = true;
+    mModalOptions.buttons.okButton.text = 'Yes';
+    mModalOptions.buttons.okButton.callbackMethod = () => {
+      this._ModalSvc.close('FileListComponent.onDeleteClick');
+    }
+    this._ModalSvc.open(mModalOptions);
+  }
+
+  onMenuPropertiesClick(item: IDirectoryTree) {
+    const mModalOptions: ModalOptions = new ModalOptions('DirectoryTreeComponent.onMenuPropertiesClick', 'Properties', this.__DirectoryProperties, new WindowSize(80, 600));
+    mModalOptions.buttons.okButton.visible = true;
+    mModalOptions.buttons.okButton.callbackMethod = () => {
+      this._ModalSvc.close('FileListComponent.onPropertiesClick');
+    }
+    this._ModalSvc.open(mModalOptions);
+  }
+
+  onMenuRenameClick(item: IDirectoryTree) {
+    console.log('item', item);
+    const mModalOptions: ModalOptions = new ModalOptions('DirectoryTreeComponent.onMenuRenameClick', 'Rename File', this._RenameDirectory, new WindowSize(84, 300));
+    this._ModalSvc.open(mModalOptions);
+  }
+
+  onRenameSubmit(form: FormGroup): void {
+
+  }
+
+  /**
+   * Method called when the user click with the right button
+   * @param event MouseEvent, it contains the coordinates
+   * @param item Our data contained in the row of the table
+   */
+  onRightClick(event: MouseEvent, item: any) {
+    console.log('item', item);
+    // preventDefault avoids to show the visualization of the right-click menu of the browser
+    event.preventDefault();
+
+    // we record the mouse position in our object
+    this.menuTopLeftPosition.x = event.clientX.toString();
+    this.menuTopLeftPosition.y = event.clientY.toString();
+
+    // we open the menu
+    // we pass to the menu the information about our object
+    this._MatMenuTrigger.menuData = { item: item }
+
+    // we open the menu
+    this._MatMenuTrigger.openMenu();
+  }
+
+  private populateRenameDirectoryForm(): void {
+    this.frmRenameDirectory = this._FormBuilder.group({
+      newFileName: ['', [Validators.required]],
+    });
+  }
 
   selectDirectory(node: IDirectoryTree): void {
     this.activeNode = node;
