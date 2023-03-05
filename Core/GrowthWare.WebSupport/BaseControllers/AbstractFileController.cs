d00f2c0ca466d09cdeb343fda6e6098d220d3a83
Kahiko <michael.regan@verizon.net>
2023-03-05 05:13:38 -0500

    Removed older devcode

--- Core/GrowthWare.WebSupport/BaseControllers/AbstractFileController.cs
+++ Core/GrowthWare.WebSupport/BaseControllers/AbstractFileController.cs
@@ -12,31 +12,6 @@ using GrowthWare.WebSupport.Utilities;
 
 namespace GrowthWare.WebSupport.BaseControllers;
 
-public class FileTreeManager
-{
-    private DirectoryInfo mDirectoryInfo;
-
-    public FileTreeManager(string aEntryPoint)
-    {
-        SetEntryPoint(aEntryPoint);
-    }
-
-    public void SetEntryPoint(string aEntryPoint)
-    {
-        this.mDirectoryInfo = new DirectoryInfo(aEntryPoint);
-    }
-
-    public override string ToString()
-    {
-        StringBuilder result = new StringBuilder();
-        foreach (DirectoryInfo dir in this.mDirectoryInfo.EnumerateDirectories())
-        {
-            result.Append("|-- " + dir.Name + Environment.NewLine);
-        }
-        return result.ToString();
-    }
-}
-
 [CLSCompliant(false)]
 public abstract class AbstractFileController : ControllerBase
 {
