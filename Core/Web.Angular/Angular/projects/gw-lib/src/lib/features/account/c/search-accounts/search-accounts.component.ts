9242bdf6f48092fde607aa45b7bdd26de03c8a23
Kahiko <michael.regan@verizon.net>
2023-03-07 06:34:47 -0500

    Droped the Growthware. from the directories but left it in code/namespace

---
+++ Core/Web.Angular/Angular/projects/gw-lib/src/lib/features/account/c/search-accounts/search-accounts.component.ts
@@ -0,0 +1,86 @@
+import { Component, OnDestroy, OnInit, ViewChild } from '@angular/core';
+import { Subscription } from 'rxjs';
+// Features (Components/Interfaces/Models/Services)
+import { DynamicTableComponent, DynamicTableService } from '@Growthware/Lib/src/lib/features/dynamic-table';
+import { DataService } from '@Growthware/Lib/src/lib/services';
+import { SearchService, SearchCriteriaNVP } from '@Growthware/Lib/src/lib/features/search';
+import { DynamicTableBtnMethods, INameValuePare } from '@Growthware/Lib/src/lib/models';
+import { ModalService, IModalOptions, ModalOptions, WindowSize } from '@Growthware/Lib/src/lib/features/modal';
+
+import { AccountDetailsComponent } from '../account-details/account-details.component';
+import { AccountService } from '../../account.service';
+
+@Component({
+  selector: 'gw-lib-search-accounts',
+  templateUrl: './search-accounts.component.html',
+  styleUrls: ['./search-accounts.component.scss']
+})
+export class SearchAccountsComponent implements OnDestroy, OnInit {
+  @ViewChild('dynamicTable', {static: false}) dynamicTable!: DynamicTableComponent;
+
+  private _SearchCriteriaChangedSub: Subscription = new Subscription();
+  private _WindowSize = new WindowSize(450,900);
+
+
+  public configurationName = 'Accounts';
+  public results: any;
+
+  constructor(
+    private _AccountSvc: AccountService,
+    private _DataSvc: DataService,
+    private _DynamicTableSvc: DynamicTableService,
+    private _ModalSvc: ModalService,
+    private _SearchSvc: SearchService,
+  ) { }
+
+  ngAfterViewInit(): void {
+    // Setup the dynamic table button methods
+    const mDynamicTableBtnMethods: DynamicTableBtnMethods = new DynamicTableBtnMethods();
+    mDynamicTableBtnMethods.btnTopRightCallBackMethod = () => { this.onBtnTopRight(); }
+    this.dynamicTable.setButtonMethods(mDynamicTableBtnMethods);
+    // setup the dynamic table "single" and "double" row clicks
+    this.dynamicTable.rowClickBackMethod = (rowNumber: number) => { this.onRowClick(rowNumber); };
+    this.dynamicTable.rowDoubleClickBackMethod = (rowNumber: number) => { this.onRowDoubleClick(rowNumber); };
+  }
+
+  ngOnInit(): void {
+    this._SearchCriteriaChangedSub = this._SearchSvc.searchCriteriaChanged.subscribe((criteria: INameValuePare) => {
+      if(criteria.name.trim().toLowerCase() === this.configurationName.trim().toLowerCase()) {
+        this._SearchSvc.searchAccounts(criteria).then((results) => {
+          this._DataSvc.notifySearchDataChanged(results.name, results.payLoad.data, results.payLoad.searchCriteria);
+        }).catch((error) => {
+          console.log(error);
+        });
+      }
+    });
+    // Get the initial SearchCriteriaNVP from the service
+    const mAccountTableConfig = this._DynamicTableSvc.getTableConfiguration(this.configurationName);
+    let mResults: SearchCriteriaNVP = this._SearchSvc.getSearchCriteriaFromConfig(this.configurationName, mAccountTableConfig);
+    // Set the search criteria to initiate search criteria changed subject
+    this._SearchSvc.setSearchCriteria(mResults.name, mResults.payLoad);
+  }
+
+  ngOnDestroy(): void {
+    this._SearchCriteriaChangedSub.unsubscribe();
+  }
+
+  private onBtnTopRight () {
+    this._AccountSvc.reason = 'NewProfile'
+    const mModalOptions: IModalOptions = new ModalOptions(this._AccountSvc.addModalId, 'Add Account', AccountDetailsComponent, this._WindowSize);
+    this._ModalSvc.open(mModalOptions);
+  }
+
+  private onRowClick (rowNumber: number): void {
+    // Do nothing ATM just leaving as an example
+    // const mMessage = 'hi from SearchAccountsComponent.onRowClick row "' + rowNumber + '" was clicked';
+    // this._LoggingSvc.toast(mMessage, 'onRowClick', LogLevel.Info);
+  }
+
+  private onRowDoubleClick (rowNumber: number): void {
+    const mDataRow: any = this.dynamicTable.getRowData(rowNumber);
+    this._AccountSvc.account = mDataRow.Account;
+    this._AccountSvc.reason = 'EditAccount';
+    const mModalOptions: IModalOptions = new ModalOptions(this._AccountSvc.editModalId, 'Edit Account', AccountDetailsComponent, this._WindowSize);
+    this._ModalSvc.open(mModalOptions);
+  }
+}
