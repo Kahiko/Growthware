ad918fb0137b4a6d486d6aa3af4f7b954ee4a67a
Kahiko <michael.regan@verizon.net>
2023-04-01 06:45:21 -0400

    Formatting and update throwerror

--- Core/Web.Angular/Angular/projects/gw-frontend/src/app/interceptors/error.interceptor.ts
+++ Core/Web.Angular/Angular/projects/gw-frontend/src/app/interceptors/error.interceptor.ts
@@ -16,17 +16,18 @@ export class ErrorInterceptor implements HttpInterceptor {
   intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
     return next.handle(request).pipe(catchError(err => {
       if (
-          [401, 403].includes(err.status)
-          && this._AccountSvc.account
-          && this._AccountSvc.account !== this._AccountSvc.defaultAccount
+        [401, 403].includes(err.status)
+        && this._AccountSvc.account
+        && this._AccountSvc.account !== this._AccountSvc.defaultAccount
       ) {
-          // auto logout if 401 or 403 response returned from api
-          this._AccountSvc.logout();
+        // auto logout if 401 or 403 response returned from api
+        this._AccountSvc.logout();
       }
 
       const error = (err && err.error && err.error.message) || err.statusText;
       console.error(err);
-      return throwError(error);
-  }))
+      // return throwError(error);
+      return throwError(() => error);
+    }));
   }
 }
