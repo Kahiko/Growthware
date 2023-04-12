4188427f189f26c6f82ae2267c7c10ecc62b823a
Kahiko <michael.regan@verizon.net>
2023-04-12 06:59:48 -0400

    Testing having the module and routing in the application

---
+++ Core/Web.Angular/Angular/projects/gw-frontend/src/app/features/accounts/accounts-routing.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+// Library MISC
+import { AuthGuard } from '@Growthware/Lib/src/lib/guards';
+// Library Components
+import { AccountDetailsComponent } from '@Growthware/Lib/src/lib/features/account/c/account-details/account-details.component';
+import { ChangePasswordComponent } from '@Growthware/Lib/src/lib/features/account/c/change-password/change-password.component';
+import { LoginComponent } from '@Growthware/Lib/src/lib/features/account/c/login/login.component';
+import { LogoutComponent } from '@Growthware/Lib/src/lib/features/account/c/logout/logout.component';
+import { SearchAccountsComponent } from '@Growthware/Lib/src/lib/features/account/c/search-accounts/search-accounts.component';
+
+const childRoutes: Routes = [
+  { path: '', component: SearchAccountsComponent, canActivate: [AuthGuard]},
+  { path: 'edit-account', component: AccountDetailsComponent, canActivate: [AuthGuard] },
+  { path: 'edit-my-account', component: AccountDetailsComponent, canActivate: [AuthGuard] },
+  { path: 'login', component: LoginComponent},
+  { path: 'change-password', component: ChangePasswordComponent },
+  { path: 'logout', component: LogoutComponent },
+];
+
+
+@NgModule({
+  imports: [RouterModule.forChild(childRoutes)],
+  exports: [RouterModule]
+})
+export class AccountsRoutingModule { }
