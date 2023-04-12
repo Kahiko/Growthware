b4dff6b837fb0e5d1023e2a0970c996d37e88629
Kahiko <michael.regan@verizon.net>
2023-04-12 06:58:34 -0400

    Testing having rounting in app not the library

--- Core/Web.Angular/Angular/projects/gw-frontend/src/app/app-routing.module.ts
+++ Core/Web.Angular/Angular/projects/gw-frontend/src/app/app-routing.module.ts
@@ -5,9 +5,10 @@ import { AuthGuard } from '@Growthware/Lib/src/lib/guards';
 
 const routes: Routes = [
   { path: '', loadChildren: () => import('./features/home/home.module').then(m => m.HomeModule) },
+  { path: 'search-accounts', loadChildren: () => import('./features/accounts/accounts.module').then(m => m.AccountsModule), canActivate: [AuthGuard]  },
+  // { path: 'search-accounts', loadChildren: () => import('@Growthware/Lib/src/lib/features/account').then(m => m.AccountModule), canActivate: [AuthGuard]  },
   { path: 'manage_cache_dependency', loadChildren: () => import('./features/file-manager/file-manager.module').then(m => m.FileManagerModule) },
   { path: 'manage_logs', loadChildren: () => import('./features/file-manager/file-manager.module').then(m => m.FileManagerModule) },
-  { path: 'search-accounts', loadChildren: () => import('@Growthware/Lib/src/lib/features/account').then(m => m.AccountModule), canActivate: [AuthGuard]  },
   { path: 'search-functions', loadChildren: () => import('./features/functions/functions.module').then(m => m.FunctionsModule), canActivate: [AuthGuard]  },
   { path: 'encryption-helper', loadChildren: () => import('@Growthware/Lib/src/lib/features/security').then(m => m.SecurityModule)  },
   { path: 'manage-groups', loadChildren: () => import('./features/groups/groups.module').then(m => m.GroupsModule), canActivate: [AuthGuard]  },
