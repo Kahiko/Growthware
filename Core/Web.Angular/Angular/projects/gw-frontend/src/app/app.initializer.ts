48425d5dd9d6549df9b9c8b52a705c566676a8a3
Kahiko <michael.regan@verizon.net>
2023-04-19 05:41:24 -0400

    Added debug msg

--- Core/Web.Angular/Angular/projects/gw-frontend/src/app/app.initializer.ts
+++ Core/Web.Angular/Angular/projects/gw-frontend/src/app/app.initializer.ts
@@ -1,14 +1,16 @@
 import { AccountService } from '@Growthware/Lib/src/lib/features/account';
-import { catchError, finalize, of } from 'rxjs';
+import { catchError, finalize, of, tap } from 'rxjs';
 
 export function appInitializer(accountSvc: AccountService) {
   return () => accountSvc.refreshToken()
-      .pipe(
-          // catch error to start app on success or failure
-          catchError((err) => {
-            // console.log(err);
-            // return nothing
-            return of();
-          })
-      );
+    .pipe(
+      tap(authenticationResponse => {  
+        // console.log('appInitializer.authenticationResponse', authenticationResponse);
+      }),
+      // catch error to start app on success or failure
+      catchError((err) => {
+        console.log(err);
+        return of();
+      })
+    );
 }
