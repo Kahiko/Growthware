a25505985743eccd380a1b35189ec018b7b4307c
Kahiko <michael.regan@verizon.net>
2023-08-18 21:48:32 -0400

    Changed icon to chevron_right

--- Core/Web.Angular/Angular/projects/gw-lib/features/navigation/c/vertical-list-item/vertical-list-item.component.ts
+++ Core/Web.Angular/Angular/projects/gw-lib/features/navigation/c/vertical-list-item/vertical-list-item.component.ts
@@ -12,8 +12,8 @@ import { MenuListService } from '../../menu-list.service';
   styleUrls: ['./vertical-list-item.component.scss'],
   animations: [
     trigger('indicatorRotate', [
-      state('collapsed', style({transform: 'rotate(270deg)'})),
-      state('expanded', style({transform: 'rotate(360deg)'})),
+      state('collapsed', style({transform: 'rotate(0deg)'})),
+      state('expanded', style({transform: 'rotate(90deg)'})),
       transition('expanded <=> collapsed',
         animate('225ms cubic-bezier(0.4,0.0,0.2,1)')
       )
