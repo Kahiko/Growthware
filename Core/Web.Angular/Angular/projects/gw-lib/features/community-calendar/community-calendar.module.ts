79be2ced524308c73d1ae2d3803c066188dc9c26
Kahiko <michael.regan@verizon.net>
2023-09-02 21:52:31 -0400

    now uses mat-button

--- Core/Web.Angular/Angular/projects/gw-lib/features/community-calendar/community-calendar.module.ts
+++ Core/Web.Angular/Angular/projects/gw-lib/features/community-calendar/community-calendar.module.ts
@@ -1,15 +1,22 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+// Angular Material
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+// Feature
 import { CalendarComponent } from './c/calendar/calendar.component';
 import { DayOfWeekComponent } from './c/day-of-week/day-of-week.component';
 
 @NgModule({
   declarations: [
     CalendarComponent,
-    DayOfWeekComponent
+    DayOfWeekComponent,
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    
+    MatButtonModule,
+    MatIconModule,
   ]
 })
 export class CommunityCalendarModule { }
