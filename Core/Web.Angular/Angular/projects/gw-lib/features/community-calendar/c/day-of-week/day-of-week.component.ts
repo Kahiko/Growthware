f8e1483f005792971c2dc7a1ea0e6cf400b6bc84
Kahiko <michael.regan@verizon.net>
2023-09-08 05:42:21 -0400

    Changed "date" to mini-fab Now display's the previous month if there is one

--- Core/Web.Angular/Angular/projects/gw-lib/features/community-calendar/c/day-of-week/day-of-week.component.ts
+++ Core/Web.Angular/Angular/projects/gw-lib/features/community-calendar/c/day-of-week/day-of-week.component.ts
@@ -16,6 +16,7 @@ export class DayOfWeekComponent implements OnInit {
     "July", "August", "September", "October", "November", "December"
   ];
   @Input() day?: any;
+  @Input() weekNumber?: number;
 
   constructor(
     private _GWCommon: GWCommon,
@@ -27,7 +28,7 @@ export class DayOfWeekComponent implements OnInit {
       this._LoggingService.toast('the "day" property is required', 'DayOfWeekComponent', LogLevel.Error);
     } else {
       this.day = new CalendarDay(new Date(this.day.value.date));
-      // console.log('DayOfWeek', this.day);
+      console.log('DayOfWeek', this.day);
     }
   }
 
