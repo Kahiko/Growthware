e745dfedcbfd9a5e0114460736c31ea37e66e93e
Kahiko <michael.regan@verizon.net>
2023-09-13 05:43:39 -0400

    Starting to add details to group-details

--- Core/Web.Angular/Angular/projects/gw-lib/features/group/c/group-details/group-details.component.ts
+++ Core/Web.Angular/Angular/projects/gw-lib/features/group/c/group-details/group-details.component.ts
@@ -1,10 +1,23 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+// Angular Material
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatTabsModule } from '@angular/material/tabs';
 
 @Component({
   selector: 'gw-lib-group-details',
   standalone: true,
-  imports: [CommonModule],
+  imports: [
+    CommonModule, 
+    FormsModule,
+    ReactiveFormsModule,
+    
+    MatButtonModule, 
+    MatIconModule, 
+    MatTabsModule
+  ],
   templateUrl: './group-details.component.html',
   styleUrls: ['./group-details.component.scss']
 })
