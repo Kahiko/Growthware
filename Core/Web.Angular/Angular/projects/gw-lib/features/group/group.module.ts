6e49e635370b41a27bec0de899f6498b16a33d9c
Kahiko <michael.regan@verizon.net>
2023-09-13 04:30:14 -0400

    removed commented code

--- Core/Web.Angular/Angular/projects/gw-lib/features/group/group.module.ts
+++ Core/Web.Angular/Angular/projects/gw-lib/features/group/group.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-// import { GroupDetailsComponent } from './c/group-details/group-details.component';
 import { SearchGroupsComponent } from './c/search-groups/search-groups.component';
 // Library
 import { DynamicTableModule } from '@Growthware/features/dynamic-table';
@@ -9,7 +8,6 @@ import { PickListModule } from '@Growthware/features/pick-list';
 
 @NgModule({
   declarations: [
-    // GroupDetailsComponent,
     SearchGroupsComponent,
   ],
   imports: [
