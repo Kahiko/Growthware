d8eeac67b84b7c12ce26a4cdc8d0c080dd93e225
Kahiko <michael.regan@verizon.net>
2023-09-24 10:54:34 -0400

    Converted FunctionDetails to standalone

--- Core/Web.Angular/Angular/projects/gw-lib/features/function/c/function-details/function-details.component.ts
+++ Core/Web.Angular/Angular/projects/gw-lib/features/function/c/function-details/function-details.component.ts
@@ -1,15 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'gw-lib-function-details',
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './function-details.component.html',
   styleUrls: ['./function-details.component.scss']
 })
-export class FunctionDetailsComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
+export class FunctionDetailsComponent {
 
 }
--- Core/Web.Angular/Angular/projects/gw-lib/features/function/function.module.ts
+++ Core/Web.Angular/Angular/projects/gw-lib/features/function/function.module.ts
@@ -3,14 +3,12 @@ import { CommonModule } from '@angular/common';
 // Library
 import { DynamicTableModule } from '@Growthware/features/dynamic-table';
 import { PickListModule } from '@Growthware/features/pick-list';
-
-import { FunctionDetailsComponent } from './c/function-details/function-details.component';
+// Feature
 import { SearchfunctionsComponent } from './c/searchfunctions/searchfunctions.component';
 import { CopyFunctionSecurityComponent } from './c/copy-function-security/copy-function-security.component';
 
 @NgModule({
   declarations: [
-    FunctionDetailsComponent,
     SearchfunctionsComponent,
     CopyFunctionSecurityComponent
   ],
