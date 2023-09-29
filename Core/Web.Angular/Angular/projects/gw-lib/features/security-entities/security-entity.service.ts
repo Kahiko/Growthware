ba494f41f1a2abc2de8c828d062e5e5a69a942fe
Kahiko <michael.regan@verizon.net>
2023-09-29 03:27:53 -0400

    Removed unused imports

--- Core/Web.Angular/Angular/projects/gw-lib/features/security-entities/security-entity.service.ts
+++ Core/Web.Angular/Angular/projects/gw-lib/features/security-entities/security-entity.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 // Library
 import { GWCommon } from '@Growthware/common-code';
 import { LoggingService } from '@Growthware/features/logging';
