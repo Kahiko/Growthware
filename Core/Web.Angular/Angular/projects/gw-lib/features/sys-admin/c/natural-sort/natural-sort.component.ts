698e1455cb72cd2e96473e265fcafce5bfa1387a
Kahiko <michael.regan@verizon.net>
2023-10-03 08:36:59 -0400

    Test natural sort now has drop down to change sort direction

--- Core/Web.Angular/Angular/projects/gw-lib/features/sys-admin/c/natural-sort/natural-sort.component.ts
+++ Core/Web.Angular/Angular/projects/gw-lib/features/sys-admin/c/natural-sort/natural-sort.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 // Angular Material
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatSelectModule } from '@angular/material/select';
 import { MatTableModule } from '@angular/material/table';
 // Library
 import { FileManagerService } from '@Growthware/features/file-manager';
@@ -12,6 +14,8 @@ import { LoggingService } from '@Growthware/features/logging';
   imports: [
     CommonModule,
   
+    MatSelectModule,
+    MatFormFieldModule,
     MatTableModule
   ],
   templateUrl: './natural-sort.component.html',
@@ -19,22 +23,21 @@ import { LoggingService } from '@Growthware/features/logging';
 })
 export class NaturalSortComponent implements OnInit {
 
-  // dataTableSource: any[] = [
-  //   {col1: 1, col2: 'Hydrogen', weight: 1.0079, symbol: 'H'},
-  //   {col1: 2, col2: 'Helium', weight: 4.0026, symbol: 'He'},
-  //   {col1: 3, col2: 'Lithium', weight: 6.941, symbol: 'Li'},
-  //   {col1: 4, col2: 'Beryllium', weight: 9.0122, symbol: 'Be'},
-  //   {col1: 5, col2: 'Boron', weight: 10.811, symbol: 'B'},
-  //   {col1: 6, col2: 'Carbon', weight: 12.0107, symbol: 'C'},
-  //   {col1: 7, col2: 'Nitrogen', weight: 14.0067, symbol: 'N'},
-  //   {col1: 8, col2: 'Oxygen', weight: 15.9994, symbol: 'O'},
-  //   {col1: 9, col2: 'Fluorine', weight: 18.9984, symbol: 'F'},
-  //   {col1: 10, col2: 'Neon', weight: 20.1797, symbol: 'Ne'},
-  // ];
   dataTableSource: any[] = [];
-  
+  dataViewSource: any[] = [];
   displayedColumns: string[] = ['col1', 'col2'];
-  dataViewSource: any[] = [{coL1: '', coL2: ''}];
+
+  startTime: string = '';
+  stopTime: string = '';
+
+  selectedSortDirection: string = 'ASC';
+
+  totalMilliseconds: string = '';
+
+  validSortDirections = [
+    { id: 'ASC', text: "Asending" },
+    { id: 'DESC', text: "Desending" },
+  ];
 
   constructor(
     private _FileManagerSvc: FileManagerService,
@@ -42,13 +45,20 @@ export class NaturalSortComponent implements OnInit {
   ) { }
 
   ngOnInit(): void {
-    this._FileManagerSvc.getTestNaturalSort('ASC').then((response: any) => {
+    this.getData();
+  }
+
+  getData(): void {
+    this._FileManagerSvc.getTestNaturalSort(this.selectedSortDirection).then((response: any) => {
       // console.log('NaturalSortComponent.ngOnInit',response.dataTable);
       this.dataTableSource = response.dataTable;
       this.dataViewSource = response.dataView;
+      this.startTime = response.startTime;
+      this.stopTime = response.stopTime;
+      this.totalMilliseconds = response.totalMilliseconds;
     }).catch((error) => {
       this._LoggingSvc.errorHandler(error, 'FileManagerService', 'getTestNaturalSort');
-    })
+    })    
   }
 
 }
