2edc0a52a89a7ee869b4a7db00b61ca3f2384942
Kahiko <michael.regan@verizon.net>
2023-10-06 23:57:12 -0400

    WIP - Starting on populating the menu order tab

---
+++ Core/Framework/Models/UI/UIFunctionMenuOrder.cs
@@ -0,0 +1,8 @@
+namespace GrowthWare.Framework.Models.UI;
+
+public class UIFunctionMenuOrder
+{
+    public int Function_Seq_Id{set; get;}
+    public string Action{set; get;}
+    public string Name{set; get;}
+}
