70d6ae37acc1d97225fc756c49ba103f512ee759
Kahiko <michael.regan@verizon.net>
2023-10-14 06:46:55 -0400

    Stating to look into caching

--- Core/Web.Support/CacheController.cs
+++ Core/Web.Support/CacheController.cs
@@ -1,14 +1,17 @@
 using Microsoft.AspNetCore.Http;
 using System;
+using GrowthWare.Framework.Enumerations;
+using System.Text.Json;
 
 namespace GrowthWare.Web.Support;
-
 /// <summary>
 /// Facade for System.Web.Caching
 /// </summary>
 [CLSCompliant(false)]
 public class CacheController
 {
+    // TODO: Cache has not been implemented we are just using session atm and should be doing something with
+    // "Cache in-memory in ASP.NET Core" (https://learn.microsoft.com/en-us/aspnet/core/performance/caching/memory?view=aspnetcore-3.1) perhaps 
     static HttpContext m_HttpContext = null;
 
     public CacheController(HttpContext httpContext)
@@ -18,13 +21,30 @@ public class CacheController
 
     public static bool AddToCacheDependency(string key, object value)
     {
-        bool mRetVal = false;
-
+        bool mRetVal = true;
+        string mJsonString = JsonSerializer.Serialize(value);
+        m_HttpContext.Session.SetString(key, mJsonString);
         return mRetVal;
     }
 
-    // public static void CheckCallback(string key, object value, CacheItemRemovedReason reason)
-    // {
-    //     m_HttpContext.Cache.Add();
-    // }
+    public static void CheckCallback(string key, object value, CacheItemRemovedReason reason)
+    {
+
+    }
+
+    public static T GetFromCache<T>(string key)
+    {
+        string mJsonString = m_HttpContext.Session.GetString(key);
+        var mRetVal = JsonSerializer.Deserialize<T>(mJsonString);
+        return mRetVal;
+    }
+
+    public static void RemoveAllCache()
+    {
+
+    }
+    public static void RemoveFromCache(string cacheName)
+    {
+
+    }
 }
