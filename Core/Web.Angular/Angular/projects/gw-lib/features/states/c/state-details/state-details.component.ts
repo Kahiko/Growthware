141a94f83b8ec105e7c2836efbb5cda7264c60b3
Kahiko <michael.regan@verizon.net>
2023-10-16 04:04:08 -0400

    Converted state details to standalone

--- Core/Web.Angular/Angular/projects/gw-lib/features/states/c/state-details/state-details.component.ts
+++ Core/Web.Angular/Angular/projects/gw-lib/features/states/c/state-details/state-details.component.ts
@@ -1,7 +1,10 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'gw-lib-state-details',
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './state-details.component.html',
   styleUrls: ['./state-details.component.scss']
 })
--- Core/Web.Angular/Angular/projects/gw-lib/features/states/states.module.ts
+++ Core/Web.Angular/Angular/projects/gw-lib/features/states/states.module.ts
@@ -3,12 +3,10 @@ import { CommonModule } from '@angular/common';
 // Library
 import { DynamicTableModule } from '@Growthware/features/dynamic-table';
 // Feature
-import { StateDetailsComponent } from './c/state-details/state-details.component';
 import { SearchStatesComponent } from './c/search-states/search-states.component';
 
 @NgModule({
   declarations: [
-    StateDetailsComponent,
     SearchStatesComponent
   ],
   imports: [
