e8aee24a753373b551faae1fe40901fe15b7677c
Kahiko <michael.regan@verizon.net>
2023-10-18 06:37:05 -0400

    Converted CopyFunctionSecurityComponent to standalone

--- Core/Web.Angular/Angular/projects/gw-lib/features/function/c/copy-function-security/copy-function-security.component.ts
+++ Core/Web.Angular/Angular/projects/gw-lib/features/function/c/copy-function-security/copy-function-security.component.ts
@@ -1,7 +1,12 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'gw-lib-copy-function-security',
+  standalone: true,
+  imports: [
+    CommonModule
+  ],
   templateUrl: './copy-function-security.component.html',
   styleUrls: ['./copy-function-security.component.scss']
 })
--- Core/Web.Angular/Angular/projects/gw-lib/features/function/function.module.ts
+++ Core/Web.Angular/Angular/projects/gw-lib/features/function/function.module.ts
@@ -5,12 +5,10 @@ import { DynamicTableModule } from '@Growthware/features/dynamic-table';
 import { PickListModule } from '@Growthware/features/pick-list';
 // Feature
 import { SearchfunctionsComponent } from './c/searchfunctions/searchfunctions.component';
-import { CopyFunctionSecurityComponent } from './c/copy-function-security/copy-function-security.component';
 
 @NgModule({
   declarations: [
     SearchfunctionsComponent,
-    CopyFunctionSecurityComponent,
   ],
   imports: [
     CommonModule,
