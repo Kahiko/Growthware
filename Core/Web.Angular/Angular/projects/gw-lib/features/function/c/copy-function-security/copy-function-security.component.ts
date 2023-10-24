0596e312f5f3ea0c0ba38cef11d16a999ac8a34c
Kahiko <michael.regan@verizon.net>
2023-10-24 18:19:31 -0400

    Form now populates

--- Core/Web.Angular/Angular/projects/gw-lib/features/function/c/copy-function-security/copy-function-security.component.ts
+++ Core/Web.Angular/Angular/projects/gw-lib/features/function/c/copy-function-security/copy-function-security.component.ts
@@ -4,12 +4,15 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { FormBuilder, FormGroup } from '@angular/forms';
 // Angular Material
 import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
 import { MatSelectModule } from '@angular/material/select';
 // Library
+import { GWCommon } from '@Growthware/common-code';
 import { LoggingService, LogLevel } from '@Growthware/features/logging';
-import { IKeyValuePair } from '@Growthware/shared/models';
+import { IKeyValuePair, KeyValuePair } from '@Growthware/shared/models';
+import { SecurityEntityService } from '@Growthware/features/security-entities';
 // Feature
 
 
@@ -22,6 +25,7 @@ import { IKeyValuePair } from '@Growthware/shared/models';
     ReactiveFormsModule,
 
     MatButtonModule,
+    MatFormFieldModule,
     MatIconModule, 
     MatInputModule,
     MatSelectModule,
@@ -35,37 +39,36 @@ export class CopyFunctionSecurityComponent implements OnInit {
   selectedSource: number = -1;
   selectedTarget: number = -1;
 
-  validSecurityEntities: IKeyValuePair[] = [];
+  validSourceEntities: IKeyValuePair[] = [];
+  validTargetEntities: IKeyValuePair[] = [];
 
   constructor(
     private _FormBuilder: FormBuilder,
     private _LoggingSvc: LoggingService,
+    private _SecuritySvc: SecurityEntityService
   ) { 
     this.createForm();
   }
 
   ngOnInit(): void {
-    this._LoggingSvc.toast('Save not working yet', 'Copy Function:', LogLevel.Error);
-  }
-
-  createForm(): void {
-    this.frmProfile = this._FormBuilder.group({
-
+    this._SecuritySvc.getValidParents(1).then((securityEntities) => {
+      this.validSourceEntities = JSON.parse(JSON.stringify(securityEntities));
+      this.validTargetEntities = JSON.parse(JSON.stringify(securityEntities));
+      const mSystem: IKeyValuePair = new KeyValuePair();
+      mSystem.key = 1;
+      mSystem.value = 'System';
+      this.validSourceEntities.push(mSystem);
+      this.validSourceEntities = GWCommon.sortArray(this.validSourceEntities, 'value', 'desc');
+    }).catch((error: any) => {
+      this._LoggingSvc.errorHandler(error, 'CopyFunctionSecurityComponent', 'ngOnInit');
     });
   }
 
+  createForm(): void {
+    this.frmProfile = this._FormBuilder.group({});
+  }
+
   onSubmit(form: FormGroup): void {
-    // this._LoggingSvc.toast('BaseDetailComponent.onSubmit', 'Function Details:', LogLevel.Error);
-    this.populateProfile();
-    this.save();
+    this._LoggingSvc.toast('Save is not working yet', 'Copy Function:', LogLevel.Error);
   }
-
-  private populateProfile(): void {
-    
-  }
-
-  private save(): void {
-    
-  }
-
 }
