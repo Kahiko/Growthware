b4868b4e670eeedb008ae6f6cb202e59d0f4bfa4
Kahiko <michael.regan@verizon.net>
2023-10-29 08:14:04 -0400

    Removed unused usings Changed return columns in SearchNameValuePairs

--- Core/Web.Support/BaseControllers/AbstractNameValuePairController.cs
+++ Core/Web.Support/BaseControllers/AbstractNameValuePairController.cs
@@ -1,16 +1,10 @@
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using System;
-using System.Collections.Generic;
 using GrowthWare.Framework;
 using GrowthWare.Framework.Models;
 using GrowthWare.Framework.Models.UI;
-using GrowthWare.Web.Support.Services;
 using GrowthWare.Web.Support.Jwt;
 using GrowthWare.Web.Support.Utilities;
-using GrowthWare.Framework.Enumerations;
-using System.Data;
-using System.Linq;
 
 namespace GrowthWare.Web.Support.BaseControllers;
 
@@ -24,7 +18,7 @@ public abstract class AbstractNameValuePairController : ControllerBase
     public String SearchNameValuePairs(UISearchCriteria searchCriteria)
     {
         String mRetVal = string.Empty;
-        string mColumns = "[NVPSeqId], [Schema_Name], [Static_Name], [Display], [Description], [StatusSeqId], [Added_By], [Added_Date], [Updated_By], [Updated_Date]";
+        string mColumns = "[NVPSeqId], [Schema_Name], [Static_Name], [Display], [Description], [StatusSeqId]";
         if(searchCriteria.sortColumns.Length > 0)
         {
             Tuple<string, string> mOrderByAndWhere = SearchUtility.GetOrderByAndWhere(mColumns, searchCriteria.searchColumns, searchCriteria.sortColumns, searchCriteria.searchText);
@@ -44,4 +38,29 @@ public abstract class AbstractNameValuePairController : ControllerBase
         }
         return mRetVal;
     }
+
+     [HttpPost("SearcNVPDetails")]
+    public String SearcNVPDetails(UISearchCriteria searchCriteria)
+    {
+        String mRetVal = string.Empty;
+        string mColumns = "[NVP_DetailSeqId], [NVPSeqId], [NVP_Detail_Name], [NVP_Detail_Value], [StatusSeqId], [Sort_Order], [Added_By], [ADDED_DATE], [Updated_By], [UPDATED_DATE]";
+        if(searchCriteria.sortColumns.Length > 0)
+        {
+            Tuple<string, string> mOrderByAndWhere = SearchUtility.GetOrderByAndWhere(mColumns, searchCriteria.searchColumns, searchCriteria.sortColumns, searchCriteria.searchText);
+            string mOrderByClause = mOrderByAndWhere.Item1;
+            string mWhereClause = mOrderByAndWhere.Item2;
+            MSearchCriteria mSearchCriteria = new MSearchCriteria
+            {
+                Columns = mColumns,
+                OrderByClause = mOrderByClause,
+                PageSize = searchCriteria.pageSize,
+                SelectedPage = searchCriteria.selectedPage,
+                TableOrView = "[ZGWSystem].[Name_Value_Pairs]",
+                WhereClause = mWhereClause
+            };
+
+            mRetVal = SearchUtility.GetSearchResults(mSearchCriteria);
+        }
+        return mRetVal;
+    }   
 }
