09ed5c01fc9f527228349df270dc84a7fad496a3
Kahiko <michael.regan@verizon.net>
2023-11-03 06:34:42 -0400

    Fixed [ZGWSystem].[Get_Name_Value_Pair_Detail] columns were incorrect

--- Core/DatabaseManager/Scripts/Downgrade/SQLServer/Version_3.0.0.0.sql
+++ Core/DatabaseManager/Scripts/Downgrade/SQLServer/Version_3.0.0.0.sql
@@ -416,6 +416,59 @@ IF EXISTS (
 --END IF
 GO
 /****** End:  UserDefinedFunction [ZGWSecurity].[udfSplit]    Script Date: 10/21/2021 4:13:21 PM ******/
+/****** Start: StoredProcedure [ZGWSystem].[Get_Name_Value_Pair_Detail] ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+IF NOT EXISTS (SELECT *
+FROM sys.objects
+WHERE object_id = OBJECT_ID(N'[ZGWSystem].[Get_Name_Value_Pair_Detail]') AND type in (N'P', N'PC'))
+BEGIN
+	EXEC dbo.sp_executesql @statement = N'CREATE PROCEDURE [ZGWSystem].[Get_Name_Value_Pair_Detail] AS'
+END
+GO
+
+/*
+Usage:
+	DECLARE
+		@P_NVPSeqId INT = 1,
+		@P_NVP_DetailSeqId INT = 1,
+		@P_Debug INT = 1
+
+	exec ZGWSystem.Get_Name_Value_Pair_Detail
+		@P_NVPSeqId,
+		@P_NVP_DetailSeqId,
+		@P_Debug
+*/
+-- =============================================
+-- Author:		Michael Regan
+-- Create date: 09/11/2011
+-- Description:	Returns name value pair detail
+-- Note:
+--	This not the most effecient however this should
+--	not be called very often ... it is intended for the
+--	front end to cache the information and only get called
+--	when needed
+-- =============================================
+ALTER PROCEDURE [ZGWSystem].[Get_Name_Value_Pair_Detail]
+	@P_NVPSeqId INT,
+	@P_NVP_DetailSeqId INT,
+	@P_Debug INT = 0
+AS
+	DECLARE @V_TableName VARCHAR(30)
+	DECLARE @V_Statement nvarchar(4000)
+	SET @V_TableName = (SELECT [Schema_Name] + '.' + Static_Name
+FROM ZGWSystem.Name_Value_Pairs
+WHERE NVPSeqId = @P_NVPSeqId)
+	SET @V_Statement = 'SELECT NVPSeqId as NVP_SEQ_ID, [Schema_Name], Static_Name, Display, Description, StatusSeqId as STATUS_SEQ_ID, Added_By, Added_Date, Updated_By, Updated_Date FROM ' + CONVERT(VARCHAR,@V_TableName) + '
+	WHERE
+		NVP_DetailSeqId = ' + CONVERT(VARCHAR,@P_NVP_DetailSeqId) + ' ORDER BY Static_Name'
+
+	EXECUTE dbo.sp_executesql @statement = @V_Statement
+RETURN 0
+GO
+/****** End: StoredProcedure [ZGWSystem].[Get_Name_Value_Pair_Detail] ******/
 
 SET ANSI_NULLS ON
 GO
