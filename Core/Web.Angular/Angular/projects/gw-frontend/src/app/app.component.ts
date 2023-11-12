7a700a5ea179cd20c00fbde28a120fb293b98016
Kahiko <michael.regan@verizon.net>
2023-11-12 08:45:19 -0500

    Skin now changes with selected SecurityEntity

--- Core/Web.Angular/Angular/projects/gw-frontend/src/app/app.component.ts
+++ Core/Web.Angular/Angular/projects/gw-frontend/src/app/app.component.ts
@@ -1,18 +1,43 @@
-import { Component } from '@angular/core';
-import { BehaviorSubject } from 'rxjs';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { BehaviorSubject, Subscription } from 'rxjs';
+// Library
+import { AccountService, IClientChoices, SecurityEntityService } from '@Growthware';
 
 @Component({
   selector: 'gw-frontend-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnDestroy, OnInit {
 
   private _Skin: BehaviorSubject<string> = new BehaviorSubject<string>('default');
+  private _Subscription: Subscription = new Subscription();
 
   readonly skin$ = this._Skin.asObservable();
   // skin = 'default';
   title = 'gw-frontend';
 
-  constructor() {}
+  constructor(
+    private _AccountSvc: AccountService,
+    private _SecurityEntitySvc: SecurityEntityService
+  ) {}
+
+  ngOnDestroy(): void {
+    this._Subscription.unsubscribe();
+  }
+
+  ngOnInit(): void {
+    this._Subscription.add(
+      this._AccountSvc.clientChoices$.subscribe((val: IClientChoices) => {
+        this.setSkin(val);
+      })
+    );
+  }
+
+  private setSkin(clientChoices: IClientChoices): void {
+    this._SecurityEntitySvc.getSecurityEntity(clientChoices.securityEntityID).then((response: any) => {
+      // console.log('AppComponent.setSkin.response', response);
+      this._Skin.next(response.skin.toLowerCase());
+    })
+  }
 }
