96c49ba155b00d04da416cd89b9447251d497817
Kahiko <michael.regan@verizon.net>
2023-11-13 05:30:14 -0500

    WIP - Arc header looks about right

--- Core/Web.Angular/Angular/projects/gw-frontend/src/app/skins/arc/arc-header/arc-header.component.ts
+++ Core/Web.Angular/Angular/projects/gw-frontend/src/app/skins/arc/arc-header/arc-header.component.ts
@@ -1,10 +1,82 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Subscription } from 'rxjs';
+// Library
+import { AccountService, IAuthenticationResponse, IClientChoices } from '@Growthware/features/account';
+import { GWCommon } from '@Growthware/common-code';
+import { ConfigurationService } from '@Growthware/features/configuration';
+// import { LoginComponent } from '@Growthware/features/account/c/login/login.component';
+// import { ModalService, ModalOptions, WindowSize } from '@Growthware/features/modal';
+import { INavLink, NavigationService } from '@Growthware/features/navigation';
+// Skin
 
 @Component({
   selector: 'gw-frontend-arc-header',
   templateUrl: './arc-header.component.html',
   styleUrls: ['./arc-header.component.scss']
 })
-export class ArcHeaderComponent {
+export class ArcHeaderComponent implements OnDestroy, OnInit {
+  private _Subscription: Subscription = new Subscription();
+
+  accountName: string = '';
+  applicationName: string = '';
+  environment: string = 'Development';
+  navDescription: string = '';
+  securityEntity: string = '';
+  securityEntityTranslation: string = '';
+  version: string = '';
+
+  constructor(
+    private _AccountSvc: AccountService,
+    private _ConfigurationSvc: ConfigurationService,
+    private _GWCommon: GWCommon,
+    // private _ModalSvc: ModalService,
+    private _NavigationSvc: NavigationService,
+    private _Router: Router
+  ) {
+    // do nothing atm
+  }
+  ngOnDestroy(): void {
+    this._Subscription.unsubscribe();
+  }
+
+  ngOnInit(): void {
+    this._Subscription.add(
+      this._ConfigurationSvc.securityEntityTranslation$.subscribe((val: string) => { this.securityEntityTranslation = val;})
+    );    
+    this._Subscription.add(
+      this._ConfigurationSvc.applicationName$.subscribe((val: string) => { this.applicationName = val; })
+    );
+    this._Subscription.add(
+      this._ConfigurationSvc.version$.subscribe((val: string) => { this.version = val; })
+    );
+    this._Subscription.add(
+      this._AccountSvc.authenticationResponse$.subscribe((val: IAuthenticationResponse) => {
+        // this.isAuthenticated = val.account.toLowerCase() != this._AccountSvc.defaultAccount.toLowerCase();
+        this.accountName = this._GWCommon.formatData(val.account, 'text:28');
+      })
+    );
+    this._Subscription.add(
+      this._AccountSvc.clientChoices$.subscribe((val: IClientChoices) => { this.securityEntity = val.securityEntityName; })
+    );
+    this._Subscription.add(
+      this._NavigationSvc.currentNavLink$.subscribe((val: INavLink) => { 
+        // console.log('ArcHeaderComponent.ngOnInit.description', val.description);
+        if(val.description.length > 0) {
+          this.navDescription = val.description; 
+        } else {
+          this.navDescription = 'Home';
+        }
+      })
+    );
+  }
+
+  onLogoClick(): void {
+    if(this._AccountSvc.authenticationResponse.account.trim().toLocaleLowerCase() !== this._AccountSvc.defaultAccount.trim().toLocaleLowerCase()) {
+      this._Router.navigate(['home']);
+    } else {
+      this._Router.navigate(['generic_home']);
+    }
+  }
 
 }
--- Core/Web.Angular/Angular/projects/gw-frontend/src/app/skins/arc/arc.module.ts
+++ Core/Web.Angular/Angular/projects/gw-frontend/src/app/skins/arc/arc.module.ts
@@ -1,11 +1,26 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+// Angular Material
+import { MatButtonModule } from '@angular/material/button';
+// import { MatDividerModule } from '@angular/material/divider';
+// import { MatIconModule } from '@angular/material/icon';
+// import { MatListModule } from '@angular/material/list';
+// import { MatMenuModule } from '@angular/material/menu';
+// import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatToolbarModule } from '@angular/material/toolbar';
+// Library Modules
+import { LoaderModule } from '@Growthware/features/loader';
+import { NavigationModule } from '@Growthware/features/navigation';
+// Library Standalone
+import { HorizontalComponent } from '@Growthware/features/navigation';
+// import { HierarchicalHorizontalComponent } from '@Growthware/features/navigation';
+// import { HierarchicalVerticalComponent } from '@Growthware/features/navigation';
+// import { VerticalComponent } from '@Growthware/features/navigation';
+// Modules/Components
 import { ArcRoutingModule } from './arc-routing.module';
 import { ArcFooterComponent } from './arc-footer/arc-footer.component';
 import { ArcHeaderComponent } from './arc-header/arc-header.component';
 import { ArcLayoutComponent } from './arc-layout/arc-layout.component';
-
 
 @NgModule({
   declarations: [
@@ -15,7 +30,21 @@ import { ArcLayoutComponent } from './arc-layout/arc-layout.component';
   ],
   imports: [
     CommonModule,
-    ArcRoutingModule
+    // Skin Modules
+    ArcRoutingModule,
+    // Angular Material Modules
+    MatButtonModule,
+    // MatDividerModule,
+    // MatIconModule,
+    // MatListModule,
+    // MatMenuModule,
+    // MatSidenavModule,
+    MatToolbarModule,
+
+    // Library Modules
+    HorizontalComponent,
+    LoaderModule,
+    NavigationModule
   ],
   exports: [
     ArcLayoutComponent
