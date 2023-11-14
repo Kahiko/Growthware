417e1a9152dd5ad873857da4c3ae0e3e39f3772f
Kahiko <michael.regan@verizon.net>
2023-11-14 05:18:22 -0500

    WIP - Arc

--- Core/Web.Angular/Angular/projects/gw-frontend/src/app/skins/arc/arc.module.ts
+++ Core/Web.Angular/Angular/projects/gw-frontend/src/app/skins/arc/arc.module.ts
@@ -2,9 +2,9 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 // Angular Material
 import { MatButtonModule } from '@angular/material/button';
-// import { MatDividerModule } from '@angular/material/divider';
+import { MatDividerModule } from '@angular/material/divider';
 // import { MatIconModule } from '@angular/material/icon';
-// import { MatListModule } from '@angular/material/list';
+import { MatListModule } from '@angular/material/list';
 // import { MatMenuModule } from '@angular/material/menu';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatToolbarModule } from '@angular/material/toolbar';
@@ -14,8 +14,8 @@ import { NavigationModule } from '@Growthware/features/navigation';
 // Library Standalone
 import { HorizontalComponent } from '@Growthware/features/navigation';
 // import { HierarchicalHorizontalComponent } from '@Growthware/features/navigation';
-// import { HierarchicalVerticalComponent } from '@Growthware/features/navigation';
-// import { VerticalComponent } from '@Growthware/features/navigation';
+import { HierarchicalVerticalComponent } from '@Growthware/features/navigation';
+import { VerticalComponent } from '@Growthware/features/navigation';
 // Modules/Components
 import { ArcRoutingModule } from './arc-routing.module';
 import { ArcFooterComponent } from './arc-footer/arc-footer.component';
@@ -34,15 +34,18 @@ import { ArcLayoutComponent } from './arc-layout/arc-layout.component';
     ArcRoutingModule,
     // Angular Material Modules
     MatButtonModule,
-    // MatDividerModule,
+    MatDividerModule,
     // MatIconModule,
-    // MatListModule,
+    MatListModule,
     // MatMenuModule,
     MatSidenavModule,
     MatToolbarModule,
 
     // Library Modules
     HorizontalComponent,
+    // HierarchicalHorizontalComponent,
+    HierarchicalVerticalComponent,
+    VerticalComponent,    
     LoaderModule,
     NavigationModule
   ],
--- Core/Web.Angular/Angular/projects/gw-lib/features/account/account.service.ts
+++ Core/Web.Angular/Angular/projects/gw-lib/features/account/account.service.ts
@@ -423,6 +423,7 @@ export class AccountService {
     .pipe(
       map((response) => {
         this._AuthenticationResponseSubject.next(response);
+        this.getClientChoices();
         // this.getClientChoices().then((clientChoices) => {
         //   // TODO: I don't think this is necessary, should delete after testing
         //   // this._Router.navigate([clientChoices.action.toLowerCase()]);
