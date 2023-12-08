37df7bc663327def9d610fb5c3c42f61e3418787
kahiko <michael.regan@verizon.net>
2023-12-08 03:32:23 -0500

    Formatting

--- Core/Web.Angular/Angular/projects/gw-frontend/src/app/interceptors/error.interceptor.ts
+++ Core/Web.Angular/Angular/projects/gw-frontend/src/app/interceptors/error.interceptor.ts
@@ -15,10 +15,7 @@ export class ErrorInterceptor implements HttpInterceptor {
 
   intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
     return next.handle(request).pipe(catchError(err => {
-      if (
-        [401, 403].includes(err.status)
-        && this._AccountSvc.authenticationResponse.account !== this._AccountSvc.anonymous
-      ) {
+      if ([401, 403].includes(err.status) && this._AccountSvc.authenticationResponse.account !== this._AccountSvc.anonymous) {
         // auto logout if 401 or 403 response returned from api
         this._AccountSvc.logout();
       }
