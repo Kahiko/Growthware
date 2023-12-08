b8b23769ea8add61dc6e02a86aaeb17e8c27004c
kahiko <michael.regan@verizon.net>
2023-12-08 04:01:27 -0500

    Temporarily removing refreshFromToken

--- Core/Web.Angular/Angular/projects/gw-frontend/src/app/app.initializer.ts
+++ Core/Web.Angular/Angular/projects/gw-frontend/src/app/app.initializer.ts
@@ -1,21 +1,27 @@
-import { catchError, of, tap } from 'rxjs';
+// import { catchError, of, tap } from 'rxjs';
 import { AccountService } from '@Growthware/features/account';
 import { ConfigurationService } from '@Growthware/features/configuration';
 
 export function appInitializer(accountSvc: AccountService, configurationSvc: ConfigurationService) {
-  configurationSvc.loadAppSettings();
-  return () => accountSvc.refreshFromToken()
-    .pipe(
-      tap(authenticationResponse => {  
-        // console.log('appInitializer.authenticationResponse', authenticationResponse);
-        if(authenticationResponse.account.toLocaleLowerCase() !== accountSvc.anonymous.toLocaleLowerCase()) {
-          accountSvc.startRefreshTokenTimer();
-        }
-      }),
-      // catch error to start app on success or failure
-      catchError((err) => {
-        console.log(err);
-        return of();
-      })
-    );
+  return () => {
+    return new Promise((resolve) => {
+      configurationSvc.loadAppSettings();
+      resolve(true);
+    });
+  };  
+  // configurationSvc.loadAppSettings();
+  // return () => accountSvc.refreshFromToken()
+  //   .pipe(
+  //     tap(authenticationResponse => {  
+  //       // console.log('appInitializer.authenticationResponse', authenticationResponse);
+  //       if(authenticationResponse.account.toLocaleLowerCase() !== accountSvc.anonymous.toLocaleLowerCase()) {
+  //         accountSvc.startRefreshTokenTimer();
+  //       }
+  //     }),
+  //     // catch error to start app on success or failure
+  //     catchError((err) => {
+  //       console.log(err);
+  //       return of();
+  //     })
+  //   );
 }
