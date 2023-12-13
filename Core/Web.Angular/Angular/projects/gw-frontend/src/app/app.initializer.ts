ec417d8a4284a04ccceaa30ef803e8e7ae94d5f4
kahiko <michael.regan@verizon.net>
2023-12-13 05:16:33 -0500

    Combined loadAppSettings and refreshFromToken into one promise

--- Core/Web.Angular/Angular/projects/gw-frontend/src/app/app.initializer.ts
+++ Core/Web.Angular/Angular/projects/gw-frontend/src/app/app.initializer.ts
@@ -1,27 +1,24 @@
-// import { catchError, of, tap } from 'rxjs';
-import { AccountService } from '@Growthware/features/account';
+import { AccountService, IAuthenticationResponse } from '@Growthware/features/account';
 import { ConfigurationService } from '@Growthware/features/configuration';
+import { finalize } from 'rxjs';
 
 export function appInitializer(accountSvc: AccountService, configurationSvc: ConfigurationService) {
   return () => {
     return new Promise((resolve) => {
       configurationSvc.loadAppSettings();
-      resolve(true);
-    });
-  };  
-  // configurationSvc.loadAppSettings();
-  // return () => accountSvc.refreshFromToken()
-  //   .pipe(
-  //     tap(authenticationResponse => {  
-  //       // console.log('appInitializer.authenticationResponse', authenticationResponse);
-  //       if(authenticationResponse.account.toLocaleLowerCase() !== accountSvc.anonymous.toLocaleLowerCase()) {
-  //         accountSvc.startRefreshTokenTimer();
-  //       }
-  //     }),
-  //     // catch error to start app on success or failure
-  //     catchError((err) => {
-  //       console.log(err);
-  //       return of();
-  //     })
-  //   );
+      accountSvc.refreshFromToken().pipe(finalize(() => {
+        resolve(true);
+      })).subscribe({
+        next: (authenticationResponse: IAuthenticationResponse) => {
+          // console.log('appInitializer.authenticationResponse', authenticationResponse);
+          if (authenticationResponse.account.toLocaleLowerCase() !== accountSvc.anonymous.toLocaleLowerCase()) {
+            accountSvc.startRefreshTokenTimer();
+          }
+        },
+        error: (error: any) => {
+          console.log(error);
+        }
+      });
+    })
+  };
 }
