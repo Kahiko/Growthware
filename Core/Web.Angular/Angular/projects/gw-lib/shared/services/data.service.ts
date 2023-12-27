6b1daa31c00246e09f464a4a016701a891511477
kahiko <michael.regan@verizon.net>
2023-12-27 01:47:57 -0500

    Fixed Unexpected any. Specify a different type

--- Core/Web.Angular/Angular/projects/gw-lib/shared/services/data.service.ts
+++ Core/Web.Angular/Angular/projects/gw-lib/shared/services/data.service.ts
@@ -14,12 +14,12 @@ export class DataService {
 
   constructor(private _GWCommon: GWCommon) { }
 
-  public notifyDataChanged(dataName: string, data: Array<any>): void {
+  public notifyDataChanged(dataName: string, data: unknown[]): void {
     const mDataNVP: DataNVP = new DataNVP(dataName, data);
     this.dataChanged.next(mDataNVP);
   }
 
-  public notifySearchDataChanged(name: string, data: Array<any>, searchCriteria: SearchCriteria): void {
+  public notifySearchDataChanged(name: string, data: unknown[], searchCriteria: SearchCriteria): void {
     const mTotalRecords = this._GWCommon.getTotalRecords(data);
     const mSearchResultsNVP: SearchResultsNVP = new SearchResultsNVP(name, { data: data, totalRecords: mTotalRecords, searchCriteria: searchCriteria });
     this.searchDataChanged.next(mSearchResultsNVP);
