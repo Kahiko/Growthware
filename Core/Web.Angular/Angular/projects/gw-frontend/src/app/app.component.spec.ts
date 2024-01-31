6349ae555c44a8bdf393a121866508435f5a983d
kahiko <michael.regan@verizon.net>
2024-01-31 14:00:40 -0500

    Fixed beforeEach, create now passes

--- Core/Web.Angular/Angular/projects/gw-frontend/src/app/app.component.spec.ts
+++ Core/Web.Angular/Angular/projects/gw-frontend/src/app/app.component.spec.ts
@@ -1,10 +1,29 @@
 import { TestBed } from '@angular/core/testing';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+
 import { AppComponent } from './app.component';
+
+import { AccountService } from '@growthware/core/account';
+import { ConfigurationService } from '@growthware/core/configuration';
+import { SecurityEntityService } from '@growthware/core/security-entities';
 
 describe('AppComponent', () => {
 	beforeEach(async () => {
 		await TestBed.configureTestingModule({
-			imports: [AppComponent],
+			imports: [
+				AppComponent,
+				RouterTestingModule,
+				HttpClientTestingModule,
+				NoopAnimationsModule,
+			],
+			declarations: [],
+			providers: [
+				AccountService,
+				ConfigurationService,
+				SecurityEntityService
+			]
 		}).compileComponents();
 	});
 
