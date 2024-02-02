2959f7da347cb1db0e22636ddb4f8d956051dbb5
kahiko <michael.regan@verizon.net>
2024-02-01 22:46:11 -0500

    Changed configureTestingModule to import RouterTestingModule, HttpClientTestingModule and NoopAnimationsModule

--- Core/Web.Angular/Angular/projects/gw-lib/core/logging/src/logging.service.spec.ts
+++ Core/Web.Angular/Angular/projects/gw-lib/core/logging/src/logging.service.spec.ts
@@ -1,17 +1,28 @@
 import { TestBed } from '@angular/core/testing';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { RouterTestingModule } from '@angular/router/testing';
 
-// import { LoggingService } from './logging.service';
+import { LoggingService } from './logging.service';
 
 describe('LoggingService', () => {
-	// let service: LoggingService;
+	let service: LoggingService;
 
-	beforeEach(() => {
-		TestBed.configureTestingModule({});
-		// service = TestBed.inject(LoggingService);
+	beforeEach(async () => {
+		await TestBed.configureTestingModule({
+			imports: [
+				RouterTestingModule,
+				HttpClientTestingModule,
+				NoopAnimationsModule,
+			],
+			declarations: [],
+			providers: [ ]
+		}).compileComponents();
+		service = TestBed.inject(LoggingService);
 	});
 
 	it('(not implemented yet) should be created', () => {
-		// expect(service).toBeTruthy();
+		expect(service).toBeTruthy();
 		expect(true).toBeTrue();
 	});
 });
