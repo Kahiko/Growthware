bf70f5fceb0cd3921f2b61c49bb01ca872ae0c3a
kahiko <michael.regan@verizon.net>
2024-02-01 22:37:48 -0500

    Changed configureTestingModule to import RouterTestingModule, HttpClientTestingModule and NoopAnimationsModule

--- Core/Web.Angular/Angular/projects/gw-lib/core/dynamic-table/src/dynamic-table.service.spec.ts
+++ Core/Web.Angular/Angular/projects/gw-lib/core/dynamic-table/src/dynamic-table.service.spec.ts
@@ -1,12 +1,23 @@
 import { TestBed } from '@angular/core/testing';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { RouterTestingModule } from '@angular/router/testing';
 
 import { DynamicTableService } from './dynamic-table.service';
 
 describe('DynamicTableService', () => {
 	let service: DynamicTableService;
 
-	beforeEach(() => {
-		TestBed.configureTestingModule({});
+	beforeEach(async () => {
+		await TestBed.configureTestingModule({
+			imports: [
+				RouterTestingModule,
+				HttpClientTestingModule,
+				NoopAnimationsModule,
+			],
+			declarations: [],
+			providers: [ ]
+		}).compileComponents();
 		service = TestBed.inject(DynamicTableService);
 	});
 
