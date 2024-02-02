5263e0ec45a746ec3fa8bf9a79158e9bcb536ad5
kahiko <michael.regan@verizon.net>
2024-02-01 23:42:22 -0500

    Changed configureTestingModule to import HttpClientTestingModule and NoopAnimationsModule

--- Core/Web.Angular/Angular/projects/gw-lib/core/file-manager/src/c/add-directory/add-directory.component.spec.ts
+++ Core/Web.Angular/Angular/projects/gw-lib/core/file-manager/src/c/add-directory/add-directory.component.spec.ts
@@ -1,4 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AddDirectoryComponent } from './add-directory.component';
 
@@ -8,9 +10,12 @@ describe('AddDirectoryComponent', () => {
 
 	beforeEach(async () => {
 		await TestBed.configureTestingModule({
-			imports: [AddDirectoryComponent]
-		})
-			.compileComponents();
+			imports: [
+				AddDirectoryComponent,
+				HttpClientTestingModule,
+				NoopAnimationsModule,
+			]
+		}).compileComponents();
     
 		fixture = TestBed.createComponent(AddDirectoryComponent);
 		component = fixture.componentInstance;
