946d0c38ea4febdb219e0286ae04f2d71f06ca09
kahiko <michael.regan@verizon.net>
2024-02-01 22:36:26 -0500

    Changed configureTestingModule to import RouterTestingModule, HttpClientTestingModule and NoopAnimationsModule

--- Core/Web.Angular/Angular/projects/gw-lib/core/community-calendar/src/calendar.service.spec.ts
+++ Core/Web.Angular/Angular/projects/gw-lib/core/community-calendar/src/calendar.service.spec.ts
@@ -1,12 +1,23 @@
 import { TestBed } from '@angular/core/testing';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 
 import { CalendarService } from './calendar.service';
 
 describe('CalendarService', () => {
 	let service: CalendarService;
 
-	beforeEach(() => {
-		TestBed.configureTestingModule({});
+	beforeEach(async () => {
+		await TestBed.configureTestingModule({
+			imports: [
+				RouterTestingModule,
+				HttpClientTestingModule,
+				NoopAnimationsModule,
+			],
+			declarations: [],
+			providers: [ ]
+		}).compileComponents();
 		service = TestBed.inject(CalendarService);
 	});
 
