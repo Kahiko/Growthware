4c51c2939eec48af1e1a465be5cfc6e3e22e8dbe
kahiko <michael.regan@verizon.net>
2024-02-01 22:48:51 -0500

    Changed configureTestingModule to import RouterTestingModule, HttpClientTestingModule and NoopAnimationsModule

--- Core/Web.Angular/Angular/projects/gw-lib/core/modal/src/modal.service.spec.ts
+++ Core/Web.Angular/Angular/projects/gw-lib/core/modal/src/modal.service.spec.ts
@@ -1,12 +1,23 @@
 import { TestBed } from '@angular/core/testing';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { RouterTestingModule } from '@angular/router/testing';
 
 import { ModalService } from './modal.service';
 
 describe('ModalService', () => {
 	let service: ModalService;
 
-	beforeEach(() => {
-		TestBed.configureTestingModule({});
+	beforeEach(async () => {
+		await TestBed.configureTestingModule({
+			imports: [
+				RouterTestingModule,
+				HttpClientTestingModule,
+				NoopAnimationsModule,
+			],
+			declarations: [],
+			providers: [ ]
+		}).compileComponents();
 		service = TestBed.inject(ModalService);
 	});
 
