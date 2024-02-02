320c7a83a60c356e29873786b1f4040c99a01a52
kahiko <michael.regan@verizon.net>
2024-02-01 23:04:35 -0500

    Changed configureTestingModule to import RouterTestingModule, HttpClientTestingModule and NoopAnimationsModule

--- Core/Web.Angular/Angular/projects/gw-lib/core/states/src/states.service.spec.ts
+++ Core/Web.Angular/Angular/projects/gw-lib/core/states/src/states.service.spec.ts
@@ -1,12 +1,23 @@
 import { TestBed } from '@angular/core/testing';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { RouterTestingModule } from '@angular/router/testing';
 
 import { StatesService } from './states.service';
 
 describe('StatesService', () => {
 	let service: StatesService;
 
-	beforeEach(() => {
-		TestBed.configureTestingModule({});
+	beforeEach(async () => {
+		await TestBed.configureTestingModule({
+			imports: [
+				RouterTestingModule,
+				HttpClientTestingModule,
+				NoopAnimationsModule,
+			],
+			declarations: [],
+			providers: [ ]
+		}).compileComponents();
 		service = TestBed.inject(StatesService);
 	});
 
