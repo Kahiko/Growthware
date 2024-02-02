7353b7477b70f4e2af1437e5f252dc87da9b834a
kahiko <michael.regan@verizon.net>
2024-02-02 14:30:30 -0500

    Changed configureTestingModule to import HttpClientTestingModule and NoopAnimationsModule

--- Core/Web.Angular/Angular/projects/gw-lib/core/group/src/c/group-details/group-details.component.spec.ts
+++ Core/Web.Angular/Angular/projects/gw-lib/core/group/src/c/group-details/group-details.component.spec.ts
@@ -1,4 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 
 import { GroupDetailsComponent } from './group-details.component';
 
@@ -8,7 +10,11 @@ describe('GroupDetailsComponent', () => {
 
 	beforeEach(async () => {
 		await TestBed.configureTestingModule({
-			imports: [GroupDetailsComponent]
+			imports: [
+				GroupDetailsComponent,
+				HttpClientTestingModule,
+				NoopAnimationsModule,
+			]
 		})
 			.compileComponents();
     
