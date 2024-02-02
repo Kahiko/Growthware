af06029f4db503970be204be7db5ebd1e44d63e1
kahiko <michael.regan@verizon.net>
2024-02-01 23:17:57 -0500

    Changed configureTestingModule to import HttpClientTestingModule and NoopAnimationsModule

--- Core/Web.Angular/Angular/projects/gw-lib/core/account/src/c/logout/logout.component.spec.ts
+++ Core/Web.Angular/Angular/projects/gw-lib/core/account/src/c/logout/logout.component.spec.ts
@@ -1,4 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 
 import { LogoutComponent } from './logout.component';
 
@@ -8,9 +10,12 @@ describe('LogoutComponent', () => {
 
 	beforeEach(async () => {
 		await TestBed.configureTestingModule({
-			imports: [LogoutComponent]
-		})
-			.compileComponents();
+			imports: [
+				LogoutComponent,
+				HttpClientTestingModule,
+				NoopAnimationsModule,
+			]
+		}).compileComponents();
     
 		fixture = TestBed.createComponent(LogoutComponent);
 		component = fixture.componentInstance;
