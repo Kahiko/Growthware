5b7ddcc72f338eb6c480ec28b24f076f8e87354e
kahiko <michael.regan@verizon.net>
2024-02-01 23:13:29 -0500

    Changed configureTestingModule to import RouterTestingModule, HttpClientTestingModule and NoopAnimationsModule

--- Core/Web.Angular/Angular/projects/gw-lib/core/account/src/c/change-password/change-password.component.spec.ts
+++ Core/Web.Angular/Angular/projects/gw-lib/core/account/src/c/change-password/change-password.component.spec.ts
@@ -1,4 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { RouterTestingModule } from '@angular/router/testing';
 
 import { ChangePasswordComponent } from './change-password.component';
 
@@ -8,7 +11,12 @@ describe('ChangePasswordComponent', () => {
 
 	beforeEach(async () => {
 		await TestBed.configureTestingModule({
-			imports: [ChangePasswordComponent]
+			imports: [
+				ChangePasswordComponent,
+				RouterTestingModule,
+				HttpClientTestingModule,
+				NoopAnimationsModule,
+			]
 		})
 			.compileComponents();
     
