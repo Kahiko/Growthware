1adf1d2610e4d49d795732e54e0b0df4bac0de68
kahiko <michael.regan@verizon.net>
2024-02-02 14:20:56 -0500

    Changed configureTestingModule to import HttpClientTestingModule and NoopAnimationsModule

--- Core/Web.Angular/Angular/projects/gw-lib/core/file-manager/src/c/upload/upload.component.spec.ts
+++ Core/Web.Angular/Angular/projects/gw-lib/core/file-manager/src/c/upload/upload.component.spec.ts
@@ -1,4 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 
 import { UploadComponent } from './upload.component';
 
@@ -8,9 +10,12 @@ describe('UploadComponent', () => {
 
 	beforeEach(async () => {
 		await TestBed.configureTestingModule({
-			imports: [UploadComponent]
-		})
-			.compileComponents();
+			imports: [
+				UploadComponent,
+				HttpClientTestingModule,
+				NoopAnimationsModule,
+			]
+		}).compileComponents();
     
 		fixture = TestBed.createComponent(UploadComponent);
 		component = fixture.componentInstance;
