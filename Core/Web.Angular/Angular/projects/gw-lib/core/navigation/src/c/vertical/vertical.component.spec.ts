a9032e3269aac2d454f8b5bbd9768318c8d98c49
kahiko <michael.regan@verizon.net>
2024-02-03 12:00:23 -0500

    Changed configureTestingModule to import HttpClientTestingModule and NoopAnimationsModule

--- Core/Web.Angular/Angular/projects/gw-lib/core/navigation/src/c/vertical/vertical.component.spec.ts
+++ Core/Web.Angular/Angular/projects/gw-lib/core/navigation/src/c/vertical/vertical.component.spec.ts
@@ -1,4 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 
 import { VerticalComponent } from './vertical.component';
 
@@ -8,7 +10,11 @@ describe('VerticalComponent', () => {
 
 	beforeEach(async () => {
 		await TestBed.configureTestingModule({
-			imports: [VerticalComponent]
+			imports: [
+				VerticalComponent,
+				HttpClientTestingModule,
+				NoopAnimationsModule,
+			]
 		})
 			.compileComponents();
     
