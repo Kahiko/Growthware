2fe58497e12c24431038aadb9380ed0de7d335e9
kahiko <michael.regan@verizon.net>
2024-02-03 06:22:16 -0500

    Changed configureTestingModule to import HttpClientTestingModule and NoopAnimationsModule

--- Core/Web.Angular/Angular/projects/gw-lib/core/navigation/src/c/hierarchical-horizontal-flyout/hierarchical-horizontal-flyout.component.spec.ts
+++ Core/Web.Angular/Angular/projects/gw-lib/core/navigation/src/c/hierarchical-horizontal-flyout/hierarchical-horizontal-flyout.component.spec.ts
@@ -1,4 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 
 import { HierarchicalHorizontalFlyoutComponent } from './hierarchical-horizontal-flyout.component';
 
@@ -8,9 +10,12 @@ describe('HierarchicalHorizontalFlyoutComponent', () => {
 
 	beforeEach(async () => {
 		await TestBed.configureTestingModule({
-			imports: [HierarchicalHorizontalFlyoutComponent]
-		})
-			.compileComponents();
+			imports: [
+				HierarchicalHorizontalFlyoutComponent,
+				HttpClientTestingModule,
+				NoopAnimationsModule,
+			]
+		}).compileComponents();
     
 		fixture = TestBed.createComponent(HierarchicalHorizontalFlyoutComponent);
 		component = fixture.componentInstance;
