fd7901bdb06f29d9cad9f0906799ba365b06af64
kahiko <michael.regan@verizon.net>
2024-02-03 20:04:39 -0500

    Changed configureTestingModule to import HttpClientTestingModule and NoopAnimationsModule

--- Core/Web.Angular/Angular/projects/gw-lib/core/role/src/c/role-details/role-details.component.spec.ts
+++ Core/Web.Angular/Angular/projects/gw-lib/core/role/src/c/role-details/role-details.component.spec.ts
@@ -1,4 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 
 import { RoleDetailsComponent } from './role-details.component';
 
@@ -8,9 +10,12 @@ describe('RoleDetailsComponent', () => {
 
 	beforeEach(async () => {
 		await TestBed.configureTestingModule({
-			imports: [RoleDetailsComponent]
-		})
-			.compileComponents();
+			imports: [
+				RoleDetailsComponent,
+				HttpClientTestingModule,
+				NoopAnimationsModule,
+			]
+		}).compileComponents();
     
 		fixture = TestBed.createComponent(RoleDetailsComponent);
 		component = fixture.componentInstance;
