a96d2c45fea67e65ff32dbdbb3ffb66bdba823b6
kahiko <michael.regan@verizon.net>
2024-02-04 04:09:22 -0500

    Changed configureTestingModule to import HttpClientTestingModule and NoopAnimationsModule Had to set component.item to new NavLink

--- Core/Web.Angular/Angular/projects/gw-lib/core/navigation/src/c/vertical-list-item/vertical-list-item.component.spec.ts
+++ Core/Web.Angular/Angular/projects/gw-lib/core/navigation/src/c/vertical-list-item/vertical-list-item.component.spec.ts
@@ -1,5 +1,8 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+// Feature
+import { NavLink } from '../../nav-link.model';
 import { VerticalListItemComponent } from './vertical-list-item.component';
 
 describe('VerticalListItemComponent', () => {
@@ -8,12 +11,18 @@ describe('VerticalListItemComponent', () => {
 
 	beforeEach(async () => {
 		await TestBed.configureTestingModule({
-			imports: [VerticalListItemComponent]
-		})
-			.compileComponents();
-    
+			imports: [
+				VerticalListItemComponent,
+				HttpClientTestingModule,
+				NoopAnimationsModule,
+			],
+			declarations: [],
+			providers: [ ]
+		}).compileComponents();
+		
 		fixture = TestBed.createComponent(VerticalListItemComponent);
 		component = fixture.componentInstance;
+		component.item = new NavLink('', '', '', '', '', 0, '', true, '');
 		fixture.detectChanges();
 	});
 
