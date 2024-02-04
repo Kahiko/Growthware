a53d255bde261fba543c699b42617b7936863828
kahiko <michael.regan@verizon.net>
2024-02-04 04:52:26 -0500

    Changed the name b/c I'm unsure if a unit test is necessary for this functionality.

--- Core/Web.Angular/Angular/projects/gw-lib/common/services/src/lower-case-url-serializer.service_spec.ts
+++ Core/Web.Angular/Angular/projects/gw-lib/common/services/src/lower-case-url-serializer.service_spec.ts
@@ -3,6 +3,11 @@ import { TestBed } from '@angular/core/testing';
 import { LowerCaseUrlSerializer } from './lower-case-url-serializer.service';
 
 describe('LowerCaseUrlSerializer', () => {
+	/**
+	 * I'm unsure that a unit test is needed for this functionality.  
+	 * The test is simply paste an URL and it should be changed lower case
+	 * This was tested during integration testing and is not likely to chnage without the same test.
+	 */
 	let service: LowerCaseUrlSerializer;
 
 	beforeEach(() => {
