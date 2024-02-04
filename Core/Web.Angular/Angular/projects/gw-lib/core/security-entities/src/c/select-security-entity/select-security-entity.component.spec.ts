a7f54d154c295bb52f62b82ecf2d1961403727aa
kahiko <michael.regan@verizon.net>
2024-02-03 20:29:12 -0500

    Changed configureTestingModule to import HttpClientTestingModule and NoopAnimationsModule

--- Core/Web.Angular/Angular/projects/gw-lib/core/security-entities/src/c/select-security-entity/select-security-entity.component.spec.ts
+++ Core/Web.Angular/Angular/projects/gw-lib/core/security-entities/src/c/select-security-entity/select-security-entity.component.spec.ts
@@ -1,4 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 
 import { SelectSecurityEntityComponent } from './select-security-entity.component';
 
@@ -8,9 +10,14 @@ describe('SelectSecurityEntityComponent', () => {
 
 	beforeEach(async () => {
 		await TestBed.configureTestingModule({
-			imports: [SelectSecurityEntityComponent]
-		})
-			.compileComponents();
+			imports: [
+				SelectSecurityEntityComponent,
+				HttpClientTestingModule,
+				NoopAnimationsModule,
+			],
+			declarations: [],
+			providers: [ ]
+		}).compileComponents();
     
 		fixture = TestBed.createComponent(SelectSecurityEntityComponent);
 		component = fixture.componentInstance;
