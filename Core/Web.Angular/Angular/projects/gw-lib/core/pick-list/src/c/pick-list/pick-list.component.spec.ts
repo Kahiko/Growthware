f0fc77cce95ce4330357fa01ed1249aeceea551f
kahiko <michael.regan@verizon.net>
2024-02-03 20:02:21 -0500

    Changed configureTestingModule to import HttpClientTestingModule and NoopAnimationsModule

--- Core/Web.Angular/Angular/projects/gw-lib/core/pick-list/src/c/pick-list/pick-list.component.spec.ts
+++ Core/Web.Angular/Angular/projects/gw-lib/core/pick-list/src/c/pick-list/pick-list.component.spec.ts
@@ -1,4 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 
 import { PickListComponent } from './pick-list.component';
 
@@ -8,9 +10,12 @@ describe('PickListComponent', () => {
 
 	beforeEach(async () => {
 		await TestBed.configureTestingModule({
-			imports: [PickListComponent]
-		})
-			.compileComponents();
+			imports: [
+				PickListComponent,
+				HttpClientTestingModule,
+				NoopAnimationsModule,
+			]
+		}).compileComponents();
     
 		fixture = TestBed.createComponent(PickListComponent);
 		component = fixture.componentInstance;
