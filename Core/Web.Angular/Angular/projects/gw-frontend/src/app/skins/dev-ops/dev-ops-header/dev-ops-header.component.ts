ffebceb198b02607f0ac7a50730097cca1ac5a01
kahiko <michael.regan@verizon.net>
2024-02-06 18:39:31 -0500

    Formatting

--- Core/Web.Angular/Angular/projects/gw-frontend/src/app/skins/dev-ops/dev-ops-header/dev-ops-header.component.ts
+++ Core/Web.Angular/Angular/projects/gw-frontend/src/app/skins/dev-ops/dev-ops-header/dev-ops-header.component.ts
@@ -21,59 +21,59 @@ export class DevOpsHeaderComponent implements OnDestroy, OnInit {
 	isAuthenticated: boolean = false;
 	version: string = '';
 
-  @Input() sidenav: any;
+	@Input() sidenav: any;
 
-  constructor(
-    private _AccountSvc: AccountService,
-    private _ConfigurationSvc: ConfigurationService,
-    private _GWCommon: GWCommon,
-    private _ModalSvc: ModalService,
-    private _Router: Router) { }
+	constructor(
+		private _AccountSvc: AccountService,
+		private _ConfigurationSvc: ConfigurationService,
+		private _GWCommon: GWCommon,
+		private _ModalSvc: ModalService,
+		private _Router: Router) { }
 
-  ngOnDestroy(): void {
-  	this._Subscription.unsubscribe();
-  }
+	ngOnDestroy(): void {
+		this._Subscription.unsubscribe();
+	}
 
-  ngOnInit(): void {
-  	this._ConfigurationSvc.loadAppSettings();
-  	this._Subscription.add(
-  		this._ConfigurationSvc.applicationName$.subscribe((val: string) => { this.applicationName = val; })
-  	);
-  	this._Subscription.add(
-  		this._ConfigurationSvc.version$.subscribe((val: string) => { 
-  			this.version = val; 
-  		})
-  	);
-  	this._Subscription.add(
-  		this._AccountSvc.accountInformationChanged$.subscribe((val: IAccountInformation) => {
-  			this.isAuthenticated = val.authenticationResponse.account.toLowerCase() != this._AccountSvc.anonymous.toLowerCase();
-  			this.accountName = this._GWCommon.formatData(val.authenticationResponse.account, 'text:28');
-  		})
-  	);
-  }
+	ngOnInit(): void {
+		this._ConfigurationSvc.loadAppSettings();
+		this._Subscription.add(
+			this._ConfigurationSvc.applicationName$.subscribe((val: string) => { this.applicationName = val; })
+		);
+		this._Subscription.add(
+			this._ConfigurationSvc.version$.subscribe((val: string) => {
+				this.version = val;
+			})
+		);
+		this._Subscription.add(
+			this._AccountSvc.accountInformationChanged$.subscribe((val: IAccountInformation) => {
+				this.isAuthenticated = val.authenticationResponse.account.toLowerCase() != this._AccountSvc.anonymous.toLowerCase();
+				this.accountName = this._GWCommon.formatData(val.authenticationResponse.account, 'text:28');
+			})
+		);
+	}
 
-  onLogin(): void {
-  	const mWindowSize: WindowSize = new WindowSize(225, 450);
-  	const mModalOptions: ModalOptions = new ModalOptions(this._AccountSvc.logInModalId, 'Logon', LoginComponent, mWindowSize);
-  	mModalOptions.buttons.okButton.callbackMethod = () => {
-  		this.onModalOk;
-  	};
-  	this._ModalSvc.open(mModalOptions);
-  }
+	onLogin(): void {
+		const mWindowSize: WindowSize = new WindowSize(225, 450);
+		const mModalOptions: ModalOptions = new ModalOptions(this._AccountSvc.logInModalId, 'Logon', LoginComponent, mWindowSize);
+		mModalOptions.buttons.okButton.callbackMethod = () => {
+			this.onModalOk;
+		};
+		this._ModalSvc.open(mModalOptions);
+	}
 
-  onLogoClick(): void {
-  	if(this._AccountSvc.authenticationResponse.account.trim().toLocaleLowerCase() !== this._AccountSvc.anonymous.trim().toLocaleLowerCase()) {
-  		this._Router.navigate(['home']);
-  	} else {
-  		this._Router.navigate(['generic_home']);
-  	}
-  }
+	onLogoClick(): void {
+		if (this._AccountSvc.authenticationResponse.account.trim().toLocaleLowerCase() !== this._AccountSvc.anonymous.trim().toLocaleLowerCase()) {
+			this._Router.navigate(['home']);
+		} else {
+			this._Router.navigate(['generic_home']);
+		}
+	}
 
-  onModalOk() {
+	onModalOk() {
 
-  }
+	}
 
-  onLogout(): void {
-  	this._AccountSvc.logout();
-  }
+	onLogout(): void {
+		this._AccountSvc.logout();
+	}
 }
