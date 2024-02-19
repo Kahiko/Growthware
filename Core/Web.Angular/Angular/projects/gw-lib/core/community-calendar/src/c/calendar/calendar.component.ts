2de9534be0c3cc2a90e29b5bde1cc015e1d82115
kahiko <michael.regan@verizon.net>
2024-02-19 15:21:31 -0500

    Added the choice of increasing/decreasing by day, month or year

--- Core/Web.Angular/Angular/projects/gw-lib/core/community-calendar/src/c/calendar/calendar.component.ts
+++ Core/Web.Angular/Angular/projects/gw-lib/core/community-calendar/src/c/calendar/calendar.component.ts
@@ -1,8 +1,10 @@
 import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 // Angular Material
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
+import { MatRadioModule } from '@angular/material/radio';
 // Library
 import { GWCommon } from '@growthware/common/services';
 // Feature
@@ -11,74 +13,101 @@ import { CalendarService } from '../../calendar.service';
 import { DayOfWeekComponent } from '../day-of-week/day-of-week.component';
 import { IMonth, Month } from '../../interfaces/month.model';
 import { NamesOfDays } from '../../interfaces/names-of-days.enum';
+import { Subscription } from 'rxjs';
 
 @Component({
-	selector: 'gw-core-calendar',
-	standalone: true,
-	imports: [
-		CommonModule,
-		// Angular Material
-		MatButtonModule,
-		MatIconModule,
-		// Feature
-		DayOfWeekComponent
-	],
-	templateUrl: './calendar.component.html',
-	styleUrls: ['./calendar.component.scss']
+  selector: 'gw-core-calendar',
+  standalone: true,
+  imports: [
+    CommonModule,
+    FormsModule,
+    // Angular Material
+    MatButtonModule,
+    MatIconModule,
+    MatRadioModule,
+    // Feature
+    DayOfWeekComponent
+  ],
+  templateUrl: './calendar.component.html',
+  styleUrls: ['./calendar.component.scss']
 })
 export class CalendarComponent implements OnInit {
-	private _FirstDayOfWeek: NamesOfDays = NamesOfDays.Monday;
-	private _SelectedDate: Date = new Date();
-	// public calendar: CalendarDay[] = [];
-	public calendar: IMonth = new Month();
-	public weekDayNames: string[] = [];
-	public monthNames = ['January', 'February', 'March', 'April', 'May', 'June',
-		'July', 'August', 'September', 'October', 'November', 'December'
-	];
-	public displayMonth: string = '';
+  private _FirstDayOfWeek: NamesOfDays = NamesOfDays.Monday;
+  private _Subscriptions: Subscription = new Subscription();
+  private _SelectedDate: Date = new Date();
+  // public calendar: CalendarDay[] = [];
+  public calendar: IMonth = new Month();
+  public displayMonth: string = '';
+  public displayYear: number = 0;
+  public incrementBy: string = 'month';
+  public weekDayNames: string[] = [];
 
-	constructor(
+  constructor(
 		private _CalendarSvc: CalendarService,
-    	private _GWCommon: GWCommon
-	){}
+		private _GWCommon: GWCommon
+  ) { }
 
-	ngOnInit(): void {
-		this._FirstDayOfWeek = NamesOfDays.Sunday;
-		this.getCalendarData();
-		// console.log('CalendarComponent.ngOnInit.calendar', this.calendar);
-		this.getWeekDayNames();
-		// console.log('CalendarComponent.ngOnInit.weekDayNames', this.weekDayNames);
-	}
+  ngOnInit(): void {
+    this._FirstDayOfWeek = NamesOfDays.Sunday;
+    this._Subscriptions.add(this._CalendarSvc.calendarData$.subscribe((data: IMonth) => {
+      this.calendar = data;
+      this.displayMonth = this._SelectedDate.toLocaleString('default', { month: 'long' });
+      this.displayYear = this._SelectedDate.getFullYear();
+    }));
+    // Set the data for the calendar header
+    this.getWeekDayNames();
+    // console.log('CalendarComponent.ngOnInit.weekDayNames', this.weekDayNames);
+    // Set the data for the calendar
+    this.getCalendarData();
+    // console.log('CalendarComponent.ngOnInit.calendar', this.calendar);
+  }
 
-	public increaseMonth() {
-		this._SelectedDate.setMonth(this._SelectedDate.getMonth() + 1);
-		this.getCalendarData();
-	}
+  public increase() {
+    /*eslint indent: ["error", 2, { "SwitchCase": 1 }]*/
+    switch (this.incrementBy) {
+      case 'day': {
+        this._SelectedDate.setDate(this._SelectedDate.getDay() + 1);
+        break;
+      }
+      case 'month': {
+        this._SelectedDate.setMonth(this._SelectedDate.getMonth() + 1);
+        break;
+      }
+      case 'year': {
+        this._SelectedDate.setFullYear(this._SelectedDate.getFullYear() + 1);
+        break;
+      }
+      default: {
+        this._SelectedDate.setMonth(this._SelectedDate.getMonth() + 1);
+      }
+    }
+    this.getCalendarData();
+  }
 
-	public decreaseMonth() {
-		this._SelectedDate.setMonth(this._SelectedDate.getMonth() - 1);
-		this.getCalendarData();
-	}
+  public decrease() {
+    this._SelectedDate.setMonth(this._SelectedDate.getMonth() - 1);
+    this.getCalendarData();
+  }
 
-	public setCurrentMonth() {
-		this._SelectedDate = new Date();
-		this.getCalendarData();
-	}
+  public currentDate() {
+    this._SelectedDate = new Date();
+    this.getCalendarData();
+  }
 
-	private getCalendarData(): void {
-		this.calendar = this._CalendarSvc.getMonthData(this._SelectedDate, this._FirstDayOfWeek);
-	}
+  private getCalendarData(): void {
+    this._CalendarSvc.getMonthData(this._SelectedDate, this._FirstDayOfWeek);
+  }
 
-	private getWeekDayNames(): void {
-		this.weekDayNames.length = 0;
-		let mDay: number = this._FirstDayOfWeek;
-		for (let i = 0; i < 7; i++) {
-			this.weekDayNames.push(NamesOfDays[mDay]);
-			if (mDay === 6) {
-				mDay = 0;
-			} else {
-				mDay++;
-			}
-		}
-	}
+  private getWeekDayNames(): void {
+    this.weekDayNames.length = 0;
+    let mDay: number = this._FirstDayOfWeek;
+    for (let i = 0; i < 7; i++) {
+      this.weekDayNames.push(NamesOfDays[mDay]);
+      if (mDay === 6) {
+        mDay = 0;
+      } else {
+        mDay++;
+      }
+    }
+  }
 }
--- Core/Web.Angular/Angular/projects/gw-lib/core/community-calendar/src/calendar.service.ts
+++ Core/Web.Angular/Angular/projects/gw-lib/core/community-calendar/src/calendar.service.ts
@@ -6,18 +6,30 @@ import { IMonth, Month } from './interfaces/month.model';
 import { IWeek, Week } from './interfaces/week.model';
 import { Day } from './interfaces/day.model';
 import { NamesOfDays } from './interfaces/names-of-days.enum';
+import { BehaviorSubject } from 'rxjs';
 
 @Injectable({
 	providedIn: 'root'
 })
 export class CalendarService {
 
+	private _CalendarData: BehaviorSubject<IMonth> = new BehaviorSubject<IMonth>(new Month());
+	public calendarData$ = this._CalendarData.asObservable();
+
 	constructor(
 		private _GWCommon: GWCommon
 	) { }
 
-	getMonthData(selectedDate: Date, firstDayOfWeek: NamesOfDays): IMonth {
-		const monthData: IMonth = new Month();
+	
+	/**
+	 * Generate the data for a month given a selected date and the first day of the week.
+	 *
+	 * @param {Date} selectedDate - the selected date
+	 * @param {NamesOfDays} firstDayOfWeek - the first day of the week IE NamesOfDays.Sunday
+	 * @return {IMonth} the month data
+	 */
+	getMonthData(selectedDate: Date, firstDayOfWeek: NamesOfDays): void {
+		const mRetVal: IMonth = new Month();
 		const mWorkingDate = new Date(selectedDate);
 		const mCurrentMonth = selectedDate.getMonth();
 		let mPreviousMonth = selectedDate.getMonth() -1;
@@ -26,20 +38,17 @@ export class CalendarService {
 		if(mPreviousMonth === -1) {
 			mPreviousMonth = 11;
 		}
-
-		mWorkingDate.setDate(1); // Set to the first day of the month
-	
+		// Set to the first day of the month
+		mWorkingDate.setDate(1);
 		// Move to the first day to display in the calendar
 		while (mWorkingDate.getDay() !== firstDayOfWeek) {
 			mWorkingDate.setDate(mWorkingDate.getDate() - 1);
 		}
-
-		// Interate through the days creating and adding them to the month as long
-		// as they fall in the previous or current months
+		// Interate for the working date creating IDay objects and IWeek objects, then
+		// adding them to the month data.
 		while (mWorkingDate.getMonth() === mPreviousMonth || mWorkingDate.getMonth() === mCurrentMonth) {
 			const week: IWeek = new Week();
-			// Iterate through days of the week
-			// looping through 7 days ensures the last week will have necessary days for
+			// Iterating through 7 days ensures the last week will have necessary days of
 			// the next month.
 			for (let i = 0; i < 7; i++) {
 				// const mNewWorkingDate = new Date(mWorkingDate);
@@ -47,9 +56,8 @@ export class CalendarService {
 				week.days.push(mDay);
 				mWorkingDate.setDate(mWorkingDate.getDate() + 1);
 			}
-			monthData.weeks.push(week);
+			mRetVal.weeks.push(week);
 		}
-
-		return monthData;
+		this._CalendarData.next(mRetVal);
 	}
 }
