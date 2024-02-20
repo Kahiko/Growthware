83cb31379f636a666a097af22867320d490ea64b
kahiko <michael.regan@verizon.net>
2024-02-19 22:38:17 -0500

    Removed isPastDate and changed isDatesEqual to datesEqual

--- Core/Web.Angular/Angular/projects/gw-lib/core/community-calendar/src/interfaces/day.model.ts
+++ Core/Web.Angular/Angular/projects/gw-lib/core/community-calendar/src/interfaces/day.model.ts
@@ -3,14 +3,13 @@ import { NamesOfDays } from './names-of-days.enum';
 import { NamesOfMonths } from './names-of-months.enum';
 
 export interface IDay {
-    day: NamesOfDays;
-    date: Date;
-    events?: IEvent[];
+	day: NamesOfDays;
+	date: Date;
+	events?: IEvent[];
 	isFutureMonth: boolean;
-	isPastDate: boolean;
 	isPreviousMonth: boolean;
 	isToday: boolean;
-    month: NamesOfMonths;
+	month: NamesOfMonths;
 	monthName: string;
 }
 
@@ -19,7 +18,6 @@ export class Day implements IDay {
 	public date: Date;
 	public events?: IEvent[];
 	public isFutureMonth: boolean = false;
-	public isPastDate: boolean = false;
 	public isPreviousMonth: boolean = false;
 	public isToday: boolean = false;
 	public month: NamesOfMonths;
@@ -29,19 +27,18 @@ export class Day implements IDay {
 		this.day = date.getDay() as NamesOfDays;
 		this.date = date;
 		this.isFutureMonth = date.getMonth() > new Date().getMonth();
-		this.isPastDate = date > new Date();
 		this.isPreviousMonth = date.getMonth() < new Date().getMonth();
-		this.isToday = this.isDatesEqual(new Date(), this.date);
+		this.isToday = this.datesEqual(new Date(), this.date);
 		this.month = date.getMonth() as NamesOfMonths;
 		this.monthName = NamesOfDays[this.day];
 	}
 
-	private isDatesEqual(date1: Date, date2: Date): boolean {
+	private datesEqual(date1: Date, date2: Date): boolean {
 		return (
 			date1.getFullYear() === date2.getFullYear() &&
 			date1.getMonth() === date2.getMonth() &&
 			date1.getDate() === date2.getDate()
-		  );		
+		);
 	}
 
 }
