de2a3182b94b70c183edbbfdf38f1b3f85c9d6b2
kahiko <michael.regan@verizon.net>
2024-04-14 12:18:59 -0400

    Updated Set_Name_Value_Pair_Detail

--- Core/Database/ZGWSystem/StoredProcedures/Set_Name_Value_Pair_Detail.sql
+++ Core/Database/ZGWSystem/StoredProcedures/Set_Name_Value_Pair_Detail.sql
@@ -3,13 +3,12 @@
 Usage:
 	DECLARE 
 		@V_NVP_DetailSeqId INT = -1,
-		@V_NVPSeqId int = (SELECT NVPSeqId FROM ZGWSystem.Name_Value_Pairs WHERE STATIC_NAME = 'Navigation_Types') ,
-		@V_NVP_Detail_Name VARCHAR(50) = 'Test',
-		@V_NVP_Detail_Value VARCHAR(300) = 'Test value',
+		@V_NVPSeqId int = 5 ,
+		@V_NVP_Detail_Name VARCHAR(50) = '7',
+		@V_NVP_Detail_Value VARCHAR(300) = '7',
 		@V_StatusSeqId INT = 1,
 		@V_Sort_Order INT = 1,
 		@V_Added_Updated_BY INT = 1,
-		@V_Primary_Key INT = null,
 		@V_ErrorCode int = null,
 		@V_Debug bit = 1
 
@@ -21,7 +20,6 @@ Usage:
 		@V_StatusSeqId,
 		@V_Sort_Order,
 		@V_Added_Updated_BY,
-		@V_Primary_Key,
 		@V_ErrorCode,
 		@V_Debug
 */
@@ -29,6 +27,10 @@ Usage:
 -- Author:		Michael Regan
 -- Create date: 07/26/2011
 -- Description:	Inserts/Updates ZGWCoreWeb.Account_Choices based on @P_Account
+-- =============================================
+-- Author:		Michael Regan
+-- Create date: 07/26/2011
+-- Description:	Changed to return a data row after the update/insert
 -- =============================================
 CREATE PROCEDURE [ZGWSystem].[Set_Name_Value_Pair_Detail]
 	@P_NVP_DetailSeqId INT,
@@ -38,82 +40,85 @@ CREATE PROCEDURE [ZGWSystem].[Set_Name_Value_Pair_Detail]
 	@P_StatusSeqId INT,
 	@P_Sort_Order INT,
 	@P_Added_Updated_By INT,
-	@P_Primary_Key INT OUTPUT,
 	@P_ErrorCode int OUTPUT,
 	@P_Debug INT = 0
 AS
 	IF @P_Debug = 1 PRINT 'Starting [ZGWSystem].[Set_Name_Value_Pair_Detail]'
 	DECLARE 
-		@V_Static_Name VARCHAR(30) = (SELECT Static_Name
-FROM ZGWSystem.Name_Value_Pairs
-WHERE NVPSeqId = @P_NVPSeqId)
-		,@V_Schema_Name VARCHAR(30) = (SELECT [Schema_Name]
-FROM ZGWSystem.Name_Value_Pairs
-WHERE NVPSeqId = @P_NVPSeqId)
+		@V_Static_Name VARCHAR(30) = (
+			SELECT Static_Name
+			FROM ZGWSystem.Name_Value_Pairs
+			WHERE NVPSeqId = @P_NVPSeqId
+		)
+		,@V_Schema_Name VARCHAR(30) = (
+			SELECT [Schema_Name]
+			FROM ZGWSystem.Name_Value_Pairs
+			WHERE NVPSeqId = @P_NVPSeqId
+		)
 		,@V_Statement NVARCHAR(4000)
 		,@V_Now DATETIME = GETDATE()
 
 	IF @P_NVP_DetailSeqId > -1
 		BEGIN
-	-- UPDATE PROFILE
-	SET @V_Statement = 'UPDATE ' + CONVERT(VARCHAR,@V_Schema_Name) + '.' + CONVERT(VARCHAR,@V_Static_Name) + '
-			SET 
-				NVP_Detail_Name = ''' + CONVERT(VARCHAR,@P_NVP_Detail_Name) + ''',
-				NVP_Detail_Value = ''' + CONVERT(VARCHAR,@P_NVP_Detail_Value) + ''',
-				StatusSeqId = ' + CONVERT(VARCHAR,@P_StatusSeqId) + ',
-				Sort_Order = ' + CONVERT(VARCHAR,@P_Sort_Order) + ',
-				Updated_By = ' + CONVERT(VARCHAR,@P_Added_Updated_By) + ',
-				UPDATED_DATE = ''' + CONVERT(VARCHAR,@V_Now) + '''
-			WHERE
-				NVP_DetailSeqId = ' + CONVERT(VARCHAR,@P_NVP_DetailSeqId)
-	IF @P_Debug = 1 PRINT @V_Statement
-	EXECUTE dbo.sp_executesql @statement = @V_Statement
-	SELECT @P_Primary_Key = @P_NVPSeqId
-END
+			-- UPDATE PROFILE
+			SET @V_Statement = '
+UPDATE ' + CONVERT(VARCHAR,@V_Schema_Name) + '.' + CONVERT(VARCHAR,@V_Static_Name) + '
+SET 
+	NVP_Detail_Name = ''' + CONVERT(VARCHAR,@P_NVP_Detail_Name) + ''',
+	NVP_Detail_Value = ''' + CONVERT(VARCHAR,@P_NVP_Detail_Value) + ''',
+	StatusSeqId = ' + CONVERT(VARCHAR,@P_StatusSeqId) + ',
+	Sort_Order = ' + CONVERT(VARCHAR,@P_Sort_Order) + ',
+	Updated_By = ' + CONVERT(VARCHAR,@P_Added_Updated_By) + ',
+	UPDATED_DATE = ''' + CONVERT(VARCHAR,@V_Now) + '''
+WHERE
+	NVP_DetailSeqId = ' + CONVERT(VARCHAR,@P_NVP_DetailSeqId);
+			IF @P_Debug = 1 PRINT @V_Statement
+			EXECUTE dbo.sp_executesql @statement = @V_Statement
+		END
 	ELSE
 		BEGIN
-	-- INSERT a new row in the table.
-	-- CHECK FOR DUPLICATE NAME BEFORE INSERTING
-	DECLARE @V_COUNT INT
-	SET @V_Statement= 'SET @V_COUNT = (SELECT COUNT(*)
-				   FROM ' + CONVERT(VARCHAR,@V_Schema_Name) + '.' + CONVERT(VARCHAR,@V_Static_Name) + '
-				   WHERE NVP_Detail_Value = ''' + CONVERT(VARCHAR,@P_NVP_Detail_Value) + ''' AND NVP_Detail_Name = ''' + CONVERT(VARCHAR,@P_NVP_Detail_Name) + ''')'
-	IF @P_Debug = 1 PRINT @V_Statement
-	EXECUTE sp_executesql @V_Statement,N'@V_COUNT int output',@V_COUNT output
-	IF @V_COUNT > 0
+			-- INSERT a new row in the table.
+			-- CHECK FOR DUPLICATE NAME BEFORE INSERTING
+			DECLARE @V_COUNT INT
+			SET @V_Statement= 'SET @V_COUNT = (SELECT COUNT(*)
+						FROM ' + CONVERT(VARCHAR,@V_Schema_Name) + '.' + CONVERT(VARCHAR,@V_Static_Name) + '
+						WHERE NVP_Detail_Value = ''' + CONVERT(VARCHAR,@P_NVP_Detail_Value) + ''' AND NVP_Detail_Name = ''' + CONVERT(VARCHAR,@P_NVP_Detail_Name) + ''')'
+			IF @P_Debug = 1 PRINT @V_Statement
+			EXECUTE sp_executesql @V_Statement,N'@V_COUNT int output',@V_COUNT output
+			IF @V_COUNT > 0
 				BEGIN
-		RAISERROR ('The entry already exists in the database.',16,1)
-		RETURN
-	END
-	SET @V_Statement = 'INSERT INTO ' + CONVERT(VARCHAR,@V_Schema_Name) + '.' + CONVERT(VARCHAR,@V_Static_Name) + '(
-					NVPSeqId,
-					NVP_Detail_Name,
-					NVP_Detail_Value,
-					StatusSeqId,
-					Sort_Order,
-					Added_By,
-					ADDED_DATE
-				)
-				VALUES
-				(
-					' + CONVERT(VARCHAR,@P_NVPSeqId) + ',
-					''' + CONVERT(VARCHAR,@P_NVP_Detail_Name) + ''',
-					''' + CONVERT(VARCHAR,@P_NVP_Detail_Value) + ''',
-					' + CONVERT(VARCHAR,@P_StatusSeqId) + ',
-					' + CONVERT(VARCHAR,@P_Sort_Order) + ',
-					' + CONVERT(VARCHAR,@P_Added_Updated_By) + ',
-					''' + CONVERT(VARCHAR,@V_Now) + '''
-				)'
-	IF @P_Debug = 1 PRINT @V_Statement
-	EXECUTE dbo.sp_executesql @statement = @V_Statement
-	SELECT @P_Primary_Key=SCOPE_IDENTITY()
--- Get the IDENTITY value for the row just inserted.
---PRINT 'DONE ADDING'
-END
--- Get the Error Code for the statement just executed.
---PRINT 'SETTING ERROR CODE'
-SELECT @P_ErrorCode=@@ERROR
+					RAISERROR ('The entry already exists in the database.',16,1)
+					RETURN
+				END
+			-- END IF
+			SET @V_Statement = '
+INSERT INTO ' + CONVERT(VARCHAR,@V_Schema_Name) + '.' + CONVERT(VARCHAR,@V_Static_Name) + '(
+	NVPSeqId,
+	NVP_Detail_Name,
+	NVP_Detail_Value,
+	StatusSeqId,
+	Sort_Order,
+	Added_By,
+	ADDED_DATE
+) VALUES (
+	' + CONVERT(VARCHAR,@P_NVPSeqId) + ',
+	''' + CONVERT(VARCHAR,@P_NVP_Detail_Name) + ''',
+	''' + CONVERT(VARCHAR,@P_NVP_Detail_Value) + ''',
+	' + CONVERT(VARCHAR,@P_StatusSeqId) + ',
+	' + CONVERT(VARCHAR,@P_Sort_Order) + ',
+	' + CONVERT(VARCHAR,@P_Added_Updated_By) + ',
+	''' + CONVERT(VARCHAR,@V_Now) + '''
+)';
+			IF @P_Debug = 1 PRINT @V_Statement
+			EXECUTE dbo.sp_executesql @statement = @V_Statement
+			-- Get the IDENTITY value for the row just inserted.
+			SELECT @P_NVP_DetailSeqId=SCOPE_IDENTITY()
+		END
+	-- Get the Error Code for the statement just executed.
+	--PRINT 'SETTING ERROR CODE'
+	SELECT @P_ErrorCode=@@ERROR
+	EXEC ZGWSystem.Get_Name_Value_Pair_Detail @P_NVPSeqId, @P_NVP_DetailSeqId, @P_Debug
+
 RETURN 0
 
 GO
-
--- Core/DatabaseManager/Scripts/Downgrade/SQLServer/Version_3.1.0.0.sql
+++ Core/DatabaseManager/Scripts/Downgrade/SQLServer/Version_3.1.0.0.sql
@@ -479,7 +479,293 @@ RETURN 0
 
 GO
 /****** END: StoredProcedure [ZGWSystem].[Get_Name_Value_Pair_Detail] ******/
+/****** Start: StoredProcedure [ZGWSystem].[Set_Name_Value_Pair_Detail] ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+IF NOT EXISTS (SELECT *
+FROM sys.objects
+WHERE object_id = OBJECT_ID(N'[ZGWSystem].[Set_Name_Value_Pair_Detail]') AND type in (N'P', N'PC'))
+BEGIN
+	EXEC dbo.sp_executesql @statement = N'CREATE PROCEDURE [ZGWSystem].[Set_Name_Value_Pair_Detail] AS'
+END
+GO
 
+/*
+Usage:
+	DECLARE 
+		@V_NVP_DetailSeqId INT = -1,
+		@V_NVPSeqId int = (SELECT NVPSeqId FROM ZGWSystem.Name_Value_Pairs WHERE STATIC_NAME = 'Navigation_Types') ,
+		@V_NVP_Detail_Name VARCHAR(50) = 'Test',
+		@V_NVP_Detail_Value VARCHAR(300) = 'Test value',
+		@V_StatusSeqId INT = 1,
+		@V_Sort_Order INT = 1,
+		@V_Added_Updated_BY INT = 1,
+		@V_Primary_Key INT = null,
+		@V_ErrorCode int = null,
+		@V_Debug bit = 1
+
+	exec ZGWSystem.Set_Name_Value_Pair_Detail
+		@V_NVP_DetailSeqId,
+		@V_NVPSeqId,
+		@V_NVP_Detail_Name,
+		@V_NVP_Detail_Value,
+		@V_StatusSeqId,
+		@V_Sort_Order,
+		@V_Added_Updated_BY,
+		@V_Primary_Key,
+		@V_ErrorCode,
+		@V_Debug
+*/
+-- =============================================
+-- Author:		Michael Regan
+-- Create date: 07/26/2011
+-- Description:	Inserts/Updates ZGWCoreWeb.Account_Choices based on @P_Account
+-- =============================================
+ALTER PROCEDURE [ZGWSystem].[Set_Name_Value_Pair_Detail]
+	@P_NVP_DetailSeqId INT,
+	@P_NVPSeqId int,
+	@P_NVP_Detail_Name VARCHAR(50),
+	@P_NVP_Detail_Value VARCHAR(300),
+	@P_StatusSeqId INT,
+	@P_Sort_Order INT,
+	@P_Added_Updated_By INT,
+	@P_Primary_Key INT OUTPUT,
+	@P_ErrorCode int OUTPUT,
+	@P_Debug INT = 0
+AS
+	IF @P_Debug = 1 PRINT 'Starting [ZGWSystem].[Set_Name_Value_Pair_Detail]'
+	DECLARE 
+		@V_Static_Name VARCHAR(30) = (SELECT Static_Name
+FROM ZGWSystem.Name_Value_Pairs
+WHERE NVPSeqId = @P_NVPSeqId)
+		,@V_Schema_Name VARCHAR(30) = (SELECT [Schema_Name]
+FROM ZGWSystem.Name_Value_Pairs
+WHERE NVPSeqId = @P_NVPSeqId)
+		,@V_Statement NVARCHAR(4000)
+		,@V_Now DATETIME = GETDATE()
+
+	IF @P_NVP_DetailSeqId > -1
+		BEGIN
+	-- UPDATE PROFILE
+	SET @V_Statement = 'UPDATE ' + CONVERT(VARCHAR,@V_Schema_Name) + '.' + CONVERT(VARCHAR,@V_Static_Name) + '
+			SET 
+				NVP_Detail_Name = ''' + CONVERT(VARCHAR,@P_NVP_Detail_Name) + ''',
+				NVP_Detail_Value = ''' + CONVERT(VARCHAR,@P_NVP_Detail_Value) + ''',
+				StatusSeqId = ' + CONVERT(VARCHAR,@P_StatusSeqId) + ',
+				Sort_Order = ' + CONVERT(VARCHAR,@P_Sort_Order) + ',
+				Updated_By = ' + CONVERT(VARCHAR,@P_Added_Updated_By) + ',
+				UPDATED_DATE = ''' + CONVERT(VARCHAR,@V_Now) + '''
+			WHERE
+				NVP_DetailSeqId = ' + CONVERT(VARCHAR,@P_NVP_DetailSeqId)
+	IF @P_Debug = 1 PRINT @V_Statement
+	EXECUTE dbo.sp_executesql @statement = @V_Statement
+	SELECT @P_Primary_Key = @P_NVPSeqId
+END
+	ELSE
+		BEGIN
+	-- INSERT a new row in the table.
+	-- CHECK FOR DUPLICATE NAME BEFORE INSERTING
+	DECLARE @V_COUNT INT
+	SET @V_Statement= 'SET @V_COUNT = (SELECT COUNT(*)
+				   FROM ' + CONVERT(VARCHAR,@V_Schema_Name) + '.' + CONVERT(VARCHAR,@V_Static_Name) + '
+				   WHERE NVP_Detail_Value = ''' + CONVERT(VARCHAR,@P_NVP_Detail_Value) + ''' AND NVP_Detail_Name = ''' + CONVERT(VARCHAR,@P_NVP_Detail_Name) + ''')'
+	IF @P_Debug = 1 PRINT @V_Statement
+	EXECUTE sp_executesql @V_Statement,N'@V_COUNT int output',@V_COUNT output
+	IF @V_COUNT > 0
+				BEGIN
+		RAISERROR ('The entry already exists in the database.',16,1)
+		RETURN
+	END
+	SET @V_Statement = 'INSERT INTO ' + CONVERT(VARCHAR,@V_Schema_Name) + '.' + CONVERT(VARCHAR,@V_Static_Name) + '(
+					NVPSeqId,
+					NVP_Detail_Name,
+					NVP_Detail_Value,
+					StatusSeqId,
+					Sort_Order,
+					Added_By,
+					ADDED_DATE
+				)
+				VALUES
+				(
+					' + CONVERT(VARCHAR,@P_NVPSeqId) + ',
+					''' + CONVERT(VARCHAR,@P_NVP_Detail_Name) + ''',
+					''' + CONVERT(VARCHAR,@P_NVP_Detail_Value) + ''',
+					' + CONVERT(VARCHAR,@P_StatusSeqId) + ',
+					' + CONVERT(VARCHAR,@P_Sort_Order) + ',
+					' + CONVERT(VARCHAR,@P_Added_Updated_By) + ',
+					''' + CONVERT(VARCHAR,@V_Now) + '''
+				)'
+	IF @P_Debug = 1 PRINT @V_Statement
+	EXECUTE dbo.sp_executesql @statement = @V_Statement
+	SELECT @P_Primary_Key=SCOPE_IDENTITY()
+-- Get the IDENTITY value for the row just inserted.
+--PRINT 'DONE ADDING'
+END
+-- Get the Error Code for the statement just executed.
+--PRINT 'SETTING ERROR CODE'
+SELECT @P_ErrorCode=@@ERROR
+RETURN 0
+
+GO
+/****** END: StoredProcedure [ZGWSystem].[Set_Name_Value_Pair_Detail] ******/
+/****** Start: StoredProcedure [ZGWSystem].[Get_Name_Value_Pair_Details] ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+IF NOT EXISTS (SELECT *
+FROM sys.objects
+WHERE object_id = OBJECT_ID(N'[ZGWSystem].[Get_Name_Value_Pair_Details]') AND type in (N'P', N'PC'))
+BEGIN
+	EXEC dbo.sp_executesql @statement = N'CREATE PROCEDURE [ZGWSystem].[Get_Name_Value_Pair_Details] AS'
+END
+GO
+/*
+Usage:
+	DECLARE
+		@P_NVPSeqId int = 1,
+		@P_Debug INT = 1
+
+	exec ZGWSystem.Get_Name_Value_Pair_Details
+		@P_NVPSeqId,
+		@P_Debug
+*/
+-- =============================================
+-- Author:		Michael Regan
+-- Create date: 08/19/2011
+-- Description:	Returns name value pair details 
+-- Note:
+--	This not the most effecient however this should
+--	not be called very often ... it is intended for the
+--	front end to cache the information and only get called
+--	when needed
+-- =============================================
+ALTER PROCEDURE [ZGWSystem].[Get_Name_Value_Pair_Details] @P_NVPSeqId INT
+	,@P_Debug INT = 0
+AS
+SET NOCOUNT ON
+
+IF @P_Debug = 1
+	PRINT 'Starting ZGWSecurity.Get_Name_Value_Pair_Details'
+
+CREATE TABLE #NVP_DETAILS (
+	 NVP_DetailSeqId INT
+	,NVPSeqId INT
+	,NVP_Detail_Name VARCHAR(50)
+	,NVP_Detail_Value VARCHAR(300)
+	,StatusSeqId INT
+	,Sort_Order INT
+	,Added_By INT
+	,Added_Date DATETIME
+	,Updated_By INT
+	,Updated_Date DATETIME
+	)
+
+DECLARE @V_NVPSeqId INT
+	,@V_Static_Name VARCHAR(30)
+	,@V_Schema_Name VARCHAR(30)
+	,@V_Statement NVARCHAR(max)
+
+SET @V_Statement = 'SELECT * FROM '
+
+DECLARE V_Name_Value_Pairs CURSOR STATIC LOCAL
+FOR
+SELECT NVPSeqId
+	,Static_Name
+	,[Schema_Name]
+FROM ZGWSystem.Name_Value_Pairs
+
+OPEN V_Name_Value_Pairs
+
+FETCH NEXT
+FROM V_Name_Value_Pairs
+INTO @V_NVPSeqId
+	,@V_Static_Name
+	,@V_Schema_Name
+
+WHILE (@@FETCH_STATUS = 0)
+BEGIN
+	SET @V_Statement = @V_Statement + CONVERT(VARCHAR, @V_Schema_Name) + '.' + CONVERT(VARCHAR, @V_Static_Name) + ' UNION ALL SELECT * FROM '
+
+	FETCH NEXT
+	FROM V_Name_Value_Pairs
+	INTO @V_NVPSeqId
+		,@V_Static_Name
+		,@V_Schema_Name
+END
+
+CLOSE V_Name_Value_Pairs
+
+DEALLOCATE V_Name_Value_Pairs
+
+SET @V_Statement = SUBSTRING(@V_Statement, 0, LEN(@V_Statement) - 23)
+
+IF @P_Debug = 1
+	PRINT @V_Statement
+
+INSERT INTO #NVP_DETAILS
+EXECUTE dbo.sp_executesql @statement = @V_Statement
+
+IF @P_NVPSeqId = - 1
+	SELECT #NVP_DETAILS.NVP_DetailSeqId AS NVP_SEQ_DET_ID
+		,#NVP_DETAILS.NVPSeqId AS NVP_SEQ_ID
+		,ZGWSystem.Name_Value_Pairs.[Schema_Name] + '.' + ZGWSystem.Name_Value_Pairs.Static_Name AS [Table_Name]
+		,#NVP_DETAILS.NVP_Detail_Name AS NVP_DET_VALUE
+		,#NVP_DETAILS.NVP_Detail_Value AS NVP_DET_TEXT
+		,#NVP_DETAILS.StatusSeqId AS STATUS_SEQ_ID
+		,#NVP_DETAILS.Sort_Order
+		,(
+			SELECT TOP (1) Account
+			FROM ZGWSecurity.Accounts
+			WHERE AccountSeqId = #NVP_DETAILS.Added_By
+			) AS Added_By
+		,#NVP_DETAILS.Added_Date
+		,(
+			SELECT TOP (1) Account
+			FROM ZGWSecurity.Accounts
+			WHERE AccountSeqId = #NVP_DETAILS.Updated_By
+			) AS Updated_By
+		,#NVP_DETAILS.Updated_Date
+	FROM #NVP_DETAILS
+		,ZGWSystem.Name_Value_Pairs
+	WHERE #NVP_DETAILS.NVPSeqId = ZGWSystem.Name_Value_Pairs.NVPSeqId
+	ORDER BY ZGWSystem.Name_Value_Pairs.Static_Name
+		,#NVP_DETAILS.NVP_Detail_Value
+ELSE
+	SELECT #NVP_DETAILS.NVP_DetailSeqId AS NVP_SEQ_DET_ID
+		,#NVP_DETAILS.NVPSeqId AS NVP_SEQ_ID
+		,ZGWSystem.Name_Value_Pairs.[Schema_Name] + '.' + ZGWSystem.Name_Value_Pairs.Static_Name AS [Table_Name]
+		,#NVP_DETAILS.NVP_Detail_Name AS NVP_DET_VALUE
+		,#NVP_DETAILS.NVP_Detail_Value AS NVP_DET_TEXT
+		,#NVP_DETAILS.StatusSeqId AS STATUS_SEQ_ID
+		,#NVP_DETAILS.Sort_Order
+		,(
+			SELECT TOP (1) Account
+			FROM ZGWSecurity.Accounts
+			WHERE AccountSeqId = #NVP_DETAILS.Added_By
+			) AS Added_By
+		,#NVP_DETAILS.Added_Date
+		,(
+			SELECT TOP (1) Account
+			FROM ZGWSecurity.Accounts
+			WHERE AccountSeqId = #NVP_DETAILS.Updated_By
+			) AS Updated_By
+		,#NVP_DETAILS.Updated_Date
+	FROM #NVP_DETAILS
+		,ZGWSystem.Name_Value_Pairs
+	WHERE #NVP_DETAILS.NVPSeqId = ZGWSystem.Name_Value_Pairs.NVPSeqId
+		AND ZGWSystem.Name_Value_Pairs.NVPSeqId = @P_NVPSeqId
+	ORDER BY ZGWSystem.Name_Value_Pairs.Static_Name
+		,#NVP_DETAILS.NVP_Detail_Value
+
+DROP TABLE #NVP_DETAILS
+
+RETURN 0
+
+GO
+/****** End: StoredProcedure [ZGWSystem].[Get_Name_Value_Pair_Details] ******/
 
 -- Update the version
 UPDATE [ZGWSystem].[Database_Information] SET
