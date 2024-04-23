ce95a85828be7de9e69fa1e1a26219510b5195f8
kahiko <michael.regan@verizon.net>
2024-04-23 14:22:58 -0400

    Got Web.Angular to use Web.Api

--- Net_Core/Web.Angular/Angular/proxy.conf.js
+++ Net_Core/Web.Angular/Angular/proxy.conf.js
@@ -1,7 +1,7 @@
 /* eslint-disable linebreak-style */
 const { env } = require('process');
 
-const target = env.ASPNETCORE_HTTPS_PORT ? `https://localhost:${env.ASPNETCORE_HTTPS_PORT}` : env.ASPNETCORE_URLS ? env.ASPNETCORE_URLS.split(';')[0] : 'http://localhost:35360';
+const target = env.ASPNETCORE_HTTPS_PORT ? `https://localhost:${env.ASPNETCORE_HTTPS_PORT}` : env.ASPNETCORE_URLS ? env.ASPNETCORE_URLS.split(';')[0] : 'http://localhost:5000';
 console.log(`proxy.conf.js - Proxy target: '${target}'`);
 // If you add or remove any API here you should think about changing jwt.interceptor.ts (JwtInterceptor)
 const PROXY_CONFIG = [
