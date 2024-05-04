ea3cc696e82e860802952e89628beb1442942ffc
kahiko <michael.regan@verizon.net>
2024-05-03 22:32:19 -0400

    Updated [ZGWSecurity].[Get_Function_Sort] to match the DB

--- Net_Core/Database/ZGWSecurity/StoredProcedures/Get_Function_Sort.sql
+++ Net_Core/Database/ZGWSecurity/StoredProcedures/Get_Function_Sort.sql
@@ -15,35 +15,42 @@ Usage:
 -- Description:	Returns sorted function information
 --	for related functions given the funtionSeqId
 -- =============================================
+-- Author:		Michael Regan
+-- Create date: 05/03/2024
+-- Description:	Fixed but where nothing is returned if the ParentSeqId <> 1
+--  this should have been ParentSeqId = @V_Parent_ID
+-- =============================================
 CREATE PROCEDURE [ZGWSecurity].[Get_Function_Sort]
 	@P_FunctionSeqId INT,
 	@P_Debug INT = 0
 AS
-	SET NOCOUNT ON
-	DECLARE @V_Parent_ID INT
-	DECLARE @V_NAV_TYPE_ID INT
-	SET @V_Parent_ID = (SELECT ParentSeqId
-FROM ZGWSecurity.Functions
-WHERE FunctionSeqId = @P_FunctionSeqId)
-	SET @V_NAV_TYPE_ID = (SELECT Navigation_Types_NVP_DetailSeqId
-FROM ZGWSecurity.Functions
-WHERE FunctionSeqId = @P_FunctionSeqId)
-	SELECT
-	FunctionSeqId as FUNCTION_SEQ_ID,
-	[Name],
-	[Action],
-	Sort_Order
-FROM
-	ZGWSecurity.Functions WITH(NOLOCK)
-WHERE
-		ParentSeqId = @V_PARENT_ID
-	AND Is_Nav = 1
-	AND Navigation_Types_NVP_DetailSeqId = @V_NAV_TYPE_ID
-	AND ParentSeqId <> 1
-ORDER BY
-		Sort_Order ASC
+	SET NOCOUNT ON;
 
-RETURN 0
+	DECLARE @V_Parent_ID INT
+			, @V_NAV_TYPE_ID INT;
+
+	SET @V_Parent_ID = (
+			SELECT ParentSeqId
+			FROM ZGWSecurity.Functions
+			WHERE FunctionSeqId = @P_FunctionSeqId
+		);
+	SET @V_NAV_TYPE_ID = (
+			SELECT Navigation_Types_NVP_DetailSeqId
+			FROM ZGWSecurity.Functions
+			WHERE FunctionSeqId = @P_FunctionSeqId
+		);
+	SELECT 
+		  FunctionSeqId AS FUNCTION_SEQ_ID
+		, [Name]
+		, [Action]
+		, Sort_Order
+	FROM ZGWSecurity.Functions WITH (NOLOCK)
+	WHERE ParentSeqId = @V_PARENT_ID
+		AND Is_Nav = 1
+		AND Navigation_Types_NVP_DetailSeqId = @V_NAV_TYPE_ID
+		AND ParentSeqId = @V_Parent_ID
+	ORDER BY Sort_Order ASC;
+
+SET NOCOUNT OFF;
 
 GO
-
