9f4e6e667046a8a3dc1daf20c77aa79db4e9d5e3
kahiko <michael.regan@verizon.net>
2024-05-21 03:54:17 -0400

    Now accoutns for the accounts/reset-password route

--- Net_Core/Web.Angular/projects/gw-frontend/src/app/app.initializer.ts
+++ Net_Core/Web.Angular/projects/gw-frontend/src/app/app.initializer.ts
@@ -1,27 +1,40 @@
+import { ActivatedRoute } from '@angular/router';
 import { finalize } from 'rxjs';
 // Library
 import { AccountService, IAuthenticationResponse } from '@growthware/core/account';
 import { ConfigurationService } from '@growthware/core/configuration';
 import { DynamicTableService } from '@growthware/core/dynamic-table';
+
 export function appInitializer(
-	accountSvc: AccountService, 
-	configurationSvc: ConfigurationService,
-	dynamicTableSvc: DynamicTableService,
+	_ActivatedRoute: ActivatedRoute,
+	_AccountSvc: AccountService, 
+	_ConfigurationSvc: ConfigurationService,
+	_DynamicTableSvc: DynamicTableService,
 ) {
 	// Called by app.config.ts:ApplicationConfig
 	return () => {
 		return new Promise((resolve) => {
-			configurationSvc.loadAppSettings();
-			dynamicTableSvc.loadDefaultTableConfig();
-			accountSvc.refreshToken().pipe(finalize(() => {
+			_ConfigurationSvc.loadAppSettings();
+			_DynamicTableSvc.loadDefaultTableConfig();
+			_AccountSvc.refreshToken().pipe(finalize(() => {
 				resolve(true);
 			})).subscribe({
 				next: (authenticationResponse: IAuthenticationResponse) => {
 					// console.log('appInitializer.authenticationResponse', authenticationResponse);
-					// We need to log out for the anonymous account in order to generate a Json Web Token
-					// that is used in auth-guard.guard.ts (canActivate)
-					if (authenticationResponse.account.toLocaleLowerCase() === 'anonymous') {
-						accountSvc.logout(true);
+					// For the '/accounts/reset-password' route we only need to log out without the default navigation
+					// Otherwise if the accunt is anonymous we need to log out and navigate to the default page
+					const isResetPasswordRoute = window.location && window.location.pathname && window.location.pathname.toLowerCase() === '/accounts/reset-password';
+					if(!isResetPasswordRoute) {
+						// We need to log out for the anonymous account in order to generate a Json Web Token
+						// that is used in auth-guard.guard.ts (canActivate)
+						if (authenticationResponse.account.toLocaleLowerCase() === 'anonymous') {
+							_AccountSvc.logout(true);
+						}
+					} else {
+						// We need to always log out and generate a Json Web Token that is used in auth-guard.guard.ts (canActivate).
+						// We do not want to navigate afterwards so that the ChangePasswordComponent component is rendered.
+						// See accounts-routing.module.ts
+						_AccountSvc.logout(false, false);
 					}
 				},
 				error: (error) => {
