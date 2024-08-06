08d10ee7210ef70d2f40269e4fdcd59e07534708
kahiko <michael.regan@verizon.net>
2024-08-06 07:23:24 -0400

    Added VerifyAccount

---
+++ Net_Core/Web.Angular/projects/gw-lib/core/account/src/c/verify-account/verify-account.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'gw-core-verify-account',
+  standalone: true,
+  imports: [],
+  templateUrl: './verify-account.component.html',
+  styleUrl: './verify-account.component.scss'
+})
+export class VerifyAccountComponent {
+
+}
