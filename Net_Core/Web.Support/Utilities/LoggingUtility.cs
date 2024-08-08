3e51a14620c08ce912b1f78aab06ab0b652abe67
kahiko <michael.regan@verizon.net>
2024-08-08 04:22:14 -0400

    Added getBusinessLogic for performance

--- Net_Core/Web.Support/Utilities/LoggingUtility.cs
+++ Net_Core/Web.Support/Utilities/LoggingUtility.cs
@@ -5,15 +5,28 @@ using GrowthWare.Framework.Models;
 namespace GrowthWare.Web.Support.Utilities;
 public static class LoggingUtility
 {
+    private static BLogger m_BusinessLogic = null;
+    /// <summary>
+    /// Returns the business logic object used to access the database.
+    /// </summary>
+    /// <returns></returns>
+    private static BLogger getBusinessLogic()
+    {
+        if(m_BusinessLogic == null || ConfigSettings.CentralManagement == true)
+        {
+            m_BusinessLogic = new(ConfigSettings.DataAccessLayerAssemblyName, ConfigSettings.DataAccessLayerNamespace, ConfigSettings.ConnectionString);
+        }
+        return m_BusinessLogic;
+    }
 
     public static MLoggingProfile GetProfile(int logSeqId)
     {
-        BLogger mBLogger = new BLogger(ConfigSettings.DataAccessLayerAssemblyName, ConfigSettings.DataAccessLayerNamespace, ConfigSettings.ConnectionString);
+        BLogger mBLogger = getBusinessLogic();
         return mBLogger.GetLoggingProfile(logSeqId);
     }
     public static void Save(MLoggingProfile profile)
     {
-        BLogger mBLogger = new BLogger(ConfigSettings.DataAccessLayerAssemblyName, ConfigSettings.DataAccessLayerNamespace, ConfigSettings.ConnectionString);
+        BLogger mBLogger = getBusinessLogic();
         mBLogger.Save(profile);
     }
 }
