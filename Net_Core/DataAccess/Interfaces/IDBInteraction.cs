a9ed6a7f5bf15b2d99d3953b35623726041070f0
kahiko <michael.regan@verizon.net>
2024-08-13 10:09:09 -0400

    Removed comment

--- Net_Core/DataAccess/Interfaces/IDBInteraction.cs
+++ Net_Core/DataAccess/Interfaces/IDBInteraction.cs
@@ -6,15 +6,6 @@ namespace GrowthWare.DataAccess.Interfaces.Base;
 /// </summary>
 public interface IDBInteraction
 {
-    /**
-      * TODO: Currently a SqlConnection is created in earch of the following methods:
-      *   ExecuteScalar
-      *   ExecuteNonQuery
-      *   GetDataSet
-      * It might be worth revising it to create one and reuse it and removing the neede for
-      * m_ConnectionString.
-      */
-
     /// <summary>
     /// Gets or sets the connection string.
     /// </summary>
