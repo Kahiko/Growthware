3b6cd9d01f93365b94b18b26bb076a41d25da4d1
kahiko <michael.regan@verizon.net>
2024-08-19 06:15:47 -0400

    Added fontColor

--- Net_Core/Web.Angular/projects/gw-lib/core/navigation/src/c/vertical/vertical.component.ts
+++ Net_Core/Web.Angular/projects/gw-lib/core/navigation/src/c/vertical/vertical.component.ts
@@ -1,5 +1,5 @@
 import { Component, Input, OnInit } from '@angular/core';
-
+import { CommonModule } from '@angular/common';
 import { Router, RouterModule } from '@angular/router';
 // Angular Material
 import { MatIconModule } from '@angular/material/icon';
@@ -20,6 +20,7 @@ import { INavLink } from '../../nav-link.model';
 	selector: 'gw-core-vertical',
 	standalone: true,
 	imports: [
+		CommonModule,
 		RouterModule,
 		MatIconModule,
 		MatListModule
@@ -29,32 +30,33 @@ import { INavLink } from '../../nav-link.model';
 })
 export class VerticalComponent extends BaseNavigationComponent implements OnInit {
 
-  @Input() override name: string = '';
+	@Input() fontColor: string = 'white';
+	@Input() override name: string = '';
 
-  constructor(
-  	accountSvc: AccountService,
-  	dataSvc: DataService,
-  	gwCommon: GWCommon,
-  	loggingSvc: LoggingService,
-  	menuListSvc: NavigationService,
-  	modalSvc: ModalService,
-  	runter: Router,
-  ) {
-  	super();
-  	this._AccountSvc = accountSvc;
-  	this._DataSvc = dataSvc;
-  	this._GWCommon = gwCommon;
-  	this._LoggingSvc = loggingSvc;
-  	this._NavigationSvc = menuListSvc;
-  	this._ModalSvc = modalSvc;
-  	this._Router = runter;
-  }
+	constructor(
+		accountSvc: AccountService,
+		dataSvc: DataService,
+		gwCommon: GWCommon,
+		loggingSvc: LoggingService,
+		menuListSvc: NavigationService,
+		modalSvc: ModalService,
+		runter: Router,
+	) {
+		super();
+		this._AccountSvc = accountSvc;
+		this._DataSvc = dataSvc;
+		this._GWCommon = gwCommon;
+		this._LoggingSvc = loggingSvc;
+		this._NavigationSvc = menuListSvc;
+		this._ModalSvc = modalSvc;
+		this._Router = runter;
+	}
 
-  ngOnInit(): void {
-  	this._MenuType = MenuTypes.Vertical;
-  }
+	ngOnInit(): void {
+		this._MenuType = MenuTypes.Vertical;
+	}
 
-  override onItemSelected(item: INavLink) {
-  	super.onItemSelected(item);
-  }
+	override onItemSelected(item: INavLink) {
+		super.onItemSelected(item);
+	}
 }
