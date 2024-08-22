d6ba1f5211ae7a8f9fe3b03eb2a78e14222483b2
kahiko <michael.regan@verizon.net>
2024-08-22 06:56:50 -0400

    WIP - Create and Delete now working

--- Net_Core/DataAccess/Oracle/DDatabaseManager.cs
+++ Net_Core/DataAccess/Oracle/DDatabaseManager.cs
@@ -4,24 +4,26 @@ using GrowthWare.Framework;
 using Oracle.ManagedDataAccess.Client;
 using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Diagnostics;
 using System.IO;
 using System.Linq;
+using System.Text.RegularExpressions;
 
 namespace GrowthWare.DataAccess.Oracle;
 
 public class DDatabaseManager : AbstractDBInteraction, IDatabaseManager
 {
-    internal string m_ConnectionWithoutDatabaseName = string.Empty;
     private string m_DatabaseName = string.Empty;
+    private string m_ScriptPath = string.Empty;
 
-    public string DatabaseName 
-    { 
+    public string DatabaseName
+    {
         get
         {
             return this.m_DatabaseName;
         }
-        set 
+        set
         {
             this.m_DatabaseName = value;
         }
@@ -29,35 +31,113 @@ public class DDatabaseManager : AbstractDBInteraction, IDatabaseManager
 
     public void Create()
     {
-        string mDataBaseName = this.m_DatabaseName;
-        string mDBNameUpper = this.m_DatabaseName.ToUpper();
-        string mDBNameLower = this.m_DatabaseName.ToLower();
-        string mFilePath = ConfigSettings.PathToPDBFiles;
-        string mCreateDBSQLStatement = @"create pluggable database {0} admin user {1} identified by '{3}'
-default tablespace {0}_USERS
-datafile '{2}\PDBSEED\GROWTHWARE\growthware_users01.dbf' size 250m autoextend on
-storage (maxsize 1g max_shared_temp_size 1g)
-file_name_convert=('{2}\PDBSEED\','{2}\{3}\');";
-        mCreateDBSQLStatement = mCreateDBSQLStatement.Replace(@"\", Path.DirectorySeparatorChar.ToString());
-        mCreateDBSQLStatement = mCreateDBSQLStatement.Replace(@"/", Path.DirectorySeparatorChar.ToString());
-        mCreateDBSQLStatement = string.Format(mCreateDBSQLStatement, mDBNameUpper, mDBNameLower, mFilePath, mDataBaseName);
-        string mike = string.Empty;
+        this.ConnectionString = ConfigSettings.ContainerConnectionString;
+        // Get the file location for the new PDB
+        string mDatabaseName = ConfigSettings.DataAccessLayerDatabaseName;
+        string mDatabasePassword = getPassword();
+        string mFileLocationQuery = "SELECT file_name FROM dba_data_files WHERE tablespace_name = 'SYSTEM'";
+        string mSeedFileLocation = this.ExecuteScalar(mFileLocationQuery).ToString().Replace("system01.dbf", "", StringComparison.OrdinalIgnoreCase).TrimEnd(Path.DirectorySeparatorChar);
+        mSeedFileLocation = mSeedFileLocation.Replace(@"\", @"/");
+        mSeedFileLocation = mSeedFileLocation.Replace(@"/", Path.DirectorySeparatorChar.ToString());
+        // Construct the datafile path
+        string mDataPath = $@"{mSeedFileLocation}\{ConfigSettings.DataAccessLayerDatabaseName}";
+        string mDataFile = $@"{mDataPath}\{mDatabaseName}_users01.dbf";
+        string mUpper = mDatabaseName.ToUpper();
+        string mLower = mDatabaseName.ToLower();
+        // Create the pluggable database
+        string mCreationText = $@"CREATE PLUGGABLE DATABASE {mDatabaseName}
+  ADMIN USER {mLower} IDENTIFIED BY ""{mDatabasePassword}""
+  ROLES = (dba)
+  DEFAULT TABLESPACE {mUpper}_USERS
+    DATAFILE '{mDataFile}' SIZE 250M AUTOEXTEND ON
+  FILE_NAME_CONVERT = ('{mSeedFileLocation + Path.DirectorySeparatorChar}',
+                       '{mDataPath + Path.DirectorySeparatorChar}')
+  STORAGE (MAXSIZE 1G)
+  PATH_PREFIX = '{mDataPath + Path.DirectorySeparatorChar}'";
+        try
+        {
+            this.ExecuteNonQuery(mCreationText);
+            // Open the pluggable database
+            string mOpenPdbQuery = $"ALTER PLUGGABLE DATABASE {mUpper} OPEN READ WRITE";
+            this.ExecuteNonQuery(mOpenPdbQuery);
+        }
+        catch (OracleException ex)
+        {
+            Logger.Instance().Error(ex);
+            throw;
+        }
     }
 
     public void Delete()
     {
-        throw new NotImplementedException();
+        this.ConnectionString = ConfigSettings.ContainerConnectionString;
+        string mUpper = ConfigSettings.DataAccessLayerDatabaseName.ToUpper();
+        // Close the pluggable database account for the database being closed already.
+        string mSqlStatement = $@"ALTER PLUGGABLE DATABASE {mUpper} CLOSE";
+        try
+        {
+            this.ExecuteNonQuery(mSqlStatement);
+        }
+        catch (DataAccessLayerException ex)
+        {
+            // If it's not an ORA-65020 error, rethrow the exception
+            if(!ex.InnerException.Message.Contains("ORA-65020", StringComparison.OrdinalIgnoreCase))
+            {
+                throw;
+            }
+        }
+        // Drop the database
+        mSqlStatement = $@"DROP PLUGGABLE DATABASE {mUpper} INCLUDING DATAFILES";
+        this.ExecuteNonQuery(mSqlStatement);
     }
 
     public bool ExecuteScriptFile(string scriptWithPath)
     {
         using OracleConnection mSqlConnection = new(this.ConnectionString);
-        return this.replace_N_Run(scriptWithPath, mSqlConnection);
+        return this.ExecuteScriptFile(scriptWithPath, mSqlConnection);
     }
 
-    private bool ExecuteScriptFile(string scriptWithPath, OracleConnection OracleConnection)
+    private bool ExecuteScriptFile(string scriptWithPath, OracleConnection oracleConnection)
     {
-        throw new NotImplementedException();
+        this.IsValid();
+        try
+        {
+            string mAllText = File.ReadAllText(scriptWithPath);
+
+            // split script on semi-colon
+            IEnumerable<string> mCommands = Regex.Split(mAllText, @"^\s*;\s*$", RegexOptions.Multiline | RegexOptions.IgnoreCase);
+            if (oracleConnection.State == ConnectionState.Closed)
+            {
+                oracleConnection.Open();
+            }
+            foreach (string mCommandText in mCommands)
+            {
+                if (!string.IsNullOrEmpty(mCommandText) && !string.IsNullOrWhiteSpace(mCommandText))
+                {
+                    try
+                    {
+                        using OracleCommand mOracleCommand = new(mCommandText, oracleConnection);
+                        mOracleCommand.CommandType = CommandType.Text;
+                        mOracleCommand.ExecuteNonQuery();
+                    }
+                    catch (OracleException ex)
+                    {
+                        string spError = mCommandText.Length > 100 ? mCommandText.Substring(0, 100) + " ...\n..." : mCommandText;
+                        string mMsg = string.Format("Please check the Oracle script.\nFile: {0} \nError: {1} \nSQL Command: \n{3}", scriptWithPath, ex.Message, spError, mCommandText);
+                        Console.WriteLine(mMsg);
+                        Logger.Instance().Error(mMsg);
+                        return false;
+                    }
+                }
+            }
+
+            return true;
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+            return false;
+        }
     }
 
     public bool Exists()
@@ -71,7 +151,7 @@ file_name_convert=('{2}\PDBSEED\','{2}\{3}\');";
             OracleParameter[] mOracleParameters = {
                 new("databaseName", ConfigSettings.DataAccessLayerDatabaseName.ToUpper())
             };
-            
+
             int mUserCount = Convert.ToInt32(this.ExecuteScalar(mSqlStatement, mOracleParameters, true));
             if (mUserCount > 0)
             {
@@ -81,14 +161,49 @@ file_name_convert=('{2}\PDBSEED\','{2}\{3}\');";
         catch (System.Exception ex)
         {
             DataAccessLayerException mException = new("DDatabaseManager.Exists", ex);
+            Logger.Instance().Error(mException);
             mRetVal = false;
+        }
+        return mRetVal;
+    }
+
+    /// <summary>
+    /// Helper function to pull the passwrod from the connection string
+    /// </summary>
+    /// <returns></returns>
+    private string getPassword()
+    {
+        string mRetVal = string.Empty;
+        string[] mConnectionStringParts = ConfigSettings.ConnectionString.Split(';');
+        foreach (string item in mConnectionStringParts)
+        {
+            string[] mNameValuePair = item.Split('=');
+            if (mNameValuePair.Length > 1)
+            {
+                if(mNameValuePair[0].Equals("password", StringComparison.OrdinalIgnoreCase))
+                {
+                    mRetVal = mNameValuePair[1];
+                }
+            }
         }
         return mRetVal;
     }
 
     public string GetScriptPath(string theDirection)
     {
-        throw new NotImplementedException();
+        if (m_ScriptPath == string.Empty)
+        {
+            string mCurrentDirectory = Directory.GetCurrentDirectory();
+            if (!mCurrentDirectory.EndsWith(Path.DirectorySeparatorChar.ToString()))
+            {
+                mCurrentDirectory += Path.DirectorySeparatorChar.ToString();
+            }
+            mCurrentDirectory += "Scripts/TheDirection/" + ConfigSettings.DataAccessLayer + Path.DirectorySeparatorChar;
+            mCurrentDirectory = mCurrentDirectory.Replace(@"/", @"\");
+            mCurrentDirectory = mCurrentDirectory.Replace(@"\", Path.DirectorySeparatorChar.ToString());
+            this.m_ScriptPath = mCurrentDirectory;
+        }
+        return m_ScriptPath.Replace("TheDirection", theDirection);
     }
 
     public Version GetVersion()
@@ -117,32 +232,32 @@ file_name_convert=('{2}\PDBSEED\','{2}\{3}\');";
 
     public void ProcessScriptFiles(bool isUpgrade, Version currentVersion, Version desiredVersion, IEnumerable<Version> availbleVersions)
     {
-            IEnumerable<Version> mVersions = null;
-            string mTheDirection = isUpgrade ? "Upgrade" : "Downgrade";
-            if(isUpgrade)
-            {
-                mVersions = availbleVersions.Where(version => version > currentVersion && version <= desiredVersion);
-            }
-            else
-            {
-                mVersions = availbleVersions.Where(version => version <= currentVersion && version > desiredVersion && version != new Version("1.0.0.0"));
-            }            
-            if (mVersions == null || mVersions.Count() == 0)
-            {
-                string mMsg = "There are no '{0}' files to execute that match the version. Requested: '{1}', Current: '{2}'";
-                Console.WriteLine(string.Format(mMsg, mTheDirection, desiredVersion.ToString(), currentVersion.ToString()));
-                return;
-            }
-            this.ConnectionString = ConfigSettings.ConnectionString;
-            foreach (Version item in mVersions)
-            {
-                string mScriptWithPath = this.GetScriptPath(mTheDirection) + "Version_" + item.ToString() + ".sql";
-                string mFileName = "Version_" + item.ToString() + ".sql";
-                Stopwatch mScriptWatch = new();
-                mScriptWatch.Start();
-                this.ExecuteScriptFile(mScriptWithPath);
-                Console.WriteLine("Elapsed time: {0} File: '{1}' ", mScriptWatch.Elapsed, mFileName);
-            }
+        IEnumerable<Version> mVersions = null;
+        string mTheDirection = isUpgrade ? "Upgrade" : "Downgrade";
+        if (isUpgrade)
+        {
+            mVersions = availbleVersions.Where(version => version > currentVersion && version <= desiredVersion);
+        }
+        else
+        {
+            mVersions = availbleVersions.Where(version => version <= currentVersion && version > desiredVersion && version != new Version("1.0.0.0"));
+        }
+        if (mVersions == null || mVersions.Count() == 0)
+        {
+            string mMsg = "There are no '{0}' files to execute that match the version. Requested: '{1}', Current: '{2}'";
+            Console.WriteLine(string.Format(mMsg, mTheDirection, desiredVersion.ToString(), currentVersion.ToString()));
+            return;
+        }
+        this.ConnectionString = ConfigSettings.ConnectionString;
+        foreach (Version item in mVersions)
+        {
+            string mScriptWithPath = this.GetScriptPath(mTheDirection) + "Version_" + item.ToString() + ".sql";
+            string mFileName = "Version_" + item.ToString() + ".sql";
+            Stopwatch mScriptWatch = new();
+            mScriptWatch.Start();
+            this.ExecuteScriptFile(mScriptWithPath);
+            Console.WriteLine("Elapsed time: {0} File: '{1}' ", mScriptWatch.Elapsed, mFileName);
+        }
     }
 
     private bool replace_N_Run(string scriptFile, OracleConnection sqlConnection)
