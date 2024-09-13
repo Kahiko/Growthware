059521cb65c0bffd2781aa44f260a38c4b12dcfa
kahiko <michael.regan@verizon.net>
2024-09-13 11:36:02 -0400

    Formatting

--- Net_Core/Web.Angular/projects/gw-lib/core/pick-list/src/c/list/list.component.ts
+++ Net_Core/Web.Angular/projects/gw-lib/core/pick-list/src/c/list/list.component.ts
@@ -25,64 +25,64 @@ import { ModalOptions, ModalService, ModalSize } from '@growthware/core/modal';
 	styleUrls: ['./list.component.scss']
 })
 export class ListComponent implements OnInit {
-  @Input() allItemsText: string = '';
-  @Input() header: string = '';
-  @Input() id: string = '';
-  @Input() name: string = '';
-  @Input() pickListTableHelp: string = '';
-  @Input() selectedItemsText: string = '';
-  @Input() size: string = '8';
-  @Input() width: string = '120';
+	@Input() allItemsText: string = '';
+	@Input() header: string = '';
+	@Input() id: string = '';
+	@Input() name: string = '';
+	@Input() pickListTableHelp: string = '';
+	@Input() selectedItemsText: string = '';
+	@Input() size: string = '8';
+	@Input() width: string = '120';
 
-  private _AvailableItemsSubject = new BehaviorSubject<any[]>([]);
-  private _AvailableItemsData: any[] = [];
-  private _ModalOptions!: ModalOptions;
+	private _AvailableItemsSubject = new BehaviorSubject<any[]>([]);
+	private _AvailableItemsData: any[] = [];
+	private _ModalOptions!: ModalOptions;
 
-  readonly availableItems$ = this._AvailableItemsSubject.asObservable();
+	readonly availableItems$ = this._AvailableItemsSubject.asObservable();
 
-  private _Subscriptions: Subscription = new Subscription();
+	private _Subscriptions: Subscription = new Subscription();
 
 
-  constructor(
-    private _DataSvc: DataService,
-    private _GWCommon: GWCommon,
-    private _LoggingSvc: LoggingService,
-    private _ModalSvc: ModalService
-  ) {
-  	// nothing atm
-  }
+	constructor(
+		private _DataSvc: DataService,
+		private _GWCommon: GWCommon,
+		private _LoggingSvc: LoggingService,
+		private _ModalSvc: ModalService
+	) {
+		// nothing atm
+	}
 
-  ngOnInit(): void {
-  	if (!this._GWCommon.isNullOrUndefined(this.id) && !this._GWCommon.isNullOrEmpty(this.id)) {
-  		this._ModalOptions = new ModalOptions(this.id + '_Modal', this.header, this.pickListTableHelp, ModalSize.Small);
-  		this._Subscriptions.add(
-  			this._DataSvc.dataChanged$.subscribe((results: INameDataPair) => {
-  				if (this.name.trim().toLowerCase() + '_availableitems' === results.name.trim().toLowerCase()) {
-  					// update the local data
-  					this._AvailableItemsData = results.value;
-  					this._AvailableItemsSubject.next(this._AvailableItemsData);
-  				}
-  			})
-  		);
-  	} else {
-  		const mLogDestinations: Array<LogDestination> = [];
-  		mLogDestinations.push(LogDestination.Console);
-  		mLogDestinations.push(LogDestination.Toast);
-  		const mLogOptions: ILogOptions = new LogOptions(
-  			'PickListComponent.ngOnInit: id is blank',
-  			LogLevel.Error,
-  			mLogDestinations,
-  			'PickListComponent',
-  			'PickListComponent',
-  			'ngOnInit',
-  			'system',
-  			'PickListComponent'
-  		);
-  		this._LoggingSvc.log(mLogOptions);
-  	}
-  }
+	ngOnInit(): void {
+		if (!this._GWCommon.isNullOrUndefined(this.id) && !this._GWCommon.isNullOrEmpty(this.id)) {
+			this._ModalOptions = new ModalOptions(this.id + '_Modal', this.header, this.pickListTableHelp, ModalSize.Small);
+			this._Subscriptions.add(
+				this._DataSvc.dataChanged$.subscribe((results: INameDataPair) => {
+					if (this.name.trim().toLowerCase() + '_availableitems' === results.name.trim().toLowerCase()) {
+						// update the local data
+						this._AvailableItemsData = results.value;
+						this._AvailableItemsSubject.next(this._AvailableItemsData);
+					}
+				})
+			);
+		} else {
+			const mLogDestinations: Array<LogDestination> = [];
+			mLogDestinations.push(LogDestination.Console);
+			mLogDestinations.push(LogDestination.Toast);
+			const mLogOptions: ILogOptions = new LogOptions(
+				'PickListComponent.ngOnInit: id is blank',
+				LogLevel.Error,
+				mLogDestinations,
+				'PickListComponent',
+				'PickListComponent',
+				'ngOnInit',
+				'system',
+				'PickListComponent'
+			);
+			this._LoggingSvc.log(mLogOptions);
+		}
+	}
 
-  onShowHelp(): void {
-  	this._ModalSvc.open(this._ModalOptions);
-  }
+	onShowHelp(): void {
+		this._ModalSvc.open(this._ModalOptions);
+	}
 }
