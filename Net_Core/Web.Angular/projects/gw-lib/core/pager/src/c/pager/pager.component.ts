8f05c3339e86a31b9ca54aef13c5a21185f02bc5
kahiko <michael.regan@verizon.net>
2024-09-13 09:42:03 -0400

    Formatted

--- Net_Core/Web.Angular/projects/gw-lib/core/pager/src/c/pager/pager.component.ts
+++ Net_Core/Web.Angular/projects/gw-lib/core/pager/src/c/pager/pager.component.ts
@@ -17,110 +17,111 @@ import { SearchService, SearchCriteria, ISearchResultsNVP } from '@growthware/co
 })
 export class PagerComponent implements OnDestroy, OnInit {
 	private _DataChangedSub: Subscription = new Subscription();
-	private _SearchCriteria: SearchCriteria = new SearchCriteria([''],[''],1,'',1);
+	private _SearchCriteria: SearchCriteria = new SearchCriteria([''], [''], 1, '', 1);
 
-  @Input() name: string = '';
+	@Input() name: string = '';
 
-  public pages: number[] = [];
-  public selectedPage: string = '1';
-  public totalPages: number = 0;
 
-  constructor(
-    private _GWCommon: GWCommon,
-    private _SearchSvc: SearchService
-  ) {}
+	public pages: number[] = [];
+	public selectedPage: string = '1';
+	public totalPages: number = 0;
 
-  ngOnDestroy(): void {
-  	this._DataChangedSub.unsubscribe();
-  }
+	constructor(
+		private _GWCommon: GWCommon,
+		private _SearchSvc: SearchService
+	) { }
 
-  ngOnInit(): void {
-  	this._DataChangedSub = this._SearchSvc.searchDataChanged$.subscribe(
-  		(results: ISearchResultsNVP) => {
-  			if (
-  				this.name.trim().toLowerCase() === results.name.trim().toLowerCase()
-  			) {
-  				this._SearchCriteria = results.payLoad.searchCriteria;
-  				if(results.payLoad.data) {
-  					const mFirstRow = results.payLoad.data[0];
-  					if (!this._GWCommon.isNullOrUndefined(mFirstRow)) {
-  						const mTotalRecords: number = parseInt(mFirstRow['TotalRecords']);
-  						const mPageSize: number = results.payLoad.searchCriteria.pageSize;
-  						if (mTotalRecords > mPageSize) {
-  							let mCalculatedPages: number = Math.floor(mTotalRecords / mPageSize);
-  							const mRemainder = mTotalRecords % mPageSize;
-  							if(mRemainder != 0) {
-  								mCalculatedPages += 1;
-  							}
-  							if (this.totalPages !== mCalculatedPages) {
-  								this.pages.splice(0, this.pages.length);
-  								this.selectedPage = '1';
-  								for (let index = 1; index < mCalculatedPages + 1; index++) {
-  									this.pages.push(index);
-  								}
-  								this.totalPages = mCalculatedPages;
-  							}
-  						}
-  					}
-  				} else {
-  					this.totalPages = 0;
-  				}
-  			}
-  		}
-  	);
-  }
+	ngOnDestroy(): void {
+		this._DataChangedSub.unsubscribe();
+	}
 
-  /**
-   * Handles when the selectPage_{{name}} click event
-   *
-   * @memberof GWLibPagerComponent
-   */
-  onGoToPageClick(): void {
-  	this._SearchCriteria.selectedPage = parseInt(this.selectedPage);
-  	this._SearchSvc.setSearchCriteria(this.name, this._SearchCriteria);
-  }
+	ngOnInit(): void {
+		this._DataChangedSub = this._SearchSvc.searchDataChanged$.subscribe(
+			(results: ISearchResultsNVP) => {
+				if (
+					this.name.trim().toLowerCase() === results.name.trim().toLowerCase()
+				) {
+					this._SearchCriteria = results.payLoad.searchCriteria;
+					if (results.payLoad.data) {
+						const mFirstRow = results.payLoad.data[0];
+						if (!this._GWCommon.isNullOrUndefined(mFirstRow)) {
+							const mTotalRecords: number = parseInt(mFirstRow['TotalRecords']);
+							const mPageSize: number = results.payLoad.searchCriteria.pageSize;
+							if (mTotalRecords > mPageSize) {
+								let mCalculatedPages: number = Math.floor(mTotalRecords / mPageSize);
+								const mRemainder = mTotalRecords % mPageSize;
+								if (mRemainder != 0) {
+									mCalculatedPages += 1;
+								}
+								if (this.totalPages !== mCalculatedPages) {
+									this.pages.splice(0, this.pages.length);
+									this.selectedPage = '1';
+									for (let index = 1; index < mCalculatedPages + 1; index++) {
+										this.pages.push(index);
+									}
+									this.totalPages = mCalculatedPages;
+								}
+							}
+						}
+					} else {
+						this.totalPages = 0;
+					}
+				}
+			}
+		);
+	}
 
-  /**
-   * Handles when there is a page change event
-   *
-   * @param {string} direction Valid values, First, Last, Next, Previous, or # (as a string so '1')
-   * @memberof DynamicTableComponent
-   */
-  onPageChange(direction: string): void {
-  	const value = direction.trim().toLowerCase();
-  	switch (value) {
-  	case 'first':
-  		if (this._SearchCriteria.selectedPage > 1) {
-  			this._SearchCriteria.selectedPage = 1;
-  			this._SearchSvc.setSearchCriteria(this.name, this._SearchCriteria);
-  		}
-  		break;
-  	case 'last':
-  		if (this._SearchCriteria.selectedPage < this.totalPages) {
-  			this._SearchCriteria.selectedPage = this.totalPages;
-  			this._SearchSvc.setSearchCriteria(this.name, this._SearchCriteria);
-  		}
-  		break;
-  	case 'next':
-  		if (this._SearchCriteria.selectedPage < this.totalPages) {
-  			this._SearchCriteria.selectedPage++;
-  			this._SearchSvc.setSearchCriteria(this.name, this._SearchCriteria);
-  		}
-  		break;
-  	case 'previous':
-  		if (this._SearchCriteria.selectedPage > 1) {
-  			this._SearchCriteria.selectedPage--;
-  			this._SearchSvc.setSearchCriteria(this.name, this._SearchCriteria);
-  		}
-  		break;
-  	default:
-  		if (Number(value)) {
-  			console.log(value);
-  		} else {
-  			throw '"' + value + '" is not supported';
-  		}
-  		break;
-  	}
-  	this.selectedPage = this._SearchCriteria.selectedPage.toString();
-  }
+	/**
+	 * Handles when the selectPage_{{name}} click event
+	 *
+	 * @memberof GWLibPagerComponent
+	 */
+	onGoToPageClick(): void {
+		this._SearchCriteria.selectedPage = parseInt(this.selectedPage);
+		this._SearchSvc.setSearchCriteria(this.name, this._SearchCriteria);
+	}
+
+	/**
+	 * Handles when there is a page change event
+	 *
+	 * @param {string} direction Valid values, First, Last, Next, Previous, or # (as a string so '1')
+	 * @memberof DynamicTableComponent
+	 */
+	onPageChange(direction: string): void {
+		const value = direction.trim().toLowerCase();
+		switch (value) {
+			case 'first':
+				if (this._SearchCriteria.selectedPage > 1) {
+					this._SearchCriteria.selectedPage = 1;
+					this._SearchSvc.setSearchCriteria(this.name, this._SearchCriteria);
+				}
+				break;
+			case 'last':
+				if (this._SearchCriteria.selectedPage < this.totalPages) {
+					this._SearchCriteria.selectedPage = this.totalPages;
+					this._SearchSvc.setSearchCriteria(this.name, this._SearchCriteria);
+				}
+				break;
+			case 'next':
+				if (this._SearchCriteria.selectedPage < this.totalPages) {
+					this._SearchCriteria.selectedPage++;
+					this._SearchSvc.setSearchCriteria(this.name, this._SearchCriteria);
+				}
+				break;
+			case 'previous':
+				if (this._SearchCriteria.selectedPage > 1) {
+					this._SearchCriteria.selectedPage--;
+					this._SearchSvc.setSearchCriteria(this.name, this._SearchCriteria);
+				}
+				break;
+			default:
+				if (Number(value)) {
+					console.log(value);
+				} else {
+					throw '"' + value + '" is not supported';
+				}
+				break;
+		}
+		this.selectedPage = this._SearchCriteria.selectedPage.toString();
+	}
 }
