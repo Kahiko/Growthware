02e3b0314d461b9b867b6bc48134b42ae49701b8
kahiko <michael.regan@verizon.net>
2024-09-17 13:23:01 -0400

    Converted applicationName$ asObservable to signal<string>

--- Net_Core/Web.Angular/projects/gw-frontend/src/app/features/home/c/generic-home/generic-home.component.ts
+++ Net_Core/Web.Angular/projects/gw-frontend/src/app/features/home/c/generic-home/generic-home.component.ts
@@ -1,34 +1,19 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
-import { Subscription } from 'rxjs';
+import { Component, effect } from '@angular/core';
 // Library
-// import { AccountService } from '@growthware/core/account';
 import { ConfigurationService } from '@growthware/core/configuration';
-// import { MenuType } from '@growthware/core/navigation';
 
 @Component({
 	selector: 'gw-frontend-generic-home',
 	templateUrl: './generic-home.component.html',
 	styleUrls: ['./generic-home.component.scss']
 })
-export class GenericHomeComponent implements OnDestroy, OnInit {
-	private _Subscription: Subscription = new Subscription();
-
+export class GenericHomeComponent {
 	applicationName: string = '';
 
 	constructor(
     // private _AccountSvc: AccountService,
     private _ConfigurationSvc: ConfigurationService
-	) { }
-
-	ngOnDestroy(): void {
-		this._Subscription.unsubscribe();
+	) { 
+		effect(() => this.applicationName = this._ConfigurationSvc.applicationName());
 	}
-
-	ngOnInit(): void {
-		// this._AccountSvc.getNavLinks(MenuType.Hierarchical);
-		this._Subscription.add(
-			this._ConfigurationSvc.applicationName$.subscribe((val: string) => { this.applicationName = val;})
-		);
-	}
-
 }
