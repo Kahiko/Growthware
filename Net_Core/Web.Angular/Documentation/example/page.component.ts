31eaa2d08d183ff045c5de6e39884b3f376ca974
kahiko <michael.regan@verizon.net>
2024-10-13 14:07:52 -0400

    Made to be more generic

--- Net_Core/Web.Angular/Documentation/example/page.component.ts
+++ Net_Core/Web.Angular/Documentation/example/page.component.ts
@@ -1,12 +1,10 @@
-import { Component, inject, OnInit, TemplateRef, ViewChild } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
 // Angular Material
 import { MatIconModule } from '@angular/material/icon';
 import { MatTabsModule } from '@angular/material/tabs';
 // Library
-import { IModalOptions, ModalOptions, ModalService, WindowSize } from '@growthware/core/modal';
-import { ILogOptions, LogDestination, LoggingService, LogOptions } from '@growthware/core/logging';
 // Feature
 
 @Component({
@@ -22,39 +20,27 @@ import { ILogOptions, LogDestination, LoggingService, LogOptions } from '@growth
   templateUrl: './page.component.html',
   styleUrl: './page.component.scss'
 })
-export class TestLoggingComponent implements OnInit {
+export class PageComponent implements OnInit {
 
-  private _FormBuilder = inject(FormBuilder);
-  private _LoggingSvc = inject(LoggingService);
+	private _FormBuilder = inject(FormBuilder);
 
-  theForm: FormGroup = this._FormBuilder.group({});
+	theForm: FormGroup = this._FormBuilder.group({});
 
-  ngOnInit(): void {
-    this.createForm();
-  }
+	ngOnInit(): void {
+		this.createForm();
+	}
 
-  createForm(): void {
-    const mLogOptions: ILogOptions = new LogOptions(
-        this.controls['msg'].getRawValue(),
-        this.controls['selectedLogLevel'].getRawValue(),
-        [LogDestination.Toast],
-        this.controls['componentName'].getRawValue(),
-        this.controls['className'].getRawValue(),
-        this.controls['methodName'].getRawValue(),
-        this.controls['account'].getRawValue(),
-        this.controls['title'].getRawValue()
-      );
-    this._LoggingSvc.log(mLogOptions);
-    this.theForm = this._FormBuilder.group({
+	createForm(): void {
+		this.theForm = this._FormBuilder.group({
 
-    });
-  }
+		});
+	}
 
-  get controls() {
-    return this.theForm.controls;
-  }
+	get controls() {
+		return this.theForm.controls;
+	}
 
-  onSubmit(form: FormGroup): void {
-    console.log('TestLoggingComponent.onSubmit: form ', form);
-  }
+	onSubmit(form: FormGroup): void {
+		console.log('PageComponent.onSubmit: form ', form);
+	}
 }
