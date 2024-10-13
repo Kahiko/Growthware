4ab357c384a99d5fa94956db8c0dc7d031dba075
kahiko <michael.regan@verizon.net>
2024-10-13 14:12:10 -0400

    Now matches other "pages"

--- Net_Core/Web.Angular/projects/gw-lib/core/workflows/src/c/workflow-details/workflow-details.component.ts
+++ Net_Core/Web.Angular/projects/gw-lib/core/workflows/src/c/workflow-details/workflow-details.component.ts
@@ -1,13 +1,44 @@
-import { Component } from '@angular/core';
-
+import { Component, inject, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
+// Angular Material
+import { MatIconModule } from '@angular/material/icon';
+import { MatTabsModule } from '@angular/material/tabs';
 
 @Component({
 	selector: 'gw-core-workflow-details',
 	standalone: true,
-	imports: [],
+	imports: [
+		CommonModule,
+		ReactiveFormsModule,
+		// Angular Material
+		MatIconModule,
+		MatTabsModule,
+	],
 	templateUrl: './workflow-details.component.html',
 	styleUrls: ['./workflow-details.component.scss']
 })
-export class WorkflowDetailsComponent {
+export class WorkflowDetailsComponent implements OnInit {
 
+	private _FormBuilder = inject(FormBuilder);
+
+	theForm: FormGroup = this._FormBuilder.group({});
+
+	ngOnInit(): void {
+		this.createForm();
+	}
+
+	createForm(): void {
+		this.theForm = this._FormBuilder.group({
+
+		});
+	}
+
+	get controls() {
+		return this.theForm.controls;
+	}
+
+	onSubmit(form: FormGroup): void {
+		console.log('TestLoggingComponent.onSubmit: form ', form);
+	}
 }
