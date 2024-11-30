7ab948daae89b10f2936cd623682db005c98a830
kahiko <michael.regan@verizon.net>
2024-11-30 07:34:53 -0500

    removed comment

--- Net_Core/Web.Angular/projects/gw-lib/core/file-manager/src/file-manager.routes.ts
+++ Net_Core/Web.Angular/projects/gw-lib/core/file-manager/src/file-manager.routes.ts
@@ -1,5 +1,4 @@
 import { Routes } from '@angular/router';
-// Feature
 
 export const fileManagerRoutes: Routes = [
     { path: '', loadComponent: () => import('./c/file-manager/file-manager.component').then(m => m.FileManagerComponent) },
