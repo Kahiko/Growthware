ebe832196d615cad8e652e6d8c5136b33d2c4ee0
kahiko <michael.regan@verizon.net>
2024-12-02 09:48:44 -0500

    Fixed nagural sort not displaying

--- Net_Core/Web.Angular/projects/gw-frontend/src/app/app.routes.ts
+++ Net_Core/Web.Angular/projects/gw-frontend/src/app/app.routes.ts
@@ -1,46 +1,24 @@
 import { Routes } from '@angular/router';
-// Application
+// Library
 import { AuthGuard } from '@growthware/common/services';
 
 export const routes: Routes = [
 	{ path: '', loadChildren: () => import('./features/home/home.module').then(m => m.HomeModule) },
 	{ path: 'naturalsort', loadChildren: () => import('@growthware/core/sys-admin').then(m => m.sysAdminRoutes), canActivate: [AuthGuard] },
-	// { path: 'selectasecurityentity', loadComponent: () => import('@growthware/core/security-entities').then(m => m.SelectSecurityEntityComponent), canActivate: [AuthGuard] },
-
 	{ path: 'securityentity', loadChildren: () => import('@growthware/core/security-entities').then(m => m.securityEntityRoutes), canActivate: [AuthGuard] },
-
-	// update not needed so commented it out will remove later
-	// { path: 'update', loadComponent: () => import('@growthware/core/sys-admin').then(m => m.UpdateSessionComponent), canActivate: [AuthGuard] },
-
-	// { path: 'accounts', loadChildren: () => import('./features/accounts/accounts.module').then(m => m.AccountsModule)  },
 	{ path: 'accounts', loadChildren: () => import('@growthware/core/account').then(m => m.accountRoutes)  },
-	// { path: 'manage_cache_dependency', loadChildren: () => import('./features/file-manager/file-manager.module').then(m => m.FileManagerModule) },
-	// { path: 'manage_logs', loadChildren: () => import('./features/file-manager/file-manager.module').then(m => m.FileManagerModule) },
 	{ path: 'manage_cache_dependency', loadChildren: () => import('@growthware/core/file-manager').then(m => m.fileManagerRoutes) },
 	{ path: 'manage_logs', loadChildren: () => import('@growthware/core/file-manager').then(m => m.fileManagerRoutes) },
 	{ path: 'feedbacks', loadChildren: () => import('@growthware/core/feedback').then(m => m.feedbackRoutes), canActivate: [AuthGuard]  },
-	// { path: 'functions', loadChildren: () => import('./features/functions/functions.module').then(m => m.FunctionsModule), canActivate: [AuthGuard]  },  
 	{ path: 'functions', loadChildren: () => import('@growthware/core/function').then(m => m.functionRoutes), canActivate: [AuthGuard]  },  
-	// { path: 'search_name_value_pairs', loadChildren: () => import('./features/name-value-pairs/name-value-pairs.module').then(m => m.NameValuePairsModule), canActivate: [AuthGuard]  },  
 	{ path: 'search_name_value_pairs', loadChildren: () => import('@growthware/core/name-value-pair').then(m => m.nameValuePairRoutes), canActivate: [AuthGuard]  },  
-	// { path: 'search_states', loadChildren: () => import('./features/states/states-routing.module').then(m => m.StatesRoutingModule), canActivate: [AuthGuard]  },  
 	{ path: 'search_states', loadChildren: () => import('@growthware/core/states').then(m => m.statesRoutes), canActivate: [AuthGuard]  },  
-	// { path: 'security', loadChildren: () => import('./features/security/security.module').then(m => m.SecurityModule)  },
 	{ path: 'security', loadChildren: () => import('@growthware/core/security').then(m => m.secutiryRoutes)  },
-	// { path: 'search_security_entities', loadChildren: () => import('./features/security-entities/security-entities.module').then(m => m.SecurityEntitiesModule)  },
-	// { path: 'manage_groups', loadChildren: () => import('./features/groups/groups.module').then(m => m.GroupsModule), canActivate: [AuthGuard]  },
 	{ path: 'manage_groups', loadChildren: () => import('@growthware/core/group').then(m => m.groupRoutes), canActivate: [AuthGuard]  },
-	// { path: 'search_roles', loadChildren: () => import('./features/roles/roles.module').then(m => m.RolesModule), canActivate: [AuthGuard]  },
 	{ path: 'search_roles', loadChildren: () => import('@growthware/core/role').then(m => m.roleRoutes), canActivate: [AuthGuard]  },
-	// { path: 'search_messages', loadChildren: () => import('./features/messages/messages.module').then(m => m.MessagesModule), canActivate: [AuthGuard]  },
 	{ path: 'search_messages', loadChildren: () => import('@growthware/core/message').then(m => m.messagesRoutes), canActivate: [AuthGuard]  },
-	// { path: 'sys_admin', loadChildren: () => import('./features/sys-admin/sys-admin.module').then(m => m.SysAdminModule)  },
 	{ path: 'sys_admin', loadChildren: () => import('@growthware/core/sys-admin').then(m => m.sysAdminRoutes)  },
-	// { path: 'communitycalendar', loadComponent: () => import('@growthware/core/community-calendar').then(m => m.CalendarComponent)  },
 	{ path: 'communitycalendar', loadChildren: () => import('@growthware/core/community-calendar').then(m => m.communityCalendarRoutes)  },
-	// { path: 'addeditworkflow', loadChildren: () => import('./features/workflows/workflows.module').then(m => m.WorkflowsModule)  },
 	{ path: 'addeditworkflow', loadChildren: () => import('@growthware/core/workflows').then(m => m.workflowRoutes)  },
-	// { path: 'setloglevel', loadChildren: () => import('./features/logging/logging.module').then(m => m.LoggingModule)  },
 	{ path: 'logging', loadChildren: () => import('@growthware/core/logging').then(m => m.loggingRoutes)  },
-	// { path: 'logging/test-logging', loadComponent: () => import('@growthware/core/logging').then(m => m.TestLoggingComponent)  },
 ];
--- Net_Core/Web.Angular/projects/gw-lib/core/sys-admin/src/sys-admin.routes.ts
+++ Net_Core/Web.Angular/projects/gw-lib/core/sys-admin/src/sys-admin.routes.ts
@@ -3,8 +3,9 @@ import { Routes } from '@angular/router';
 import { AuthGuard } from '@growthware/common/services';
 
 export const sysAdminRoutes: Routes = [
-    // path '' really isn't needed because of the way the angular 2.x and > work but left it in for now
-    { path: '', loadComponent: () => import('./c/update-session/update-session.component').then(m => m.UpdateSessionComponent) , canActivate: [AuthGuard]},
+    // update-session really isn't needed because of the way the angular 2.x and > work but left it in for now
+    // { path: '', loadComponent: () => import('./c/update-session/update-session.component').then(m => m.UpdateSessionComponent) , canActivate: [AuthGuard]},
+    { path: '', loadComponent: () => import('./c/natural-sort/natural-sort.component').then(m => m.NaturalSortComponent) , canActivate: [AuthGuard]},
 	{ path: 'linecount', loadComponent: () => import('./c/line-count/line-count.component').then(m => m.LineCountComponent) , canActivate: [AuthGuard] },
 	{ path: 'editdbinformation', loadComponent: () => import('@growthware/core/configuration').then(m => m.EditDbInformationComponent)   , canActivate: [AuthGuard] },
 ];
