a79aa0a100b9a6976e4de648fa82f7b7f11f0c23
kahiko <michael.regan@verizon.net>
2024-12-02 10:56:07 -0500

    Removed unnecessary imports

--- Net_Core/Web.Angular/projects/gw-frontend/src/app/features/home/home.module.ts
+++ Net_Core/Web.Angular/projects/gw-frontend/src/app/features/home/home.module.ts
@@ -2,14 +2,9 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 // This Feature
 import { HomeRoutingModule } from './home-routing.module';
-import { HomeComponent } from './c/home/home.component';
-import { GenericHomeComponent } from './c/generic-home/generic-home.component';
 
 @NgModule({
-	declarations: [
-		GenericHomeComponent,
-		HomeComponent
-	],
+	declarations: [],
 	imports: [
 		CommonModule,
 		HomeRoutingModule
