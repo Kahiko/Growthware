e6271cfbeb5a04c51944fd9b9e1d7969d739cb52
kahiko <michael.regan@verizon.net>
2024-12-12 08:18:23 -0500

    Formatting

--- Net_Core/Framework/Models/UI/UIAppSettings.cs
+++ Net_Core/Framework/Models/UI/UIAppSettings.cs
@@ -7,6 +7,6 @@ public class UIAppSettings
     public String Environment { get; set; }
     public string Name { get; set; }
     public string Version { get; set; }
-     public string LogPriority { get; set; }
-     public string SecurityEntityTranslation{get; set;}
+    public string LogPriority { get; set; }
+    public string SecurityEntityTranslation { get; set; }
 }
