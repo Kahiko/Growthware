e4aa4da39a76ae1b10a59d2e25e964cd8ba47c4d
kahiko <michael.regan@verizon.net>
2024-12-19 08:59:43 -0500

    Changed:   MCalendar to inherit from AAddedUpdated

--- Net_Core/Framework/Models/Table/MCalendar.cs
+++ Net_Core/Framework/Models/Table/MCalendar.cs
@@ -8,29 +8,46 @@ namespace GrowthWare.Framework.Models;
 /// Properties for a calendar event.
 /// </summary>
 [Serializable(), CLSCompliant(true)]
-public class MCalendar : AbstractBaseModel
+public class MCalendar : AAddedUpdated
 {
 
-	public int CalendarSeqId {get; set;}
+    public int CalendarEventSeqId {get; set;}
+
 	public int SecurityEntitySeqId {get; set;}
+
 	public int FunctionSeqId {get; set;}
+
+	[DBPrimaryKey]
+    [DBColumnName("CalendarSeqId")]
+    public int Id {get; set;}
+
 	public string Comment {get; set;}
+
 	public bool Active {get; set;}
 
     public MCalendar()
     {
-        this.IdColumnName = "CalendarSeqId";
-        this.NameColumnName = string.Empty;
+        SetupClass();
+        // this.IdColumnName = "CalendarSeqId";
+        // this.NameColumnName = string.Empty;
     }
 
     public MCalendar(DataRow detailRow)
     {
-        this.IdColumnName = "CalendarSeqId";
-        this.NameColumnName = string.Empty;
+        SetupClass();
+        // this.IdColumnName = "CalendarSeqId";
+        // this.NameColumnName = string.Empty;
         this.Initialize(detailRow);
         this.Active = base.GetBool(detailRow, "Active");
         this.Comment = base.GetString(detailRow, "Comment");
         this.FunctionSeqId = base.GetInt(detailRow, "FunctionSeqId");
         this.SecurityEntitySeqId = base.GetInt(detailRow, "SecurityEntitySeqId");
     }
+
+    protected override void SetupClass()
+    {
+        base.m_ForeignKeyName = "NOT_USED";
+        m_TableName = "[ZGWOptional].[Calendars]";
+    }
+
 }
