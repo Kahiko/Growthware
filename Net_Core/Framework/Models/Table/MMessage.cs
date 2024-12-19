4c3c43b3b2ef3c8894b076f2a9e90e1d59dfb9d5
kahiko <michael.regan@verizon.net>
2024-12-19 08:22:12 -0500

    Changed:   MMessage to inherit from AAddedUpdated   DMessages to support ADatabaseTable

--- Net_Core/Framework/Models/Table/MMessage.cs
+++ Net_Core/Framework/Models/Table/MMessage.cs
@@ -3,7 +3,6 @@ using System;
 using System.Data;
 using System.Reflection;
 using GrowthWare.Framework.Models.Base;
-using System.Globalization;
 using System.Linq;
 
 namespace GrowthWare.Framework.Models;
@@ -11,7 +10,7 @@ namespace GrowthWare.Framework.Models;
 /// Class MMessage
 /// </summary>
 [Serializable(), CLSCompliant(true)]
-public class MMessage : AbstractBaseModel, IMessage
+public class MMessage : AAddedUpdated, IMessage
 {
     #region "Member Properties"
     private int m_SecurityEntity_Seq_Id = 1;
@@ -23,121 +22,142 @@ public class MMessage : AbstractBaseModel, IMessage
     #endregion
 
     #region "Private Methods"
-    /// <summary>
-    /// Initializes values given a DataRow
-    /// </summary>
-    /// <param name="dataRow">DataRow</param>
-    /// <remarks>Does not set ID or Name .. ColumnName should be unique to
-    /// each inheriting class.</remarks>
-    protected new void Initialize(DataRow dataRow)
-    {
-        base.NameColumnName = "Name";
-        base.IdColumnName = "MESSAGE_SEQ_ID";
-        base.Initialize(dataRow);
-        m_SecurityEntity_Seq_Id = base.GetInt(dataRow, "SecurityEntityID");
-        m_Title = base.GetString(dataRow, "TITLE");
-        m_Description = base.GetString(dataRow, "DESCRIPTION");
-        m_FormatAsHTML = base.GetBool(dataRow, "FORMAT_AS_HTML");
-        m_Body = base.GetString(dataRow, "BODY");
-    }
+        /// <summary>
+        /// Initializes values given a DataRow
+        /// </summary>
+        /// <param name="dataRow">DataRow</param>
+        /// <remarks>Does not set ID or Name .. ColumnName should be unique to
+        /// each inheriting class.</remarks>
+        protected new void Initialize(DataRow dataRow)
+        {
+            base.Initialize(dataRow);
+            this.Id = base.GetInt(dataRow, "MESSAGE_SEQ_ID");
+            this.Name = base.GetString(dataRow, "NAME");
+            m_SecurityEntity_Seq_Id = base.GetInt(dataRow, "SecurityEntityID");
+            m_Title = base.GetString(dataRow, "TITLE");
+            m_Description = base.GetString(dataRow, "DESCRIPTION");
+            m_FormatAsHTML = base.GetBool(dataRow, "FORMAT_AS_HTML");
+            m_Body = base.GetString(dataRow, "BODY");
+        }
     #endregion
 
-    #region "Public Methods"
-    /// <summary>
-    /// Will return a message profile with the default values
-    /// </summary>
-    /// <remarks></remarks>
-    public MMessage()
-    {
-    }
-
-    /// <summary>
-    /// Initializes a new instance of the <see cref="MMessage" /> class.
-    /// </summary>
-    /// <param name="profile">MMessage</param>
-    public MMessage(MMessage profile)
-    {
-        if (profile != null)
+    #region "Constructors"
+        /// <summary>
+        /// Will return a message profile with the default values
+        /// </summary>
+        /// <remarks></remarks>
+        public MMessage()
         {
-            this.AddedBy = profile.AddedBy;
-            this.AddedDate = profile.AddedDate;
-            this.Body = profile.Body;
-            this.Description = profile.Description;
-            this.FormatAsHtml = profile.FormatAsHtml;
-            this.Id = profile.Id;
-            this.Name = profile.Name;
-            this.SecurityEntitySeqId = profile.SecurityEntitySeqId;
-            this.Title = profile.Title;
-            this.UpdatedBy = profile.UpdatedBy;
-            this.UpdatedDate = profile.UpdatedDate;
+            SetupClass();
         }
-    }
 
-    /// <summary>
-    /// Initializes a new instance of the <see cref="MMessage" /> class.
-    /// </summary>
-    /// <param name="dataRow">The DataRow.</param>
-    public MMessage(DataRow dataRow)
-    {
-        this.Initialize(dataRow);
-    }
-
-    /// <summary>
-    /// Sets or gets the body property
-    /// </summary>
-    /// <value>Sets the value</value>
-    /// <returns>String</returns>
-    /// <remarks></remarks>
-    public string Body
-    {
-        get { return m_Body; }
-        set { if (value != null) m_Body = value.Trim(); }
-    }
-
-    /// <summary>
-    /// Gets or sets the description.
-    /// </summary>
-    /// <value>The description.</value>
-    public string Description
-    {
-        get { return m_Description; }
-        set
+        /// <summary>
+        /// Initializes a new instance of the <see cref="MMessage" /> class.
+        /// </summary>
+        /// <param name="profile">MMessage</param>
+        public MMessage(MMessage profile)
         {
-            if (value != null) m_Description = value.Trim();
+            SetupClass();
+            if (profile != null)
+            {
+                this.AddedBy = profile.AddedBy;
+                this.AddedDate = profile.AddedDate;
+                this.Body = profile.Body;
+                this.Description = profile.Description;
+                this.FormatAsHtml = profile.FormatAsHtml;
+                this.Id = profile.Id;
+                this.Name = profile.Name;
+                this.SecurityEntitySeqId = profile.SecurityEntitySeqId;
+                this.Title = profile.Title;
+                this.UpdatedBy = profile.UpdatedBy;
+                this.UpdatedDate = profile.UpdatedDate;
+            }
         }
-    }
 
-    /// <summary>
-    /// Sets or gets the Title property
-    /// </summary>
-    /// <value>Sets the value</value>
-    /// <returns>String</returns>
-    /// <remarks></remarks>
-    public string Title
-    {
-        get { return m_Title; }
-        set { if (value != null) m_Title = value.Trim(); }
-    }
-    /// <summary>
-    /// Gets or sets the S e_ SE q_ ID.
-    /// </summary>
-    /// <value>The Security Entity ID.</value>
-    public int SecurityEntitySeqId
-    {
-        get { return m_SecurityEntity_Seq_Id; }
-        set { m_SecurityEntity_Seq_Id = value; }
-    }
+        /// <summary>
+        /// Initializes a new instance of the <see cref="MMessage" /> class.
+        /// </summary>
+        /// <param name="dataRow">The DataRow.</param>
+        public MMessage(DataRow dataRow)
+        {
+            SetupClass();
+            this.Initialize(dataRow);
+        }
+    #endregion
 
-    /// <summary>
-    /// Sets or gets the FormatAsHtml property
-    /// </summary>
-    /// <value>Sets the value</value>
-    /// <returns>String</returns>
-    /// <remarks></remarks>
-    public bool FormatAsHtml
+    #region "Public Properties"
+        /// <summary>
+        /// Sets or gets the body property
+        /// </summary>
+        /// <value>Sets the value</value>
+        /// <returns>String</returns>
+        /// <remarks></remarks>
+        public string Body
+        {
+            get { return m_Body; }
+            set { if (value != null) m_Body = value.Trim(); }
+        }
+
+        /// <summary>
+        /// Gets or sets the description.
+        /// </summary>
+        /// <value>The description.</value>
+        public string Description
+        {
+            get { return m_Description; }
+            set
+            {
+                if (value != null) m_Description = value.Trim();
+            }
+        }
+
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Gets or sets the S e_ SE q_ ID.
+        /// </summary>
+        /// <value>The Security Entity ID.</value>
+        public int SecurityEntitySeqId
+        {
+            get { return m_SecurityEntity_Seq_Id; }
+            set { m_SecurityEntity_Seq_Id = value; }
+        }
+
+        /// <summary>
+        /// Sets or gets the Title property
+        /// </summary>
+        /// <value>Sets the value</value>
+        /// <returns>String</returns>
+        /// <remarks></remarks>
+        public string Title
+        {
+            get { return m_Title; }
+            set { if (value != null) m_Title = value.Trim(); }
+        }
+
+        /// <summary>
+        /// Sets or gets the FormatAsHtml property
+        /// </summary>
+        /// <value>Sets the value</value>
+        /// <returns>String</returns>
+        /// <remarks></remarks>
+        [DBColumnName("Format_As_HTML")]
+        public bool FormatAsHtml
+        {
+            get { return m_FormatAsHTML; }
+            set { m_FormatAsHTML = value; }
+        }
+
+        [DBPrimaryKey]
+        [DBColumnName("MessageSeqId")]
+        public int Id { get; set; }
+
+
+    #endregion
+
+    protected override void SetupClass() 
     {
-        get { return m_FormatAsHTML; }
-        set { m_FormatAsHTML = value; }
+        m_TableName = "[ZGWCoreWeb].[Messages]";
     }
 
     /// <summary>
@@ -180,4 +200,3 @@ public class MMessage : AbstractBaseModel, IMessage
         return retVal;
     }
 }
-#endregion
