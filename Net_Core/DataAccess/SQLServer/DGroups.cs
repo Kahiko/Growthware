f730f6ef5de240a38dbdb732d756b874affd9186
kahiko <michael.regan@verizon.net>
2024-12-21 13:35:16 -0500

    Formatting

--- Net_Core/DataAccess/SQLServer/DGroups.cs
+++ Net_Core/DataAccess/SQLServer/DGroups.cs
@@ -35,7 +35,10 @@ namespace GrowthWare.DataAccess.SQLServer
         /// <returns>bool</returns>
         public bool DeleteGroup()
         {
-            SqlParameter[] mParameters = { new SqlParameter("@P_SecurityEntitySeqId", Profile.SecurityEntityID), new SqlParameter("@P_GroupSeqId", Profile.Id) };
+            SqlParameter[] mParameters = [
+                new SqlParameter("@P_SecurityEntitySeqId", Profile.SecurityEntityID), 
+                new SqlParameter("@P_GroupSeqId", Profile.Id) 
+            ];
             String mStoreProc = "ZGWSecurity.Delete_Group";
             base.ExecuteNonQuery( mStoreProc, mParameters);
             return true;
@@ -57,7 +60,10 @@ namespace GrowthWare.DataAccess.SQLServer
         /// <returns>DataTable</returns>
         public DataTable GroupsBySecurityEntity()
         {
-            SqlParameter[] mParameters = { new SqlParameter("@P_SecurityEntitySeqId", Profile.SecurityEntityID), new SqlParameter("@P_GroupSeqId", -1) };
+            SqlParameter[] mParameters = [ 
+                new SqlParameter("@P_SecurityEntitySeqId", Profile.SecurityEntityID), 
+                new SqlParameter("@P_GroupSeqId", -1) 
+            ];
             String mStoreProc = "ZGWSecurity.Get_Group";
             return base.GetDataTable( mStoreProc, mParameters);
         }
@@ -68,7 +74,10 @@ namespace GrowthWare.DataAccess.SQLServer
         /// <returns>DataRow</returns>
         public DataRow ProfileData()
         {
-            SqlParameter[] mParameters = { new SqlParameter("@P_SecurityEntitySeqId", Profile.SecurityEntityID), new SqlParameter("@P_GroupSeqId", Profile.Id) };
+            SqlParameter[] mParameters = [
+                new SqlParameter("@P_SecurityEntitySeqId", Profile.SecurityEntityID), 
+                new SqlParameter("@P_GroupSeqId", Profile.Id) 
+            ];
             String mStoreProc = "ZGWSecurity.Get_Group";
             return base.GetDataRow( mStoreProc, mParameters);
         }
@@ -98,7 +107,11 @@ namespace GrowthWare.DataAccess.SQLServer
                 throw new ArgumentException("The GroupRoles Profile must be set.");
             }
             string mymStoreProcedure = "ZGWSecurity.Get_Group_Roles";
-            SqlParameter[] mParameters = { new SqlParameter("@P_SecurityEntitySeqId", GroupRolesProfile.SecurityEntityID), new SqlParameter("@P_GroupSeqId", GroupRolesProfile.GroupSeqId) };
+            SqlParameter[] mParameters = [ 
+                new SqlParameter("@P_SecurityEntitySeqId", 
+                GroupRolesProfile.SecurityEntityID), 
+                new SqlParameter("@P_GroupSeqId", GroupRolesProfile.GroupSeqId) 
+            ];
             return base.GetDataTable(mymStoreProcedure, mParameters);
         }
 
@@ -114,7 +127,14 @@ namespace GrowthWare.DataAccess.SQLServer
                 throw new ArgumentException("The GroupRoles Profile must be set.");
             }
             string mymStoreProcedure = "ZGWSecurity.Set_Group_Roles";
-            SqlParameter[] mParameters = { new SqlParameter("@P_GroupSeqId", GroupRolesProfile.GroupSeqId), new SqlParameter("@P_SecurityEntitySeqId", GroupRolesProfile.SecurityEntityID), new SqlParameter("@P_Roles", GroupRolesProfile.Roles), new SqlParameter("@P_Added_Updated_By", GetAddedUpdatedBy(GroupRolesProfile)) };
+            SqlParameter[] mParameters = [
+                new SqlParameter("@P_GroupSeqId", 
+                GroupRolesProfile.GroupSeqId), 
+                new SqlParameter("@P_SecurityEntitySeqId", 
+                GroupRolesProfile.SecurityEntityID), new SqlParameter("@P_Roles", 
+                GroupRolesProfile.Roles), new SqlParameter("@P_Added_Updated_By", 
+                GetAddedUpdatedBy(GroupRolesProfile)) 
+            ];
             base.ExecuteNonQuery(mymStoreProcedure, mParameters);
             return true;
         }
