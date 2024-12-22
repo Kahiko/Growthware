38f963a87689f0b7c6726225df9d13a98712604e
kahiko <michael.regan@verizon.net>
2024-12-22 08:17:15 -0500

    Formatting

--- Net_Core/Framework/Models/Table/MNameValuePair.cs
+++ Net_Core/Framework/Models/Table/MNameValuePair.cs
@@ -2,64 +2,23 @@
 using System.Data;
 using GrowthWare.Framework.Models.Base;
 
-namespace GrowthWare.Framework.Models
+namespace GrowthWare.Framework.Models;
+
+/// <summary>
+/// Properties for an account.
+/// </summary>
+[Serializable(), CLSCompliant(true)]
+public class MNameValuePair : AbstractBaseModel
 {
-    /// <summary>
-    /// Properties for an account.
-    /// </summary>
-    [Serializable(), CLSCompliant(true)]
-    public class MNameValuePair : AbstractBaseModel
-    {
-#region "Member Properties"
+    #region #region Member Fields
         private string m_SchemaName = "dbo";
         private string m_StaticName = "NEW";
         private string m_Display = string.Empty;
         private string m_Description = string.Empty;
         private int m_Status = 1;
-#endregion
+    #endregion
 
-#region "Protected Methods"
-        /// <summary>
-        /// Initializes the specified DataRow.
-        /// </summary>
-        /// <param name="dataRow">The DataRow.</param>
-        protected new void Initialize(DataRow dataRow)
-        {
-            base.NameColumnName = "STATIC_NAME";
-            base.IdColumnName = "NVP_SEQ_ID";
-            base.Initialize(dataRow);
-            m_SchemaName = base.GetString(dataRow, "SCHEMA_NAME");
-            m_StaticName = base.Name;
-            m_Display = base.GetString(dataRow, "DISPLAY");
-            m_Description = base.GetString(dataRow, "DESCRIPTION");
-            m_Status = base.GetInt(dataRow, "STATUS_SEQ_ID");
-        }
-#endregion
-
-#region "Public Methods"
-        /// <summary>
-        /// Provides a new account profile with the default vaules
-        /// </summary>
-        /// <remarks></remarks>
-
-        public MNameValuePair()
-        {
-        }
-
-        /// <summary>
-        /// Will populate values based on the contents of the data row.
-        /// </summary>
-        /// <param name="dataRow">DataRow containing base values</param>
-        /// <remarks>
-        /// Class should be inherited to extend to your project specific properties
-        /// </remarks>
-        public MNameValuePair(DataRow dataRow)
-        {
-            this.Initialize(dataRow);
-        }
-#endregion
-
-#region "Public Properties"
+    #region Public Properties
         /// <summary>
         /// Gets or sets the status.
         /// </summary>
@@ -109,8 +68,45 @@ namespace GrowthWare.Framework.Models
         public string Description
         {
             get { return m_Description; }
-            set { if(!String.IsNullOrEmpty(value)) m_Description = value.Trim(); }
+            set { if (!String.IsNullOrEmpty(value)) m_Description = value.Trim(); }
         }
-#endregion
+    #endregion
+
+    #region Constructors
+        /// <summary>
+        /// Provides a new Name Value Pair profile with the default vaules
+        /// </summary>
+        /// <remarks></remarks>
+        public MNameValuePair()
+        {
+        }
+
+        /// <summary>
+        /// Will populate values based on the contents of the data row.
+        /// </summary>
+        /// <param name="dataRow">DataRow containing base values</param>
+        /// <remarks>
+        /// Class should be inherited to extend to your project specific properties
+        /// </remarks>
+        public MNameValuePair(DataRow dataRow)
+        {
+            this.Initialize(dataRow);
+        }
+    #endregion
+
+    /// <summary>
+    /// Initializes the class with the specified DataRow.
+    /// </summary>
+    /// <param name="dataRow">The DataRow.</param>
+    protected new void Initialize(DataRow dataRow)
+    {
+        base.NameColumnName = "STATIC_NAME";
+        base.IdColumnName = "NVP_SEQ_ID";
+        base.Initialize(dataRow);
+        m_SchemaName = base.GetString(dataRow, "SCHEMA_NAME");
+        m_StaticName = base.Name;
+        m_Display = base.GetString(dataRow, "DISPLAY");
+        m_Description = base.GetString(dataRow, "DESCRIPTION");
+        m_Status = base.GetInt(dataRow, "STATUS_SEQ_ID");
     }
 }
