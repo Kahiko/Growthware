32ae0d4f3ceea2fac1aeef290279e0ba7712c391
kahiko <michael.regan@verizon.net>
2024-12-25 05:04:26 -0500

    Formatting

--- Net_Core/Framework/Models/UI/UIFeedback.cs
+++ Net_Core/Framework/Models/UI/UIFeedback.cs
@@ -10,43 +10,7 @@ namespace GrowthWare.Framework.Models;
 [Serializable(), CLSCompliant(true)]
 public class UIFeedback : AbstractDatabaseFunctions 
 {
-    public UIFeedback() { }
 
-    public UIFeedback(DataRow dataRow) 
-    { 
-        this.Initialize(dataRow);
-    }
-
-    #region "Protected Methods"
-        /// <summary>
-        /// Populates direct properties as well as passing the DataRow to the abstract class
-        /// for the population of the base properties.
-        /// </summary>
-        /// <param name="dataRow">DataRow</param>
-        protected void Initialize(DataRow dataRow)
-        {
-            this.FeedbackId = base.GetInt(dataRow, "FeedbackId");
-            this.Action = base.GetString(dataRow, "Action");
-            this.Assignee = base.GetString(dataRow, "Assignee");
-            this.AssigneeId = base.GetInt(dataRow, "AssigneeId");
-            this.DateClosed = base.GetDateTime(dataRow, "Date_Closed", DateTime.Now);
-            this.DateOpened = base.GetDateTime(dataRow, "Date_Opened", DateTime.Now);
-            this.Details = base.GetString(dataRow, "Details");
-            this.FoundInVersion = base.GetString(dataRow, "Found_In_Version");
-            this.FunctionSeqId = base.GetInt(dataRow, "FunctionSeqId");
-            this.Notes = base.GetString(dataRow, "Notes");
-            this.Severity = base.GetString(dataRow, "Severity");
-            this.Status = base.GetString(dataRow, "Status");
-            this.SubmittedBy = base.GetString(dataRow, "SubmittedBy");
-            this.SubmittedById = base.GetInt(dataRow, "SubmittedById");
-            this.TargetVersion = base.GetString(dataRow, "TargetVersion");
-            this.Type = base.GetString(dataRow, "Type");
-            this.UpdatedBy = base.GetString(dataRow, "UpdatedBy");
-            this.UpdatedById = base.GetInt(dataRow, "UpdatedById");
-            this.VerifiedBy = base.GetString(dataRow, "VerifiedBy");
-            this.VerifiedById = base.GetInt(dataRow, "VerifiedById");
-        }    
-    #endregion
     #region Public Properties
         public int FeedbackId { get; set; }
         public string Action { get; set; }
@@ -72,4 +36,42 @@ public class UIFeedback : AbstractDatabaseFunctions
         public string VerifiedBy { get; set; }
         public int VerifiedById { get; set; }
     #endregion
+
+    #region Constructors
+        public UIFeedback() { }
+
+        public UIFeedback(DataRow dataRow) 
+        { 
+            this.Initialize(dataRow);
+        }
+    #endregion
+
+    /// <summary>
+    /// Populates direct properties as well as passing the DataRow to the abstract class
+    /// for the population of the base properties.
+    /// </summary>
+    /// <param name="dataRow">DataRow</param>
+    protected void Initialize(DataRow dataRow)
+    {
+        this.FeedbackId = base.GetInt(dataRow, "FeedbackId");
+        this.Action = base.GetString(dataRow, "Action");
+        this.Assignee = base.GetString(dataRow, "Assignee");
+        this.AssigneeId = base.GetInt(dataRow, "AssigneeId");
+        this.DateClosed = base.GetDateTime(dataRow, "Date_Closed", DateTime.Now);
+        this.DateOpened = base.GetDateTime(dataRow, "Date_Opened", DateTime.Now);
+        this.Details = base.GetString(dataRow, "Details");
+        this.FoundInVersion = base.GetString(dataRow, "Found_In_Version");
+        this.FunctionSeqId = base.GetInt(dataRow, "FunctionSeqId");
+        this.Notes = base.GetString(dataRow, "Notes");
+        this.Severity = base.GetString(dataRow, "Severity");
+        this.Status = base.GetString(dataRow, "Status");
+        this.SubmittedBy = base.GetString(dataRow, "SubmittedBy");
+        this.SubmittedById = base.GetInt(dataRow, "SubmittedById");
+        this.TargetVersion = base.GetString(dataRow, "TargetVersion");
+        this.Type = base.GetString(dataRow, "Type");
+        this.UpdatedBy = base.GetString(dataRow, "UpdatedBy");
+        this.UpdatedById = base.GetInt(dataRow, "UpdatedById");
+        this.VerifiedBy = base.GetString(dataRow, "VerifiedBy");
+        this.VerifiedById = base.GetInt(dataRow, "VerifiedById");
+    }
 }
