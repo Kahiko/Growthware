4f982e1613c1e395ee294d03aa94f639c5d13f99
kahiko <michael.regan@verizon.net>
2024-12-25 03:49:09 -0500

    Formatting

--- Net_Core/Framework/Models/Table/MState.cs
+++ Net_Core/Framework/Models/Table/MState.cs
@@ -2,33 +2,32 @@ using System;
 using System.Data;
 using GrowthWare.Framework.Models.Base;
 
-namespace GrowthWare.Framework.Models
+namespace GrowthWare.Framework.Models;
+
+/// <summary>
+/// Properties that represent the table [ZGWOptional].[States]
+/// </summary>
+[Serializable(), CLSCompliant(true)]
+public class MState : AbstractBaseModel
 {
-    /// <summary>
-    /// Properties that represent the table [ZGWOptional].[States]
-    /// </summary>
-    [Serializable(), CLSCompliant(true)]
-    public class MState : AbstractBaseModel
-    {
-#region "Public Methods"
-        public MState() {}
+
+    #region Public Properties
+        public string Description { get; set; }
+        public string State { get; set; }
+        public int StatusId { get; set; }
+    #endregion
+
+    #region Constructors
+        public MState() { }
 
         public MState(DataRow detailRow)
-        {
-            base.NameColumnName = "State";
-            // base.IdColumnName = "State";
-            this.Description = this.GetString(detailRow, "Description");
-            this.State = this.GetString(detailRow, "State");
-            this.StatusId = this.GetInt(detailRow, "StatusSeqId");
-            base.Initialize(detailRow);
-        }
-#endregion
-
-#region "Public Properties"
-        public string Description{get; set;}
-        public string State{get; set;}
-        public int StatusId{get; set;}
-#endregion
-
+    {
+        base.NameColumnName = "State";
+        // base.IdColumnName = "State";
+        this.Description = this.GetString(detailRow, "Description");
+        this.State = this.GetString(detailRow, "State");
+        this.StatusId = this.GetInt(detailRow, "StatusSeqId");
+        base.Initialize(detailRow);
     }
+    #endregion
 }
