7d10285fa7fe3d69545e3639e45212a9a0436a8d
kahiko <michael.regan@verizon.net>
2024-12-25 06:40:11 -0500

    Reverted to inherit from AbstractBaseModel

--- Net_Core/Framework/Models/Table/MCalendar.cs
+++ Net_Core/Framework/Models/Table/MCalendar.cs
@@ -8,7 +8,7 @@ namespace GrowthWare.Framework.Models;
 /// Properties for a calendar event.
 /// </summary>
 [Serializable(), CLSCompliant(true)]
-public class MCalendar : AAddedUpdated
+public class MCalendar : AbstractBaseModel
 {
 
     public int CalendarEventSeqId {get; set;}
@@ -17,34 +17,22 @@ public class MCalendar : AAddedUpdated
 
 	public int FunctionSeqId {get; set;}
 
-	[DBPrimaryKey]
-    [DBColumnName("CalendarSeqId")]
-    public int Id {get; set;}
-
 	public string Comment {get; set;}
 
 	public bool Active {get; set;}
 
     public MCalendar()
     {
-        SetupClass();
     }
 
     public MCalendar(DataRow detailRow)
     {
-        SetupClass();
+        this.IdColumnName = "CalendarSeqId";
+        this.NameColumnName = "";
         this.Initialize(detailRow);
         this.Active = base.GetBool(detailRow, "Active");
         this.Comment = base.GetString(detailRow, "Comment");
         this.FunctionSeqId = base.GetInt(detailRow, "FunctionSeqId");
         this.SecurityEntitySeqId = base.GetInt(detailRow, "SecurityEntitySeqId");
     }
-
-    protected override void SetupClass()
-    {
-        this.Id = -1;
-        base.m_ForeignKeyName = "NOT_USED";
-        m_TableName = "[ZGWOptional].[Calendars]";
-    }
-
 }
