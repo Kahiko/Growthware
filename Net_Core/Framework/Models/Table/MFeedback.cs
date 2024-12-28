b93a043f0d2510bc8fa0e3276a0e711426bc5985
kahiko <michael.regan@verizon.net>
2024-12-28 03:36:19 -0500

    Reverted to inherit from AbstractDatabaseFunctions

--- Net_Core/Framework/Models/Table/MFeedback.cs
+++ Net_Core/Framework/Models/Table/MFeedback.cs
@@ -8,8 +8,12 @@ namespace GrowthWare.Framework.Models;
 /// Class MFeedback is used to transport data to the Data Access Layer for storage in the data store.
 /// </summary>
 [Serializable(), CLSCompliant(true)]
-public class MFeedback : ADatabaseTable
+public class MFeedback : AbstractDatabaseFunctions
 {
+
+    #region Member Fields
+        private DateTime m_DefaultSystemDateTime = new(1753, 1, 1, 0, 0, 0);
+    #endregion
 
     #region Public Properties
         [DBPrimaryKey]
@@ -19,6 +23,7 @@ public class MFeedback : ADatabaseTable
         public DateTime DateClosed { get; set; }
         [DBColumnName("Date_Opened")]
         public DateTime DateOpened { get; set; }
+        public DateTime DefaultSystemDateTime { get{ return m_DefaultSystemDateTime; } }
         public string Details { get; set; }
         [DBColumnName("Found_In_Version")]
         public string FoundInVersion { get; set; }
@@ -72,12 +77,12 @@ public class MFeedback : ADatabaseTable
         }
     #endregion
 
-    private void Initialize(DataRow dataRow)
+    protected new void Initialize(DataRow dataRow)
     {
         DateTime mNow = DateTime.Now;
         this.FeedbackId = base.GetInt(dataRow, "FeedbackId");
         this.AssigneeId = base.GetInt(dataRow, "AssigneeId");
-        this.DateClosed = base.GetDateTime(dataRow, "Date_Closed", base.DefaultSystemDateTime);
+        this.DateClosed = base.GetDateTime(dataRow, "Date_Closed", m_DefaultSystemDateTime);
         this.DateOpened = base.GetDateTime(dataRow, "Date_Opened", mNow);
         this.Details = base.GetString(dataRow, "Details");
         this.FoundInVersion = base.GetString(dataRow, "Found_In_Version");
@@ -90,14 +95,14 @@ public class MFeedback : ADatabaseTable
         this.Type = base.GetString(dataRow, "Type");
         this.UpdatedById = base.GetInt(dataRow, "UpdatedById");
         this.VerifiedById = base.GetInt(dataRow, "VerifiedById");
-        this.StartDate = base.GetDateTime(dataRow, "Start_Date", base.DefaultSystemDateTime);
-        this.EndDate = base.GetDateTime(dataRow, "End_Date", base.DefaultSystemDateTime);
+        this.StartDate = base.GetDateTime(dataRow, "Start_Date", m_DefaultSystemDateTime);
+        this.EndDate = base.GetDateTime(dataRow, "End_Date", m_DefaultSystemDateTime);
     }
 
-    protected override void SetupClass()
+    private void SetupClass()
     {
-        base.m_ForeignKeyName = "NOT_USED";
-        base.m_IsForeignKeyNumeric = true;
-        m_TableName = "[ZGWOptional].[Feedbacks]";
+        // base.m_ForeignKeyName = "NOT_USED";
+        // base.m_IsForeignKeyNumeric = true;
+        // m_TableName = "[ZGWOptional].[Feedbacks]";
     }
 }
