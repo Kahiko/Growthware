550303f2fc6282a2349fbaedd6113363d0b379ef
kahiko <michael.regan@verizon.net>
2024-12-29 03:39:17 -0500

    Reverted to inherit from AbstractBaseModel

--- Net_Core/Framework/Models/Table/MState.cs
+++ Net_Core/Framework/Models/Table/MState.cs
@@ -8,38 +8,39 @@ namespace GrowthWare.Framework.Models;
 /// Properties that represent the table [ZGWOptional].[States]
 /// </summary>
 [Serializable(), CLSCompliant(true)]
-public class MState : AAddedUpdated
+public class MState : AbstractBaseModel
 {
 
-    #region Public Properties
-        public string Description { get; set; }
- 
-        [DBPrimaryKey]
-        public string State { get; set; }
- 
-        [DBColumnName("StatusSeqId")]
-        public int StatusId { get; set; }
-    #endregion
+#region Public Properties
+    public string Description { get; set; }
 
-    #region Constructors
-        public MState() 
-        { 
-            this.SetupClass();
-        }
+    [DBPrimaryKey]
+    public string State { get; set; }
 
-        public MState(DataRow detailRow)
-        {
-            this.SetupClass();
-            this.Description = this.GetString(detailRow, "Description");
-            this.State = this.GetString(detailRow, "State");
-            this.StatusId = this.GetInt(detailRow, "StatusSeqId");
-            base.Initialize(detailRow);
-        }
-    #endregion
+    [DBColumnName("StatusSeqId")]
+    public int StatusId { get; set; }
+#endregion
 
-    protected override void SetupClass()
-    {
-        base.m_ForeignKeyName = "NOT_USED";
-        m_TableName = "[ZGWOptional].[States]";
+#region Constructors
+    public MState() 
+    { 
+        this.SetupClass();
     }
+
+    public MState(DataRow detailRow)
+    {
+        this.SetupClass();
+        this.Description = this.GetString(detailRow, "Description");
+        this.State = this.GetString(detailRow, "State");
+        this.StatusId = this.GetInt(detailRow, "StatusSeqId");
+        base.Initialize(detailRow);
+    }
+#endregion
+
+    private void SetupClass()
+    {
+        base.NameColumnName = "State";
+        // base.IdColumnName = "State";
+    }
+
 }
