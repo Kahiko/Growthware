74a80d4580bbc16b0427ce7e1eae1ff251f94f91
kahiko <michael.regan@verizon.net>
2025-01-17 23:48:52 -0500

    Added SessionHelperTests

---
+++ Net_Core/Unit_Testing/NUnit/Test.Web.Support/MockSession .cs
@@ -0,0 +1,44 @@
+using Microsoft.AspNetCore.Http;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+
+namespace GrowthWare.Test.Web.Support;
+
+public class MockSession : ISession
+{
+    private readonly Dictionary<string, string> _storage = new Dictionary<string, string>();
+
+    public string Id => "test-session-id";
+    public bool IsAvailable => true;
+    public IEnumerable<string> Keys => _storage.Keys;
+
+    public void Clear() => _storage.Clear();
+
+    public Task CommitAsync(CancellationToken cancellationToken = default) => Task.CompletedTask;
+
+    public void Remove(string key) => _storage.Remove(key);
+
+    public void Set(string key, byte[] value) => _storage[key] = Convert.ToBase64String(value);
+
+    public void SetString(string key, string value) => _storage[key] = value;
+
+    public string GetString(string key) => _storage.TryGetValue(key, out var value) ? value : null;
+
+    public void Load() { }
+
+    public Task LoadAsync(CancellationToken cancellationToken = default) => Task.CompletedTask;
+
+    public bool TryGetValue(string key, out byte[] value)
+    {
+        if (_storage.TryGetValue(key, out var stringValue))
+        {
+            value = Convert.FromBase64String(stringValue);
+            return true;
+        }
+        value = null;
+        return false;
+    }
+
+    public void Set(string key, byte[] value, TimeSpan? expiresIn) => Set(key, value);
+    public void SetString(string key, string value, TimeSpan? expiresIn) => SetString(key, value);
+}
