f90dbbbcd2f192f271ce0d61c870e3926d88eced
kahiko <michael.regan@verizon.net>
2025-01-24 11:40:23 -0500

    Removed the Data property it's not used or needed

--- Net_Core/Framework/Models/DTO_UploadResponse.cs
+++ Net_Core/Framework/Models/DTO_UploadResponse.cs
@@ -5,7 +5,6 @@ namespace GrowthWare.Framework.Models;
 [Serializable(), CLSCompliant(true)]
 public class DTO_UploadResponse
 {
-    public dynamic Data { get; set; }
     public string ErrorMessage { get; set; }
     public string FileName { get; set; }
     public bool IsSuccess { get; set; } = true;
