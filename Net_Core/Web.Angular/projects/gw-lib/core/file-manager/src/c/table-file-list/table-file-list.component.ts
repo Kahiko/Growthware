c39387d8de6c2bacfc301f7a095fd31e2e26e8c6
kahiko <michael.regan@verizon.net>
2025-01-27 16:46:49 -0500

    Added the Check/Uncheck box and the delete checked button

--- Net_Core/Web.Angular/projects/gw-lib/core/file-manager/src/c/table-file-list/table-file-list.component.ts
+++ Net_Core/Web.Angular/projects/gw-lib/core/file-manager/src/c/table-file-list/table-file-list.component.ts
@@ -1,4 +1,4 @@
-import { AfterViewInit, Component, OnDestroy, OnInit, TemplateRef, ViewChild } from '@angular/core';
+import { AfterViewInit, Component, computed, effect, OnDestroy, OnInit, TemplateRef, ViewChild } from '@angular/core';
 import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 // Angular Material
@@ -16,6 +16,7 @@ import { ISecurityInfo, SecurityService } from '@growthware/core/security';
 // Feature
 import { IFileInfoLight } from '../../interfaces/file-info-light.model';
 import { FileManagerService } from '../../file-manager.service';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
 	selector: 'gw-core-table-file-list',
@@ -24,6 +25,7 @@ import { FileManagerService } from '../../file-manager.service';
 		FormsModule,
 		ReactiveFormsModule,
 		
+		MatButtonModule,
 		MatFormFieldModule,
 		MatInputModule,
 		MatMenuModule,
@@ -44,6 +46,7 @@ export class TableFileListComponent implements AfterViewInit, OnDestroy, OnInit 
 	dataSource = new MatTableDataSource<IFileInfoLight>([]);
 	// displayedColumns pulled from IFileInfoLight Note: order here effects the order in the table
 	displayedColumns: string[] = [
+		'selected',
 		'fullName',
 		'createdShort',
 		'modifiedShort',
@@ -53,6 +56,7 @@ export class TableFileListComponent implements AfterViewInit, OnDestroy, OnInit 
 	frmRenameFile!: FormGroup;
 	menuTopLeftPosition = { x: '0', y: '0' }; // we create an object that contains coordinates 
 	selectedFile!: IFileInfoLight;
+	selectUnselectText: string = 'Check';
 	showDelete: boolean = false;
 	showDownload: boolean = false;
 	showRename: boolean = false;
@@ -62,6 +66,7 @@ export class TableFileListComponent implements AfterViewInit, OnDestroy, OnInit 
 	// reference to the MatMenuTrigger in the DOM 
 	@ViewChild( MatMenuTrigger, {static: true}) private _MatMenuTrigger!: MatMenuTrigger;
 	@ViewChild('deleteFile', { read: TemplateRef }) private _DeleteFile!:TemplateRef<unknown>;
+	@ViewChild('deleteSelected', { read: TemplateRef }) private _DeleteSelected!: TemplateRef<unknown>;
 	@ViewChild('fileProperties', { read: TemplateRef }) private _FileProperties!:TemplateRef<unknown>;
 	@ViewChild('renameFile', { read: TemplateRef }) private _RenameFile!:TemplateRef<unknown>;
 	
@@ -73,7 +78,11 @@ export class TableFileListComponent implements AfterViewInit, OnDestroy, OnInit 
 		private _ModalSvc: ModalService,
 		private _Router: Router,
 		private _SecuritySvc: SecurityService
-	) { }
+	) { 
+		effect(() => {
+			this.dataSource.data = this._FileManagerSvc.fileInfoList$();
+		});
+	}
 
 	ngOnDestroy(): void {
 		this._Action = '';
@@ -81,6 +90,7 @@ export class TableFileListComponent implements AfterViewInit, OnDestroy, OnInit 
 
 	ngOnInit(): void {
 		this._Action = this._Router.url.split('?')[0].replace('/', '').replace('\\', '');
+		this._FileManagerSvc.setAllSelected(false);
 		this._SecuritySvc.getSecurityInfo(this._Action).then((response: ISecurityInfo) => {
 			this.showDownload = response.mayView;
 			this.showDelete = response.mayDelete;
@@ -113,6 +123,31 @@ export class TableFileListComponent implements AfterViewInit, OnDestroy, OnInit 
 		}
 		return undefined;
 	}
+
+	/**
+	 * Checks if all the files are selected, and if so, changes the text of the
+	 * select/unselect button to 'Unselect'. If not, changes the text to 'Select'.
+	 * @returns {boolean} true if all files are selected, false otherwise
+	 */
+	get allSelected(): boolean {
+		if (!this.paginator || this.paginator.pageIndex === undefined) {
+			return false; // or true, depending on your default behavior
+		}
+		const startIndex = this.paginator.pageIndex * this.paginator.pageSize;
+		const endIndex = startIndex + this.paginator.pageSize;
+		const visibleRows = this.dataSource.data.slice(startIndex, endIndex); // Get currently visible rows
+		return visibleRows.every(row => row.selected); // Check if all visible rows are selected
+	}
+
+	/**
+	 * Determines if any of the files are selected.
+	 *
+	 * @returns {boolean} true if any of the files are selected, false otherwise
+	 */
+	get anySelected(): boolean {
+		return this._FileManagerSvc.fileInfoList$().some(file => file.selected);
+	}
+
 	/**
 	 * Returns the controls of the 'frmRenameFile' form.
 	 *
@@ -146,6 +181,27 @@ export class TableFileListComponent implements AfterViewInit, OnDestroy, OnInit 
 		const sizeA = this._FileManagerSvc.convertSizeToBytes(a.size);
 		const sizeB = this._FileManagerSvc.convertSizeToBytes(b.size);
 		return sizeA - sizeB;
+	}
+
+	/**
+	 * Handles the "Delete Selected" button click event.
+	 *
+	 * @returns {void}
+	 */
+	onDeleteSelected() {
+		const mModalOptions: ModalOptions = new ModalOptions(this._ModalId_Delete, 'Delete Selected Files', this._DeleteSelected, new WindowSize(84, 300));
+		mModalOptions.buttons.okButton.visible = true;
+		mModalOptions.buttons.okButton.text = 'Yes';
+		mModalOptions.buttons.okButton.callbackMethod = () => {
+			this._FileManagerSvc.deleteFiles(this._Action).then(() => {
+				this._LoggingSvc.toast('Files were deleted', 'Delete files', LogLevel.Success);
+			}).catch((error) => {
+				this._LoggingSvc.errorHandler(error, 'FileListComponent', 'onDeleteSelected');
+				this._LoggingSvc.toast('Was not able to delete the files', 'Delete files error', LogLevel.Error);
+			});			
+			this._ModalSvc.close(this._ModalId_Delete);
+		};
+		this._ModalSvc.open(mModalOptions);
 	}
 
 	/**
@@ -218,6 +274,7 @@ export class TableFileListComponent implements AfterViewInit, OnDestroy, OnInit 
 	 */
 	onRightClick(event: MouseEvent, item: IFileInfoLight) {
 		this.selectedFile = item;
+		this._FileManagerSvc.setAllSelected(false);
 		// preventDefault avoids to show the visualization of the right-click menu of the browser
 		event.preventDefault();
 
@@ -232,6 +289,30 @@ export class TableFileListComponent implements AfterViewInit, OnDestroy, OnInit 
 		// we open the menu
 		this._MatMenuTrigger.openMenu();
 	}
+
+	/**
+	 * Handle "Select All" checkbox click event
+	 * @param {Event} event - description of parameter
+	 * @return {void} description of return value
+	 */
+	onToggleSelectAll(event: Event) {
+		const isChecked = (event.target as HTMLInputElement).checked;
+		this.selectUnselectText = isChecked ? 'Uncheck' : 'Check';
+		// We can not use the service because we are using an Angular Material component
+		// and we do not want to have any rows checked if they are not visiable
+		// Get the currently visible rows
+		const startIndex = this.paginator.pageIndex * this.paginator.pageSize;
+		const endIndex = startIndex + this.paginator.pageSize;
+		const visibleRows = this.dataSource.data.slice(startIndex, endIndex);
+		// Update the selected property of visible rows
+		visibleRows.forEach(row => row.selected = isChecked);
+		this._FileManagerSvc.fileInfoList$().forEach((fileInfo) => {
+			if (fileInfo.selected) {
+				console.log('onToggleSelectAll:', fileInfo.name);
+			}
+		});
+	}
+
 	/**
 	 * Performs the rename by calling _FileManagerSvc.renameFile.
 	 *
