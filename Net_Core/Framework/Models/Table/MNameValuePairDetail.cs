5f92560b604f62032df2c40578aa0544de466e64
kahiko <michael.regan@verizon.net>
2025-02-08 08:52:36 -0500

    Formatting

--- Net_Core/Framework/Models/Table/MNameValuePairDetail.cs
+++ Net_Core/Framework/Models/Table/MNameValuePairDetail.cs
@@ -11,107 +11,107 @@ namespace GrowthWare.Framework.Models;
 public class MNameValuePairDetail : AbstractBaseModel
 {
 
-    #region Member Fields
-        private int m_NameValuePairSeqId = -1;
-        private string m_Text = string.Empty;
-        private string m_Value = string.Empty;
-        private int m_SortOrder = 0;
-        private int m_Status = 1;
-    #endregion
+#region Member Fields
+    private int m_NameValuePairSeqId = -1;
+    private string m_Text = string.Empty;
+    private string m_Value = string.Empty;
+    private int m_SortOrder = 0;
+    private int m_Status = 1;
+#endregion
 
-    #region Public Properties
-        // // Commented out for now this should need to come back when we fix the base class
-        // [DBPrimaryKey]
-        // [DBColumnName("NVP_DetailSeqId")]
-        // public int Id { get; set; }
+#region Public Properties
+    // // Commented out for now this should need to come back when we fix the base class
+    // [DBPrimaryKey]
+    // [DBColumnName("NVP_DetailSeqId")]
+    // public int Id { get; set; }
 
-        // // Commented out for now this should need to come back when we fix the base class
-        // [DBColumnName("NVP_Detail_Name")]
-        // public string Name
-        // {
-        //     get { return m_Text; }
-        //     set { if (!String.IsNullOrEmpty(value)) m_Text = value.Trim(); }
-        // }
+    // // Commented out for now this should need to come back when we fix the base class
+    // [DBColumnName("NVP_Detail_Name")]
+    // public string Name
+    // {
+    //     get { return m_Text; }
+    //     set { if (!String.IsNullOrEmpty(value)) m_Text = value.Trim(); }
+    // }
 
-        /// <summary>
-        /// Gets or sets the Name Value Pair SeqId.
-        /// </summary>
-        /// <value>The Name Value Pair SeqId.</value>
-        [DBColumnName("NVPSeqId")]
-        public int NameValuePairSeqId
-        {
-            get { return m_NameValuePairSeqId; }
-            set { m_NameValuePairSeqId = value; }
-        }
+    /// <summary>
+    /// Gets or sets the Name Value Pair SeqId.
+    /// </summary>
+    /// <value>The Name Value Pair SeqId.</value>
+    [DBColumnName("NVPSeqId")]
+    public int NameValuePairSeqId
+    {
+        get { return m_NameValuePairSeqId; }
+        set { m_NameValuePairSeqId = value; }
+    }
 
-        /// <summary>
-        /// Gets or sets the sort order.
-        /// </summary>
-        /// <value>The sort order.</value>
-        [DBColumnName("Sort_Order")]
-        public int SortOrder
-        {
-            get { return m_SortOrder; }
-            set { m_SortOrder = value; }
-        }
+    /// <summary>
+    /// Gets or sets the sort order.
+    /// </summary>
+    /// <value>The sort order.</value>
+    [DBColumnName("Sort_Order")]
+    public int SortOrder
+    {
+        get { return m_SortOrder; }
+        set { m_SortOrder = value; }
+    }
 
-        /// <summary>
-        /// Gets or sets the status.
-        /// </summary>
-        /// <value>The status.</value>
-        [DBColumnName("StatusSeqId")]
-        public int Status
-        {
-            get { return m_Status; }
-            set { m_Status = value; }
-        }
+    /// <summary>
+    /// Gets or sets the status.
+    /// </summary>
+    /// <value>The status.</value>
+    [DBColumnName("StatusSeqId")]
+    public int Status
+    {
+        get { return m_Status; }
+        set { m_Status = value; }
+    }
 
-        /// <summary>
-        /// Gets or sets the text.
-        /// </summary>
-        /// <value>The text.</value>
-        [DBColumnName("NVP_Detail_Name")]
-        public string Text
-        {
-            get { return m_Text; }
-            set { if (!String.IsNullOrEmpty(value)) m_Text = value.Trim(); }
-        }
+    /// <summary>
+    /// Gets or sets the text.
+    /// </summary>
+    /// <value>The text.</value>
+    [DBColumnName("NVP_Detail_Name")]
+    public string Text
+    {
+        get { return m_Text; }
+        set { if (!String.IsNullOrEmpty(value)) m_Text = value.Trim(); }
+    }
 
-        /// <summary>
-        /// Gets or sets the value.
-        /// </summary>
-        /// <value>The value.</value>
-        [DBColumnName("NVP_Detail_Value")]
-        public string Value
-        {
-            get { return m_Value; }
-            set { if (!String.IsNullOrEmpty(value)) m_Value = value.Trim(); }
-        }
-    #endregion
+    /// <summary>
+    /// Gets or sets the value.
+    /// </summary>
+    /// <value>The value.</value>
+    [DBColumnName("NVP_Detail_Value")]
+    public string Value
+    {
+        get { return m_Value; }
+        set { if (!String.IsNullOrEmpty(value)) m_Value = value.Trim(); }
+    }
+#endregion
 
-    #region Constructors
-        /// <summary>
-        /// Provides a new account profile with the default vaules
-        /// </summary>
-        /// <remarks></remarks>
-        public MNameValuePairDetail()
-        {
-            this.SetupClass();
-        }
+#region Constructors
+    /// <summary>
+    /// Provides a new account profile with the default vaules
+    /// </summary>
+    /// <remarks></remarks>
+    public MNameValuePairDetail()
+    {
+        this.SetupClass();
+    }
 
-        /// <summary>
-        /// Will populate values based on the contents of the data row.
-        /// </summary>
-        /// <param name="dataRow">Datarow containing base values</param>
-        /// <remarks>
-        /// Class should be inherited to extend to your project specific properties
-        /// </remarks>
-        public MNameValuePairDetail(DataRow dataRow)
-        {
-            this.SetupClass();
-            this.Initialize(dataRow);
-        }
-    #endregion
+    /// <summary>
+    /// Will populate values based on the contents of the data row.
+    /// </summary>
+    /// <param name="dataRow">Datarow containing base values</param>
+    /// <remarks>
+    /// Class should be inherited to extend to your project specific properties
+    /// </remarks>
+    public MNameValuePairDetail(DataRow dataRow)
+    {
+        this.SetupClass();
+        this.Initialize(dataRow);
+    }
+#endregion
 
     /// <summary>
     /// Initializes the specified dr.
@@ -153,7 +153,7 @@ public class MNameValuePairDetail : AbstractBaseModel
          *    [ZGWCoreWeb].[Work_Flows]
          */
         m_TableName = $"[{schemaName}].[{tableName}]";
-        if (!useBrackets) 
+        if (!useBrackets)
         {
             m_TableName = m_TableName.Replace("[", "").Replace("]", "");
         }
