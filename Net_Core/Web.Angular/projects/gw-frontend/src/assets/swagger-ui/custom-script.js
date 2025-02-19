1ce405e422e992bdfdacddbf978cd38976eb0835
kahiko <michael.regan@verizon.net>
2025-02-19 17:09:47 -0500

    Added /assets to InjectJavascript and InjectStylesheet to fix 404 errors Added code to custom-script.js to set the favicon.

---
+++ Net_Core/Web.Angular/projects/gw-frontend/src/assets/swagger-ui/custom-script.js
@@ -0,0 +1,7 @@
+var link = document.querySelector("link[rel~='icon']");
+if (!link) {
+    link = document.createElement('link');
+    link.rel = 'icon';
+    document.head.appendChild(link);
+}
+link.href = '/icons/favicon.ico';
