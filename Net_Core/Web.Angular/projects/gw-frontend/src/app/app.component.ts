ac3a9884f8b9558640b054f74db5adf0de2b5e81
kahiko <michael.regan@verizon.net>
2025-02-20 15:38:18 -0500

    Removed unused imports

--- Net_Core/Web.Angular/projects/gw-frontend/src/app/app.component.ts
+++ Net_Core/Web.Angular/projects/gw-frontend/src/app/app.component.ts
@@ -3,7 +3,7 @@ import { Component, effect, signal } from '@angular/core';
 import { Title } from '@angular/platform-browser';
 // Library
 import { ToasterComponent } from '@growthware/core/toast';
-import { AccountService, IAccountInformation } from '@growthware/core/account';
+import { AccountService } from '@growthware/core/account';
 import { IClientChoices } from '@growthware/core/clientchoices';
 import { ConfigurationService } from '@growthware/core/configuration';
 import { ISecurityEntityProfile, SecurityEntityService } from '@growthware/core/security-entities';
