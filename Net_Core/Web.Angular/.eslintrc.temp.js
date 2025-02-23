f8b53c405ed374de9e2180b45e7b1bd4ae2ef681
kahiko <michael.regan@verizon.net>
2025-02-23 11:51:42 -0500

    added eslintrc.temp.js to help fix a single type of error at one time.

---
+++ Net_Core/Web.Angular/.eslintrc.temp.js
@@ -0,0 +1,16 @@
+module.exports = {
+	rules: {
+		'indent': ['error', 'tab'], // Enforce tabs for indentation
+		// Disable all other rules
+		'no-unused-vars': 'off',
+		'no-console': 'off',
+		'semi': 'off',
+		'no-extra-semi': 'off',
+		'prefer-const': 'off',
+		'quotes': 'off',
+		'@angular-eslint/component-class-suffix': 'off',
+		'@typescript-eslint/prefer-const': 'off',
+		'@typescript-eslint/no-unused-vars': 'off',
+		'@typescript-eslint/no-explicit-any': 'off',
+	},
+};
