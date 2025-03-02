a8e247a7261fc43e14f56d3aa777b381bad778bb
kahiko <michael.regan@verizon.net>
2025-03-02 10:25:46 -0500

    feature(navigation) Code clean up (eslint)

--- Net_Core/Web.Angular/projects/gw-lib/core/navigation/src/c/horizontal/horizontal.component.ts
+++ Net_Core/Web.Angular/projects/gw-lib/core/navigation/src/c/horizontal/horizontal.component.ts
@@ -1,13 +1,10 @@
-import { Component, ElementRef, input, OnInit, ViewChild } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { Router, RouterModule } from '@angular/router';
+import { RouterModule } from '@angular/router';
 // Angular Material
 import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
 // Library
-import { AccountService } from '@growthware/core/account';
-import { GWCommon } from '@growthware/common/services';
-import { LoggingService } from '@growthware/core/logging';
 // Feature
 import { NavigationComponentBase } from '../navigation-component-base/navigation-component-base.component';
 import { MenuTypes } from '../../menu-types.enum';
