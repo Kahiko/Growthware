c25c6525f25ce0b452e3dd8bcecd6f8ecdc29b65
kahiko <michael.regan@verizon.net>
2025-03-05 09:30:36 -0500

    feature(core\logging) Code clean up test-logging.component.ts (eslint)

--- Net_Core/Web.Angular/projects/gw-lib/core/logging/src/c/test-logging/test-logging.component.ts
+++ Net_Core/Web.Angular/projects/gw-lib/core/logging/src/c/test-logging/test-logging.component.ts
@@ -1,7 +1,6 @@
 import { Component, inject, OnInit, TemplateRef, ViewChild } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormArray, FormBuilder, FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
-import { Validators } from '@angular/forms';
+import { FormBuilder, FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 // Angular Material
 import { MatButtonModule } from '@angular/material/button';
 import { MatCheckboxModule } from '@angular/material/checkbox';
@@ -16,7 +15,6 @@ import { IModalOptions, ModalOptions, ModalService, WindowSize } from '@growthwa
 // Feature
 import { LoggingService } from '../../logging.service';
 import { ILogOptions, LogOptions } from '../../log-options.model';
-import { LogLevel } from '../../log-level.enum';
 import { LogDestination } from '../../log-destination.enum';
 
 @Component({
@@ -42,105 +40,105 @@ export class TestLoggingComponent implements OnInit {
 
 	private _FormBuilder = inject(FormBuilder);
 	private _HelpOptions: IModalOptions = new ModalOptions('help', 'Help', '', 1);
-  @ViewChild('File') private _HelpFile!: TemplateRef<unknown>;
-  @ViewChild('Console') private _HelpConsole!: TemplateRef<unknown>;
-  @ViewChild('DB') private _HelpDB!: TemplateRef<unknown>;
-  @ViewChild('Toast') private _HelpToast!: TemplateRef<unknown>;
-  private _LoggingSvc = inject(LoggingService);
-  private _ModalSvc = inject(ModalService);
+	@ViewChild('File') private _HelpFile!: TemplateRef<unknown>;
+	@ViewChild('Console') private _HelpConsole!: TemplateRef<unknown>;
+	@ViewChild('DB') private _HelpDB!: TemplateRef<unknown>;
+	@ViewChild('Toast') private _HelpToast!: TemplateRef<unknown>;
+	private _LoggingSvc = inject(LoggingService);
+	private _ModalSvc = inject(ModalService);
 
-  theForm: FormGroup = this._FormBuilder.group({});
-  validDestinations = [
-  	{ id: 0, name: 'File' },    // Used by the API
-  	{ id: 1, name: 'Console' }, // Used by the Fronend
-  	{ id: 2, name: 'DB' },      // Used by the API
-  	{ id: 3, name: 'Toast' },   // Used by the Fronend
-  ];
+	theForm: FormGroup = this._FormBuilder.group({});
+	validDestinations = [
+		{ id: 0, name: 'File' },    // Used by the API
+		{ id: 1, name: 'Console' }, // Used by the Fronend
+		{ id: 2, name: 'DB' },      // Used by the API
+		{ id: 3, name: 'Toast' },   // Used by the Fronend
+	];
 
-  validLogLevels = [
-  	{ id: 0, name: 'Debug' },
-  	{ id: 1, name: 'Error' },
-  	{ id: 2, name: 'Fatal' },
-  	{ id: 3, name: 'Info' },
-  	{ id: 4, name: 'Warn' },
-  	{ id: 5, name: 'Success' },
-  	{ id: 6, name: 'Trace' },
-  ];
+	validLogLevels = [
+		{ id: 0, name: 'Debug' },
+		{ id: 1, name: 'Error' },
+		{ id: 2, name: 'Fatal' },
+		{ id: 3, name: 'Info' },
+		{ id: 4, name: 'Warn' },
+		{ id: 5, name: 'Success' },
+		{ id: 6, name: 'Trace' },
+	];
 
-  ngOnInit(): void {
-  	this.createForm();
-  }
+	ngOnInit(): void {
+		this.createForm();
+	}
 
-  createForm(): void {
-  	this.theForm = this._FormBuilder.group({
-  		msg: ['Just a message to test with', Validators.required],
-  		selectedLogLevel: [0, Validators.required],
-  		title: ['My Title', Validators.required],
-  		componentName: ['componentName', Validators.required],
-  		className: ['className', Validators.required],
-  		methodName: ['methodName', Validators.required],
-  		account: ['account', Validators.required],
-  	});
-  	this.validDestinations.forEach((item) => {
-  		if (item.name.toLocaleLowerCase() !== 'toast') {
-  			this.theForm.addControl(item.name, new FormControl<boolean>(false));
-  		} else {
-  			this.theForm.addControl(item.name, new FormControl<boolean>(true));
-  		}
-  	});
-  	// console.log('TestLoggingComponent.createForm: theForm ', this.theForm);
-  }
+	createForm(): void {
+		this.theForm = this._FormBuilder.group({
+			msg: ['Just a message to test with', Validators.required],
+			selectedLogLevel: [0, Validators.required],
+			title: ['My Title', Validators.required],
+			componentName: ['componentName', Validators.required],
+			className: ['className', Validators.required],
+			methodName: ['methodName', Validators.required],
+			account: ['account', Validators.required],
+		});
+		this.validDestinations.forEach((item) => {
+			if (item.name.toLocaleLowerCase() !== 'toast') {
+				this.theForm.addControl(item.name, new FormControl<boolean>(false));
+			} else {
+				this.theForm.addControl(item.name, new FormControl<boolean>(true));
+			}
+		});
+		// console.log('TestLoggingComponent.createForm: theForm ', this.theForm);
+	}
 
-  get controls() {
-  	return this.theForm.controls;
-  }
+	get controls() {
+		return this.theForm.controls;
+	}
 
-  onHelp(controleName: string): void {
-  	switch (controleName) {
-  	case 'File':
-  		// this._HelpOptions.windowSize = 1;
-  		this._HelpOptions.windowSize = new WindowSize(250, 600);
-  		this._HelpOptions.contentPayLoad = this._HelpFile;
-  		break;
-  	case 'Console':
-  		this._HelpOptions.windowSize = 1;
-  		this._HelpOptions.contentPayLoad = this._HelpConsole;
-  		break;
-  	case 'DB':
-  		this._HelpOptions.windowSize = 1;
-  		this._HelpOptions.contentPayLoad = this._HelpDB;
-  		break;
-  	case 'Toast':
-  		this._HelpOptions.windowSize = new WindowSize(250, 600);
-  		this._HelpOptions.contentPayLoad = this._HelpToast;
-  		break;
-  	default:
-  		break;
-  	}
-  	this._ModalSvc.open(this._HelpOptions);
-  }
+	onHelp(controleName: string): void {
+		switch (controleName) {
+			case 'File':
+				// this._HelpOptions.windowSize = 1;
+				this._HelpOptions.windowSize = new WindowSize(250, 600);
+				this._HelpOptions.contentPayLoad = this._HelpFile;
+				break;
+			case 'Console':
+				this._HelpOptions.windowSize = 1;
+				this._HelpOptions.contentPayLoad = this._HelpConsole;
+				break;
+			case 'DB':
+				this._HelpOptions.windowSize = 1;
+				this._HelpOptions.contentPayLoad = this._HelpDB;
+				break;
+			case 'Toast':
+				this._HelpOptions.windowSize = new WindowSize(250, 600);
+				this._HelpOptions.contentPayLoad = this._HelpToast;
+				break;
+			default:
+				break;
+		}
+		this._ModalSvc.open(this._HelpOptions);
+	}
 
-  onSubmit(form: FormGroup): void {
-  	// 
-  	const mLogDestination: LogDestination[] = [];
-  	this.validDestinations.forEach((item) => {
-  		const mCheckbox = this.controls[item.name].value;
-  		if (mCheckbox && mCheckbox === true) {
-  			mLogDestination.push(item.id);
-  		}
-  	});
-  	// Create the Object to send to the API
-  	const mLogOptions: ILogOptions = new LogOptions(
-  		this.controls['msg'].getRawValue(),
-  		this.controls['selectedLogLevel'].getRawValue(),
-  		mLogDestination,
-  		this.controls['componentName'].getRawValue(),
-  		this.controls['className'].getRawValue(),
-  		this.controls['methodName'].getRawValue(),
-  		this.controls['account'].getRawValue(),
-  		this.controls['title'].getRawValue()
-  	);
-  	this._LoggingSvc.log(mLogOptions);
-  }
+	onSubmit(): void {
+		// 
+		const mLogDestination: LogDestination[] = [];
+		this.validDestinations.forEach((item) => {
+			const mCheckbox = this.controls[item.name].value;
+			if (mCheckbox && mCheckbox === true) {
+				mLogDestination.push(item.id);
+			}
+		});
+		// Create the Object to send to the API
+		const mLogOptions: ILogOptions = new LogOptions(
+			this.controls['msg'].getRawValue(),
+			this.controls['selectedLogLevel'].getRawValue(),
+			mLogDestination,
+			this.controls['componentName'].getRawValue(),
+			this.controls['className'].getRawValue(),
+			this.controls['methodName'].getRawValue(),
+			this.controls['account'].getRawValue(),
+			this.controls['title'].getRawValue()
+		);
+		this._LoggingSvc.log(mLogOptions);
+	}
 
 }
