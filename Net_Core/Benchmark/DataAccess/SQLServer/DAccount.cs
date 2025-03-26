36e3bba9a9f5a614f3b553cfacd858358dc1a6f4
kahiko <michael.regan@verizon.net>
2025-03-26 16:14:31 -0400

    feature(Benchmark) Added the Benchmark project and benchmark tests for in DAccounts for GetProfile and GetProfileAsync

---
+++ Net_Core/Benchmark/DataAccess/SQLServer/DAccount.cs
@@ -0,0 +1,123 @@
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Jobs;
+using GrowthWare.DataAccess.Interfaces.Base;
+using GrowthWare.DataAccess.SQLServer.Base;
+using GrowthWare.Framework;
+using GrowthWare.Framework.Models;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace GrowthWare.Benchmark.DataAccess.SQLServer;
+
+[SimpleJob(RuntimeMoniker.Net90)]
+[MemoryDiagnoser]
+public class DAccounts : AbstractDBInteraction, IDBInteraction
+{
+
+    #region Member Fields
+    private string m_ConnectionString = ConfigSettings.ConnectionString;
+    private MAccountProfile m_Profile = null;
+    private int m_SecurityEntitySeqID = 1;
+    #endregion
+
+    #region Constructors
+    public DAccounts() : base()
+    {
+        this.ConnectionString = ConfigSettings.ConnectionString;
+        this.m_SecurityEntitySeqID = 1;
+        this.m_Profile = new()
+        {
+            Account = "Developer"
+        };
+    }
+    #endregion
+
+    [Benchmark(Baseline = true)]
+    public MAccountProfile GetProfile()
+    {
+        string mCommandText = "ZGWSecurity.Get_Account";
+        SqlParameter[] mParameters = {
+                GetSqlParameter("@P_Is_System_Admin", m_Profile.IsSystemAdmin, ParameterDirection.Input),
+                GetSqlParameter("@P_SecurityEntitySeqId", m_SecurityEntitySeqID, ParameterDirection.Input),
+                GetSqlParameter("@P_Account", this.Cleanup(m_Profile.Account), ParameterDirection.Input)
+            };
+        DataRow mDataRow = base.GetDataRow(mCommandText, mParameters);
+
+        mCommandText = "SELECT [RefreshTokenId], RT.[AccountSeqId], [Token], [Expires], [Created], [CreatedByIp], [Revoked], [RevokedByIp], [ReplacedByToken], [ReasonRevoked] ";
+        mCommandText += "FROM [ZGWSecurity].[RefreshTokens] RT ";
+        mCommandText += "INNER JOIN [ZGWSecurity].[Accounts] ACCT ON ACCT.[Account] = @P_Account AND RT.AccountSeqId = ACCT.[AccountSeqId] ";
+        mCommandText += "ORDER BY [Created] ASC;";
+        mParameters = [
+            new("@P_Account", m_Profile.Account),
+        ];
+        DataTable mRefreshTokens = base.GetDataTable(mCommandText, mParameters, true);
+
+        mCommandText = "ZGWSecurity.Get_Account_Roles";
+        mParameters = [
+            new("@P_Account", this.Cleanup(m_Profile.Account)),
+            new("@P_SecurityEntitySeqId", m_SecurityEntitySeqID)
+        ];
+        DataTable mAssignedRoles = base.GetDataTable(mCommandText, mParameters);
+
+        mCommandText = "ZGWSecurity.Get_Account_Groups";
+        mParameters = [
+            new("@P_Account", this.Cleanup(m_Profile.Account)),
+            new("@P_SecurityEntitySeqId", m_SecurityEntitySeqID)
+        ];
+        DataTable mAssignedGroups = base.GetDataTable(mCommandText, mParameters);
+
+        mCommandText = "ZGWSecurity.Get_Account_Security";
+        mParameters = [
+            new("@P_Account", this.Cleanup(m_Profile.Account)),
+            new("@P_SecurityEntitySeqId", m_SecurityEntitySeqID)
+        ];
+        DataTable mRoles = base.GetDataTable(mCommandText, mParameters);
+
+        return new MAccountProfile(mDataRow, mRefreshTokens, mAssignedRoles, mAssignedGroups, mRoles);
+    }
+
+    [Benchmark]
+    public async Task<MAccountProfile> GetProfileAsync()
+    {
+        string mCommandText = "ZGWSecurity.Get_Account";
+        SqlParameter[] mParameters = [
+            GetSqlParameter("@P_Is_System_Admin", m_Profile.IsSystemAdmin, ParameterDirection.Input),
+            GetSqlParameter("@P_SecurityEntitySeqId", m_SecurityEntitySeqID, ParameterDirection.Input),
+            GetSqlParameter("@P_Account", this.Cleanup(m_Profile.Account), ParameterDirection.Input)
+        ];
+        DataRow mDataRow = await base.GetDataRowAsync(mCommandText, mParameters);
+
+        mCommandText = "SELECT [RefreshTokenId], RT.[AccountSeqId], [Token], [Expires], [Created], [CreatedByIp], [Revoked], [RevokedByIp], [ReplacedByToken], [ReasonRevoked] ";
+        mCommandText += "FROM [ZGWSecurity].[RefreshTokens] RT ";
+        mCommandText += "INNER JOIN [ZGWSecurity].[Accounts] ACCT ON ACCT.[Account] = @P_Account AND RT.AccountSeqId = ACCT.[AccountSeqId] ";
+        mCommandText += "ORDER BY [Created] ASC;";
+        mParameters = [
+            new("@P_Account", m_Profile.Account),
+        ];
+        DataTable mRefreshTokens = await base.GetDataTableAsync(mCommandText, mParameters, true);
+
+        mCommandText = "ZGWSecurity.Get_Account_Roles";
+        mParameters = [
+            new("@P_Account", this.Cleanup(m_Profile.Account)),
+            new("@P_SecurityEntitySeqId", m_SecurityEntitySeqID)
+        ];
+        DataTable mAssignedRoles = await base.GetDataTableAsync(mCommandText, mParameters);
+
+        mCommandText = "ZGWSecurity.Get_Account_Groups";
+        mParameters = [
+            new("@P_Account", this.Cleanup(m_Profile.Account)),
+            new("@P_SecurityEntitySeqId", m_SecurityEntitySeqID)
+        ];
+        DataTable mAssignedGroups = await base.GetDataTableAsync(mCommandText, mParameters);
+
+        mCommandText = "ZGWSecurity.Get_Account_Security";
+        mParameters = [
+            new("@P_Account", this.Cleanup(m_Profile.Account)),
+            new("@P_SecurityEntitySeqId", m_SecurityEntitySeqID)
+        ];
+        DataTable mRoles = await base.GetDataTableAsync(mCommandText, mParameters);
+
+        return new MAccountProfile(mDataRow, mRefreshTokens, mAssignedRoles, mAssignedGroups, mRoles);
+    }
+
+}
---
+++ Net_Core/Benchmark/Program.cs
@@ -0,0 +1,27 @@
+﻿using BenchmarkDotNet.Running;
+using GrowthWare.Benchmark.DataAccess.SQLServer;
+
+namespace GrowthWare.Benchmark;
+
+public partial class Program
+{
+    public static void Main(string[] args)
+    {
+        var results = BenchmarkRunner.Run<DAccounts>();
+        /**  Summary
+
+        BenchmarkDotNet v0.14.0, Windows 11 (10.0.26100.3476)
+        12th Gen Intel Core i7-12700KF, 1 CPU, 20 logical and 12 physical cores
+        .NET SDK 9.0.201
+        [Host]   : .NET 9.0.3 (9.0.325.11113), X64 RyuJIT AVX2
+        .NET 9.0 : .NET 9.0.3 (9.0.325.11113), X64 RyuJIT AVX2
+
+        Job=.NET 9.0  Runtime=.NET 9.0
+
+        | Method          | Mean     | Error     | StdDev    | Median   | Ratio | RatioSD | Gen0    | Gen1   | Allocated | Alloc Ratio |
+        |---------------- |---------:|----------:|----------:|---------:|------:|--------:|--------:|-------:|----------:|------------:|
+        | GetProfile      | 1.126 ms | 0.0767 ms | 0.2251 ms | 1.058 ms |  1.04 |    0.28 | 11.7188 |      - | 168.52 KB |        1.00 |
+        | GetProfileAsync | 1.296 ms | 0.0192 ms | 0.0180 ms | 1.293 ms |  1.19 |    0.21 | 27.3438 | 7.8125 | 366.64 KB |        2.18 |
+        */
+    }
+}
