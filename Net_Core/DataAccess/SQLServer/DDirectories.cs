952174998db2d792721859dabf38c284d51fb536
kahiko <michael.regan@verizon.net>
2025-03-27 08:21:12 -0400

    feature(DataAccess) Added constructor to SQLServer.DDirectories
    feature(BusinessLogic) BDirectories now passes the connection string and the securityEntitySeqID to DDirectories constructor

--- Net_Core/DataAccess/SQLServer/DDirectories.cs
+++ Net_Core/DataAccess/SQLServer/DDirectories.cs
@@ -5,47 +5,55 @@ using System;
 using System.Data;
 using System.Data.SqlClient;
 
-namespace GrowthWare.DataAccess.SQLServer
+namespace GrowthWare.DataAccess.SQLServer;
+
+/// <summary>
+/// Class DDirectories.
+/// </summary>
+public class DDirectories : AbstractDBInteraction, IDirectories
 {
-    /// <summary>
-    /// Class DDirectories.
-    /// </summary>
-    public class DDirectories : AbstractDBInteraction, IDirectories
+
+#region Constructors
+    public DDirectories(string connectionString, int securityEntitySeqId): base() 
     {
-        DataTable IDirectories.Directories()
-        {
-            String mStoredProcedure = "ZGWOptional.Get_Directory";
-            SqlParameter[] mParameters =
-			{
-			 new SqlParameter("@P_FunctionSeqId", -1)
-			};
-            return base.GetDataTable(mStoredProcedure, mParameters);
-        }
-
-        void IDirectories.Save(MDirectoryProfile profile)
-        {
-            if (profile == null) throw new ArgumentNullException(nameof(profile), "profile cannot be a null reference (Nothing in Visual Basic)!");
-            String mStoredProcedure = "ZGWOptional.Set_Directory";
-            String mImpersonateAccount = profile.ImpersonateAccount;
-            String mImpersonatePassword = profile.ImpersonatePassword;
-            if (!profile.Impersonate || string.IsNullOrWhiteSpace(profile.Directory))
-            {
-                mImpersonateAccount = string.Empty;
-                mImpersonatePassword = string.Empty;
-            }
-            SqlParameter[] mParameters =
-			{
-			  new SqlParameter("@P_FunctionSeqId", profile.Id),
-			  new SqlParameter("@P_Directory", profile.Directory),
-			  new SqlParameter("@P_Impersonate", profile.Impersonate),
-			  new SqlParameter("@P_Impersonating_Account", mImpersonateAccount),
-			  new SqlParameter("@P_Impersonating_Password", mImpersonatePassword),
-			  new SqlParameter("@P_Added_Updated_By", GetAddedUpdatedBy(profile, profile.Id)),
-			  GetSqlParameter("@P_Primary_Key", -1, ParameterDirection.Output)			
-			};
-            base.ExecuteNonQuery(mStoredProcedure, mParameters);
-        }
-
-        int IDirectories.SecurityEntitySeqId { get; set; }
+        this.ConnectionString = connectionString;
+        this.SecurityEntitySeqId = securityEntitySeqId;
     }
+#endregion
+
+    DataTable IDirectories.Directories()
+    {
+        String mStoredProcedure = "ZGWOptional.Get_Directory";
+        SqlParameter[] mParameters =
+        {
+            new SqlParameter("@P_FunctionSeqId", -1)
+        };
+        return base.GetDataTable(mStoredProcedure, mParameters);
+    }
+
+    void IDirectories.Save(MDirectoryProfile profile)
+    {
+        if (profile == null) throw new ArgumentNullException(nameof(profile), "profile cannot be a null reference (Nothing in Visual Basic)!");
+        String mStoredProcedure = "ZGWOptional.Set_Directory";
+        String mImpersonateAccount = profile.ImpersonateAccount;
+        String mImpersonatePassword = profile.ImpersonatePassword;
+        if (!profile.Impersonate || string.IsNullOrWhiteSpace(profile.Directory))
+        {
+            mImpersonateAccount = string.Empty;
+            mImpersonatePassword = string.Empty;
+        }
+        SqlParameter[] mParameters =
+        {
+            new SqlParameter("@P_FunctionSeqId", profile.Id),
+            new SqlParameter("@P_Directory", profile.Directory),
+            new SqlParameter("@P_Impersonate", profile.Impersonate),
+            new SqlParameter("@P_Impersonating_Account", mImpersonateAccount),
+            new SqlParameter("@P_Impersonating_Password", mImpersonatePassword),
+            new SqlParameter("@P_Added_Updated_By", GetAddedUpdatedBy(profile, profile.Id)),
+            GetSqlParameter("@P_Primary_Key", -1, ParameterDirection.Output)			
+        };
+        base.ExecuteNonQuery(mStoredProcedure, mParameters);
+    }
+
+    public int SecurityEntitySeqId { get; set; }
 }
--- Release_Notes.md
+++ Release_Notes.md
@@ -108,6 +108,10 @@
 - DDBInformation Added a constructor that accepts connectionString so we don't have to set the properties in BDBInformation.
     - BDBInformation changed the constructor to pass the connectionString to the DDBInformation constructor
     - BDBInformation now accounts for CentralManagement
+- DDirectories Added a constructor that accepts connectionString so we don't have to set the properties in BDirectories.
+    - BDirectories changed the constructor to pass the connectionString and the securityEntitySeqID to the DDirectories constructor
+    - BDirectories now accounts for CentralManagement
+
 
 ### Known Bugs
 - There is an issue where the refresh token is being revoked when it shouldn't be causing a logout to occure
