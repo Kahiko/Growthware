2933c52a5e97bdfe144a2126b3f4dc6eb128054f
kahiko <michael.regan@verizon.net>
2025-04-13 22:07:06 -0400

    feature(Web.Support.Jwt\JwtMiddleware) added try catch around await _next(httpContext) in the Invoke method

--- Net_Core/Web.Support/Jwt/JwtMiddleware.cs
+++ Net_Core/Web.Support/Jwt/JwtMiddleware.cs
@@ -4,12 +4,14 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using GrowthWare.Framework.Models;
 using GrowthWare.Web.Support.Utilities;
+using GrowthWare.Framework;
 
 namespace GrowthWare.Web.Support.Jwt;
 
 public class JwtMiddleware
 {
     private readonly RequestDelegate _next;
+    private readonly Logger m_Logger = Logger.Instance();
     
     [CLSCompliant(false)]
     public JwtMiddleware(RequestDelegate next)
@@ -30,6 +32,14 @@ public class JwtMiddleware
                 httpContext.Items["AccountProfile"] = AccountUtility.GetAccount(mAccount);
             }
         }
-        await _next(httpContext);
+        try
+        {
+            await _next(httpContext);
+        }
+        catch (System.Exception ex)
+        {
+            m_Logger.Error(ex);
+            throw;
+        }
     }
 }
