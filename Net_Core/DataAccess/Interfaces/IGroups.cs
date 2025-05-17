f1cce6cf61789a82fc15c88edc9a23af97ebfd4c
kahiko <michael.regan@verizon.net>
2025-05-17 06:05:47 -0400

    Formatting

--- Net_Core/DataAccess/Interfaces/IGroups.cs
+++ Net_Core/DataAccess/Interfaces/IGroups.cs
@@ -3,59 +3,58 @@ using GrowthWare.Framework.Models;
 using System.Data;
 using System.Threading.Tasks;
 
-namespace GrowthWare.DataAccess.Interfaces
+namespace GrowthWare.DataAccess.Interfaces;
+
+public interface IGroups : IDBInteraction
 {
-    public interface IGroups : IDBInteraction
-    {
 
-        /// <summary>
-        /// Sets or gets the SecurityEntitySeqID
-        /// </summary>
-        int SecurityEntitySeqId { get; set; }
+    /// <summary>
+    /// Sets or gets the SecurityEntitySeqID
+    /// </summary>
+    int SecurityEntitySeqId { get; set; }
 
-        /// <summary>
-        /// GroupProfile
-        /// </summary>
-        MGroupProfile Profile { get; set; }
+    /// <summary>
+    /// GroupProfile
+    /// </summary>
+    MGroupProfile Profile { get; set; }
 
-        /// <summary>
-        /// GroupRoles
-        /// </summary>
-        MGroupRoles GroupRolesProfile { get; set; }
+    /// <summary>
+    /// GroupRoles
+    /// </summary>
+    MGroupRoles GroupRolesProfile { get; set; }
 
-        /// <summary>
-        /// Returns a DataTable of Group roles
-        /// </summary>
-        /// <returns>DataTable</returns>
-        Task<DataTable> GroupRoles();
+    /// <summary>
+    /// Returns a DataTable of Group roles
+    /// </summary>
+    /// <returns>DataTable</returns>
+    Task<DataTable> GroupRoles();
 
-        /// <summary>
-        /// Updates the Groups roles
-        /// </summary>
-        /// <returns>bool</returns>
-        Task<bool> UpdateGroupRoles();
+    /// <summary>
+    /// Updates the Groups roles
+    /// </summary>
+    /// <returns>bool</returns>
+    Task<bool> UpdateGroupRoles();
 
-        /// <summary>
-        /// Get's all of the groups for a given Security Entity
-        /// </summary>
-        /// <returns>DataTable</returns>
-        Task<DataTable> GroupsBySecurityEntity();
+    /// <summary>
+    /// Get's all of the groups for a given Security Entity
+    /// </summary>
+    /// <returns>DataTable</returns>
+    Task<DataTable> GroupsBySecurityEntity();
 
-        /// <summary>
-        /// Returns a data row necessary to populate MGroupProfile
-        /// </summary>
-        /// <returns>DataRow</returns>
-        Task<DataRow> ProfileData();
+    /// <summary>
+    /// Returns a data row necessary to populate MGroupProfile
+    /// </summary>
+    /// <returns>DataRow</returns>
+    Task<DataRow> ProfileData();
 
-        /// <summary>
-        /// Deletes a group in a given Security Entity
-        /// </summary>
-        /// <returns>bool</returns>
-        Task<bool> DeleteGroup();
+    /// <summary>
+    /// Deletes a group in a given Security Entity
+    /// </summary>
+    /// <returns>bool</returns>
+    Task<bool> DeleteGroup();
 
-        /// <summary>
-        /// Saves this instance.
-        /// </summary>
-        Task<int> Save();
-    }
+    /// <summary>
+    /// Saves this instance.
+    /// </summary>
+    Task<int> Save();
 }
