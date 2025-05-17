179dd2d282695f2b2e9f9ef3cb839f25ba2e4efb
kahiko <michael.regan@verizon.net>
2025-05-17 06:03:22 -0400

    Formatting

--- Net_Core/DataAccess/Interfaces/IDirectories.cs
+++ Net_Core/DataAccess/Interfaces/IDirectories.cs
@@ -4,29 +4,28 @@ using GrowthWare.Framework.Models;
 using System.Data;
 using System.Threading.Tasks;
 
-namespace GrowthWare.DataAccess.Interfaces
+namespace GrowthWare.DataAccess.Interfaces;
+
+/// <summary>
+/// Interface IDDirectories
+/// </summary>
+public interface IDirectories : IDBInteraction
 {
-    /// <summary>
-    /// Interface IDDirectories
-    /// </summary>
-    public interface IDirectories : IDBInteraction
-    {
-        		/// <summary>
-		/// Gets the directories.
-		/// </summary>
-		/// <returns>DataTable.</returns>
-		Task<DataTable> Directories();
+	/// <summary>
+	/// Gets the directories.
+	/// </summary>
+	/// <returns>DataTable.</returns>
+	Task<DataTable> Directories();
 
-		/// <summary>
-		/// Saves the specified profile.
-		/// </summary>
-		/// <param name="profile">The profile.</param>
-		Task Save(MDirectoryProfile profile);
+	/// <summary>
+	/// Saves the specified profile.
+	/// </summary>
+	/// <param name="profile">The profile.</param>
+	Task Save(MDirectoryProfile profile);
 
-		/// <summary>
-		/// Gets or sets the security entity seq ID.
-		/// </summary>
-		/// <value>The security entity seq ID.</value>
-		int SecurityEntitySeqId { get; set; }
-    }
+	/// <summary>
+	/// Gets or sets the security entity seq ID.
+	/// </summary>
+	/// <value>The security entity seq ID.</value>
+	int SecurityEntitySeqId { get; set; }
 }
