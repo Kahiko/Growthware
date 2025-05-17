0f5875939b4f9445772cd613ae0cd3c2f6065249
kahiko <michael.regan@verizon.net>
2025-05-17 06:07:00 -0400

    Formatting

--- Net_Core/DataAccess/Interfaces/ILogging.cs
+++ Net_Core/DataAccess/Interfaces/ILogging.cs
@@ -5,32 +5,30 @@ using System.Collections.Generic;
 using System.Threading;
 using System.Threading.Tasks;
 
-namespace GrowthWare.DataAccess.Interfaces
+namespace GrowthWare.DataAccess.Interfaces;
+
+/// <summary>
+/// The base interface for Database interaction code
+/// </summary>
+public interface ILogging : IDBInteraction
 {
+
     /// <summary>
-    /// The base interface for Database interaction code
+    /// Returns a populated MLoggingProfile object given the logSeqId
     /// </summary>
-    public interface ILogging : IDBInteraction
-    {
+    /// <param name="logSeqId">int</param>
+    /// <returns>MLoggingProfile</returns>
+    Task<MLoggingProfile> GetLog(int logSeqId);
 
-        /// <summary>
-        /// Returns a populated MLoggingProfile object given the logSeqId
-        /// </summary>
-        /// <param name="logSeqId">int</param>
-        /// <returns>MLoggingProfile</returns>
-        Task<MLoggingProfile> GetLog(int logSeqId);
+    /// <summary>
+    /// Returns data from the Logging table
+    /// </summary>
+    /// <param name="cancellationToken"></param>
+    /// <returns>IAsyncEnumerable<IDataRecord></returns>
+    IAsyncEnumerable<IDataRecord> GetLogs(CancellationToken cancellationToken = default);
 
-        /// <summary>
-        /// Returns data from the Logging table
-        /// </summary>
-        /// <param name="cancellationToken"></param>
-        /// <returns>IAsyncEnumerable<IDataRecord></returns>
-        IAsyncEnumerable<IDataRecord> GetLogs(CancellationToken cancellationToken = default);
-
-        /// <summary>
-        /// Saves this instance.
-        /// </summary>
-        Task Save(MLoggingProfile profile);
-    }
-
+    /// <summary>
+    /// Saves this instance.
+    /// </summary>
+    Task Save(MLoggingProfile profile);
 }
