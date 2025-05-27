272f33331b8400747717db2ae158421c634b8c26
kahiko <michael.regan@verizon.net>
2025-05-27 15:45:27 -0400

    feature(Database stored procedure Get_Role) Fixed returning too much information needed to add the BEGIN/END keywords it worked but	no need in returning too much information if it's not needed.

--- Net_Core/DatabaseManager/Scripts/Upgrade/SQLServer/Version_6.0.1.0.sql
+++ Net_Core/DatabaseManager/Scripts/Upgrade/SQLServer/Version_6.0.1.0.sql
@@ -615,6 +615,85 @@ RETURN 0
 GO
 /****** End: [ZGWSecurity].[Get_Function] ******/
 
+/****** Start: [ZGWSecurity].[Get_Role] ******/
+SET QUOTED_IDENTIFIER ON;
+GO
+/*
+Usage:
+DECLARE 
+	@P_RoleSeqId AS INT = -1,
+	@P_SecurityEntitySeqId AS INT = 6,
+	@P_Debug INT = 1
+
+EXEC [ZGWSecurity].[Get_Role]
+	@P_RoleSeqId,
+	@P_SecurityEntitySeqId,
+	@P_Debug
+*/
+-- =============================================
+-- Author:		Michael Regan
+-- Create date: 08/18/2011
+-- Description:	Retrieves roles given the
+--	the RoleSeqId and SecurityEntitySeqId
+-- Note:
+--	RoleSeqId of -1 returns all roles.
+-- =============================================
+-- Author:		Michael Regan
+-- Create date: 05/27/2025
+-- Description:	Fixed returning too much information needed to add the BEGIN/END keywords it worked but
+--	no need in returning too much information if it's not needed.
+-- =============================================
+CREATE OR ALTER PROCEDURE [ZGWSecurity].[Get_Role]
+	@P_RoleSeqId INT,
+	@P_SecurityEntitySeqId INT,
+	@P_Debug INT = 0
+AS
+	SET NOCOUNT ON
+	IF @P_Debug = 1 PRINT 'Start ZGWSecurity.Get_Role and SELECT an existing row from the table.'
+	IF @P_RoleSeqId > -1 -- SELECT an existing row from the table.
+		BEGIN
+			SELECT
+				[Roles].[RoleSeqId] AS [ROLE_SEQ_ID],
+				[Roles].[Name],
+				[Roles].[Description],
+				[Roles].[Is_System],
+				[Roles].[Is_System_Only],
+				[Roles].[Added_By],
+				[Roles].[Added_Date],
+				[Roles].[Updated_By],
+				[Roles].[Updated_Date]
+			FROM
+				[ZGWSecurity].[Roles] AS [Roles]
+			WHERE
+				[Roles].RoleSeqId = @P_RoleSeqId
+		END
+	ELSE -- GET ALL ROLES FOR A GIVEN Security Entity
+		BEGIN
+			IF @P_Debug = 1 PRINT 'GET ALL ROLES FOR A GIVEN Security Entity.'
+			SELECT
+				[Roles].[RoleSeqId] AS ROLE_SEQ_ID,
+				[Roles].[Name],
+				[Roles].[Description],
+				[Roles].[Is_System],
+				[Roles].[Is_System_Only],
+				[Roles].[Added_By],
+				[Roles].[Added_Date],
+				[Roles].[Updated_By],
+				[Roles].[Updated_Date]
+			FROM
+				[ZGWSecurity].[Roles] AS [Roles]
+				INNER JOIN [ZGWSecurity].[Roles_Security_Entities] AS [RSE] ON
+					[RSE].[SecurityEntitySeqId] = @P_SecurityEntitySeqId
+					AND [Roles].[RoleSeqId] = [RSE].[RoleSeqId]
+			ORDER BY
+				[Roles].[Name]
+		END
+	-- END IF		
+	IF @P_Debug = 1 PRINT 'End ZGWSecurity.Get_Role'
+RETURN 0
+GO
+/****** End: [ZGWSecurity].[Get_Role] ******/
+
 -- Update the version
 UPDATE [ZGWSystem].[Database_Information]
 SET [Version] = '6.0.1.0'
